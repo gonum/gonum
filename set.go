d042febb2f6ee6e28c36889fde38b8c2161f0406
Jsor <Jragonmiris@gmail.com>
2013-04-30 07:38:43 -0700

    Changed intersect a tad

--- set.go
+++ set.go
@@ -6,8 +6,12 @@ package discrete
 // the implementation, but that adds a lot of calls and needless operations, making the library slower
 type Set map[interface{}]struct{}
 
+// For cleanliness
 var flag struct{} = struct{}{}
 
+/* All functions copy the sets. Shouldn't be too much of a problem. I could be persuaded to change thing to be in-place though I guess. Partially depends on what we decide for other repositories */
+
+// Add elements of set 2 to set 1
 func (s1 Set) Union(s2 Set) Set {
 	
 	for el := range s2 {
@@ -17,23 +21,23 @@ func (s1 Set) Union(s2 Set) Set {
 	return s2
 }
 
-func (s1 Set) Intersection(s2 Set) (out Set) {
+// Iterate over the smaller set, remove all elements not also in the bigger set
+func (s1 Set) Intersection(s2 Set) Set {
 	// Iterate over the smaller set
 	if len(s1) > len(s2) {
 		s1,s2 = s2,s1
 	} //Q: Should this just be an if-else? Style-wise I like the swap, not sure if it's performance heavy
-	
-	out = make(map[interface{}]struct{}, len(s1))
-	
+
 	for el := range s1 {
-		if s2[el] == flag {
-			out[el] = flag
+		if s2[el] != flag {
+			delete(el, s1)
 		}
 	}
 	
-	return out
+	return s1
 }
 
+// Del
 func (s1 Set) Diff(s2 Set) Set {
 	// Usually modifying an element being iterated over is a no-no, but go copies the argument to range
 	// so this is safe

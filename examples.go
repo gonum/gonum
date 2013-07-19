a611cdf84e0ee514506f661aa4f28bc2e7e658ac
btracey <bdtinternet@gmail.com>
2013-07-18 17:30:08 -0700

    Trying to get example to match godoc

--- examples.go
+++ examples.go
@@ -1,9 +1,18 @@
 package sliceops
 
+import (
+	"fmt"
+)
+
 // Set of examples for all the functions
 
 func ExampleAdd() {
 	s1 := []float64{1, 2, 3, 4}
 	s2 := []float64{5, 6, 7, 8}
 	Add(s1, s2)
+	fmt.Println("s1 = ", s1)
+	fmt.Println("s2 = ", s2)
+	// Output:
+	// s1 = [6,8,10,12]
+	// s2 = [6,8,10,12]
 }

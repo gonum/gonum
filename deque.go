a342f2e9ed5637e064c03741ed13ea5e1f7015ff
Jsor <Jragonmiris@gmail.com>
2013-07-24 04:37:28 -0700

    Fixed a stack/queue/deque bug (pointer receiever

--- deque.go
+++ deque.go
@@ -33,84 +33,88 @@ type Stack []interface{}
 
 type Queue []interface{}
 
-func (s Stack) Push(x interface{}) {
-	s = append(s, x)
+func (s *Stack) Push(x interface{}) {
+	*s = append(*s, x)
 }
 
-func (s Stack) Pop() (interface{}, error) {
-	if len(s) == 0 {
+func (s *Stack) Pop() (interface{}, error) {
+	if len(*s) == 0 {
 		return 0, errors.New("No element to pop")
 	}
 
-	x := s[len(s)-1]
-	s = s[:len(s)-1]
+	x := (*s)[len(*s)-1]
+	*s = (*s)[:len(*s)-1]
 
 	return x, nil
 }
 
-func (s Stack) Peek() (interface{}, error) {
-	if len(s) == 0 {
+func (s *Stack) Peek() (interface{}, error) {
+	if len(*s) == 0 {
 		return 0, errors.New("No element to peek at")
 	}
 
-	return s[len(s)-1], nil
+	return (*s)[len(*s)-1], nil
 }
 
-func (s Stack) IsEmpty() bool {
-	return len(s) == 0
+func (s *Stack) IsEmpty() bool {
+	return len(*s) == 0
 }
 
-func (q Queue) Push(x interface{}) {
-	q = append(q, x)
+func (q *Queue) Push(x interface{}) {
+	*q = append(*q, x)
 }
 
-func (q Queue) Pop() (interface{}, error) {
-	if len(q) == 0 {
+func (q *Queue) Pop() (interface{}, error) {
+	if len(*q) == 0 {
 		return 0, errors.New("No element to pop")
 	}
 
-	x := q[0]
-	q = q[1:]
+	x := (*q)[0]
+	*q = (*q)[1:]
 
 	return x, nil
 }
 
-func (q Queue) Peek() (interface{}, error) {
-	if len(q) == 0 {
+func (q *Queue) Peek() (interface{}, error) {
+	if len(*q) == 0 {
 		return 0, errors.New("No element to peek at")
 	}
 
-	return q[0], nil
+	return (*q)[0], nil
 }
 
-func (q Queue) IsEmpty() bool {
-	return len(q) == 0
+func (q *Queue) IsEmpty() bool {
+	return len(*q) == 0
 }
 
 // Deque is a stack/queue hybrid (from "deck"), I'm not sure if the type conversions will hurt performance or not (I suspect not)
 type Deque []interface{}
 
-func (d Deque) IsEmpty() bool {
-	return len(d) == 0
+func (d *Deque) IsEmpty() bool {
+	return len(*d) == 0
 }
 
-func (d Deque) Push(x interface{}) {
-	d = append(d, x)
+func (d *Deque) Push(x interface{}) {
+	*d = append(*d, x)
 }
 
-func (d Deque) Pop() (interface{}, error) {
-	return Stack(d).Pop()
+func (d *Deque) Pop() (interface{}, error) {
+	a := Stack(*d)
+	return (&a).Pop()
 }
 
 // Poll is a queue-pop
-func (d Deque) Poll() (interface{}, error) {
-	return Queue(d).Pop()
+func (d *Deque) Poll() (interface{}, error) {
+	a := Queue(*d)
+	return (&a).Pop()
 }
 
-func (d Deque) PeekLast() (interface{}, error) {
-	return Stack(d).Peek()
+func (d *Deque) PeekLast() (interface{}, error) {
+	a := Stack(*d)
+	return (&a).Peek()
 }
 
-func (d Deque) PeekFirst() (interface{}, error) {
-	return Queue(d).Peek()
+func (d *Deque) PeekFirst() (interface{}, error) {
+	a := Queue(*d)
+	return (&a).Peek()
 }

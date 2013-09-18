d844a6a502693772ac3d137062d42fc4dab988e6
btracey <bdtinternet@gmail.com>
2013-09-17 17:46:36 -0700

    Added dimless

--- unit.go
+++ unit.go
@@ -5,7 +5,7 @@ import (
 	"math"
 )
 
-// Represents a mass in kg
+// Represents a mass in kilograms
 type Mass float64
 
 const (
@@ -16,6 +16,7 @@ const (
 	Microgram Mass = 1e-9
 )
 
+// Length converts the Length to a unit
 func (m Mass) Unit() *Unit {
 	return CreateUnit(float64(m), &Dimensions{Mass: 1})
 }
@@ -33,5 +34,62 @@ func (m *Mass) FromUnit(u Uniter) error {
 		return errors.New("Dimension mismatch")
 	}
 	(*m) = Mass(u.Unit().Value())
+	return nil
+}
+
+// Length represents a length in meters
+type Length float64
+
+const (
+	Meter      Length = 1.0
+	Centimeter Length = 0.01
+)
+
+// Unit converts the Length to a unit
+func (l Length) Unit() *Unit {
+	return CreateUnit(float64(l), &Dimensions{Length: 1})
+}
+
+// So it can implement a lengther interface
+func (l Length) Length() Length {
+	return l
+}
+
+// FromUnit converts a uniter to a length. Returns an error if there
+// is a mismatch in dimension
+func (l *Length) FromUnit(u Uniter) error {
+	if !DimensionsMatch(u, Meter) {
+		(*l) = Length(math.NaN())
+		return errors.New("Dimension mismatch")
+	}
+	(*l) = Length(u.Unit().Value())
+	return nil
+}
+
+// Dimless represents a dimensionless constant
+type Dimless float64
+
+const (
+	One Dimless = 1.0
+)
+
+// Unit converts the Dimless to a unit
+func (d Dimless) Unit() *Unit {
+	return CreateUnit(float64(d), &Dimensions{})
+}
+
+// So it can implement a Dimless interface
+func (d Dimless) Dimless() Dimless {
+	return d
+}
+
+// FromUnit converts the unit to a dimless. Returns an error if there
+// is a mismatch in dimension
+func (d *Dimless) FromUnit(u *Unit) error {
+	if !DimensionsMatch(u, One) {
+		(*d) = Dimless(math.NaN())
+		return errors.New("Dimension mismatch")
+	}
+	(*d) = Dimless(u.Unit().Value())
 	return nil
 }

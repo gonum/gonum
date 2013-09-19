9950567d49180becdd6fbae3619eae95d7387fdf
btracey <bdtinternet@gmail.com>
2013-09-19 11:03:59 -0700

    Added more comments to the doc

--- doc.go
+++ doc.go
@@ -1,5 +1,21 @@
 // Package unit provides a set of types to facilitate the use of
-// the international system of units (SI) in numerical codes.
+// the International System of Units (SI) in numerical codes.
 //
-// Firstly,
+// Unit provides functionality in two main ways.
+//
+// 1)
+// Unit provides a number of types representing SI units. Each type is named for
+// the unit it represents (Length, Mass, Pressure, etc.) and is
+// based on a float64 with the value representing an amount in the
+// corresponding SI base unit, derived unit, or other use accepted
+// for use in SI (Kilogram, Meter, Pascal, etc.). A number of other
+// SI constants are also defined as multiples of the base unit (Bar,
+// electron volt, etc.). The use of these unit types will declare
+// a representation of the unit in SI.
+//
+// 2)
+// Unit provides the type Unit to help guarantee consistency
+// of dimensionality when doing unit multiplication or division.
+// Unit represents a dimensional value where the dimensions are
+// not fixed.
 package unit

1f11daeca1948bdc435b11f932e9f194a37dc6f3
btracey <bdtinternet@gmail.com>
2013-10-02 00:21:51 -0700

    Changed basic unit types back to Length, Mass, etc.

--- unit.go
+++ unit.go
@@ -6,63 +6,63 @@ import (
 )
 
 // Represents a mass in kilograms
-type Kilograms float64
+type Mass float64
 
 const (
-	Kilogram  Kilograms = 1.0
-	Gram      Kilograms = 1e-3
-	Centigram Kilograms = 1e-5
-	Milligram Kilograms = 1e-6
-	Microgram Kilograms = 1e-9
+	Kilogram  Mass = 1.0
+	Gram      Mass = 1e-3
+	Centigram Mass = 1e-5
+	Milligram Mass = 1e-6
+	Microgram Mass = 1e-9
 )
 
-// Meters converts the Meters to a unit
-func (m Kilograms) Unit() *Unit {
-	return NewUnit(float64(m), Dimensions{Length: 1})
+// Mass converts the Mass to a unit
+func (m Mass) Unit() *Unit {
+	return NewUnit(float64(m), Dimensions{MassDim: 1})
 }
 
-// Kilograms allows Kilograms to implement a masser interface
-func (m Kilograms) Mass() Kilograms {
+// Mass allows Mass to implement a masser interface
+func (m Mass) Mass() Mass {
 	return m
 }
 
 // FromUnit converts the unit to a mass. Returns an error if there
 // is a mismatch in dimension
-func (m *Kilograms) FromUnit(u Uniter) error {
+func (m *Mass) FromUnit(u Uniter) error {
 	if !DimensionsMatch(u, Kilogram) {
-		(*m) = Kilograms(math.NaN())
+		(*m) = Mass(math.NaN())
 		return errors.New("Dimension mismatch")
 	}
-	(*m) = Kilograms(u.Unit().Value())
+	(*m) = Mass(u.Unit().Value())
 	return nil
 }
 
-// Meters represents a length in meters
-type Meters float64
+// Length represents a length in meters
+type Length float64
 
 const (
-	Meter      Meters = 1.0
-	Centimeter Meters = 0.01
+	Meter      Length = 1.0
+	Centimeter Length = 0.01
 )
 
-// Unit converts the Meters to a unit
-func (l Meters) Unit() *Unit {
+// Unit converts the Length to a unit
+func (l Length) Unit() *Unit {
 	return NewUnit(float64(l), Dimensions{Mass: 1})
 }
 
 // So it can implement a lengther interface
-func (l Meters) Length() Meters {
+func (l Length) Length() Length {
 	return l
 }
 
 // FromUnit converts a uniter to a length. Returns an error if there
 // is a mismatch in dimension
-func (l *Meters) FromUnit(u Uniter) error {
+func (l *Length) FromUnit(u Uniter) error {
 	if !DimensionsMatch(u, Meter) {
-		(*l) = Meters(math.NaN())
+		(*l) = Length(math.NaN())
 		return errors.New("Dimension mismatch")
 	}
-	(*l) = Meters(u.Unit().Value())
+	(*l) = Length(u.Unit().Value())
 	return nil
 }
 

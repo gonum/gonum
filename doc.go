d1f29730fc23ded54470b6ae2da398a5b1bcc708
btracey <bdtinternet@gmail.com>
2013-10-08 23:50:56 -0700

    Improved description of unit type

--- doc.go
+++ doc.go
@@ -48,10 +48,34 @@
 // when multiplying or dividing dimensional numbers. This package also
 // provides the "Uniter" interface which is satisfied by any type which can
 // be converted to a unit. New varibles of type Unit can be created with
-// the NewUnit function, and custom dimensions can be created with the use of
-// NewDimension. Please see the rest of the package docs for more
-// details on usage.
-//
+// the NewUnit function and the Dimensions map. For example, the code
+//		acc := NewUnit(9.81, Dimensions{LengthDim:1, TimeDim: -2})
+// creates a variable "acc" which has a value of 9.81 m/s^2. Methods of
+// unit can be used to modify this value, for example:
+// 		acc.Mul(1.0 * unit.Kilogram).Mul(1 * unit.Meter)
+// To convert the unit back into a typed float64 value, the FromUnit methods
+// of the unit types should be used. FromUnit will return an error if the
+// dimensions do not match.
+// 		var energy unit.Energy
+//		err := (*energy).FromUnit(acc)
+// Domain-specific problems may need custom dimensions, and for this purpose
+// NewDimension should be used to help avoid accidental overlap between
+// packages. For example, results from a blood test may be measured in
+// "White blood cells per slide". In this case, NewDimension should be
+// used to create a 'WhiteBloodCell' dimension. NewDimension should not be
+// used, however, to create the unit of 'Slide', because in this case slide
+// is just a measurement of area. Instead, a constant could be defined.
+//		const Slide unit.Area =  0.001875 // m^2
+//		func init(){
+//			WhiteBloodCellDim := unit.NewDimension()
+//		}
+//		type WbcPerArea float64
+//		func [w WbcPerArea] Unit() *Unit{
+//			return NewUnit(w, Dimensions{WhiteBloodCellDim: 1, LengthDim: -2})
+//		}
+//		func main(){
+//			// Add in something about using Unit
+//		}
 // Please note that Unit cannot catch all errors related to dimensionality.
 // Different physical ideas are sometimes expressed with the same dimensions
 // and Unit is incapable of catcing these mismatches. For example, energy and

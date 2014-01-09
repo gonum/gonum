99d5679f8e58f4ad406c7ac3b447c6eb95201f50
Jsor <Jragonmiris@gmail.com>
2014-01-08 23:16:18 -0700

    Updated stack/queue/deque API

--- xifo/deque.go
+++ xifo/deque.go
@@ -1,120 +1,118 @@
 package xifo
 
-import (
-	"errors"
-)
+import ()
 
 /* Simple Stack/Queue implementations, I don't think they need to be explained much */
 
-type Pusher interface {
+type Stack interface {
 	Push(x interface{})
-}
-
-type Peeker interface {
-	Peek() (interface{}, error)
-}
-
-type Popper interface {
-	Pop() (interface{}, error)
-}
-
-type IsEmptyer interface {
+	Pop() interface{}
+	PeekLast() interface{}
 	IsEmpty() bool
 }
 
-type XInFirstOut interface {
-	Pusher
-	Popper
-	Peeker
-	IsEmptyer
+type Queue interface {
+	Push(x interface{})
+	Poll() interface{}
+	PeekFirst() interface{}
+	IsEmpty() bool
 }
 
-type Stack []interface{}
+type Deque interface {
+	Push(x interface{})
+	Poll() interface{}
+	Pop() interface{}
+	PeekFirst() interface{}
+	PeekLas() interface{}
+	IsEmpty() bool
+}
 
-type Queue []interface{}
+type GonumStack []interface{}
 
-func (s *Stack) Push(x interface{}) {
+type GonumQueue []interface{}
+
+func (s *GonumStack) Push(x interface{}) {
 	*s = append(*s, x)
 }
 
-func (s *Stack) Pop() (interface{}, error) {
+func (s *GonumStack) Pop() interface{} {
 	if len(*s) == 0 {
-		return 0, errors.New("No element to pop")
+		panic("No element to pop")
 	}
 
 	x := (*s)[len(*s)-1]
 	*s = (*s)[:len(*s)-1]
 
-	return x, nil
+	return x
 }
 
-func (s *Stack) Peek() (interface{}, error) {
+func (s *GonumStack) PeekLast() interface{} {
 	if len(*s) == 0 {
-		return 0, errors.New("No element to peek at")
+		panic("No element to peek at")
 	}
 
-	return (*s)[len(*s)-1], nil
+	return (*s)[len(*s)-1]
 }
 
-func (s *Stack) IsEmpty() bool {
+func (s *GonumStack) IsEmpty() bool {
 	return len(*s) == 0
 }
 
-func (q *Queue) Push(x interface{}) {
+func (q *GonumQueue) Push(x interface{}) {
 	*q = append(*q, x)
 }
 
-func (q *Queue) Pop() (interface{}, error) {
+func (q *GonumQueue) Poll() interface{} {
 	if len(*q) == 0 {
-		return 0, errors.New("No element to pop")
+		panic("No element to poll")
 	}
 
 	x := (*q)[0]
 	*q = (*q)[1:]
 
-	return x, nil
+	return x
 }
 
-func (q *Queue) Peek() (interface{}, error) {
+func (q *GonumQueue) PeekFirst() interface{} {
 	if len(*q) == 0 {
-		return 0, errors.New("No element to peek at")
+		panic("No element to peek at")
 	}
 
-	return (*q)[0], nil
+	return (*q)[0]
 }
 
-func (q *Queue) IsEmpty() bool {
+func (q *GonumQueue) IsEmpty() bool {
 	return len(*q) == 0
 }
 
 // Deque is a stack/queue hybrid (from "deck"), I'm not sure if the type conversions will hurt performance or not (I suspect not)
-type Deque []interface{}
+type GonumDeque []interface{}
 
-func (d *Deque) IsEmpty() bool {
+func (d *GonumDeque) IsEmpty() bool {
 	return len(*d) == 0
 }
 
-func (d *Deque) Push(x interface{}) {
+func (d *GonumDeque) Push(x interface{}) {
 	*d = append(*d, x)
 }
 
-func (d *Deque) Pop() (interface{}, error) {
-	a := Stack(*d)
-	return (&a).Pop()
+func (d *GonumDeque) Pop() interface{} {
+	a := (*GonumStack)(d)
+	return a.Pop()
 }
 
 // Poll is a queue-pop
-func (d *Deque) Poll() (interface{}, error) {
-	a := Queue(*d)
-	return (&a).Pop()
+func (d *GonumDeque) Poll() interface{} {
+	a := (*GonumQueue)(d)
+	return a.Poll()
 }
 
-func (d *Deque) PeekLast() (interface{}, error) {
-	a := Stack(*d)
-	return (&a).Peek()
+func (d *GonumDeque) PeekLast() interface{} {
+	a := (*GonumStack)(d)
+	return a.PeekLast()
 }
 
-func (d *Deque) PeekFirst() (interface{}, error) {
-	a := Queue(*d)
-	return (&a).Peek()
+func (d *GonumDeque) PeekFirst() interface{} {
+	a := (*GonumQueue)(d)
+	return a.PeekFirst()
 }

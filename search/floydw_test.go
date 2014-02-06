5c043ce1730315affc56fe002f0aed496d9b7e9f
Jsor <Jragonmiris@gmail.com>
2014-02-06 00:31:03 -0700

    Basic no-panic test

---
+++ search/floydw_test.go
@@ -0,0 +1,94 @@
+package search_test
+
+import (
+	"github.com/gonum/graph/concrete"
+	"github.com/gonum/graph/search"
+	"math"
+	"testing"
+)
+
+func TestFWOneEdge(t *testing.T) {
+	dg := concrete.NewDenseGraph(2, true)
+	aPaths, sPath := search.FloydWarshall(dg, nil)
+
+	path, cost, err := sPath(concrete.GonumNode(0), concrete.GonumNode(1))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if math.Abs(cost-1.0) > .000001 {
+		t.Errorf("FW got wrong cost %f", cost)
+	}
+
+	if len(path) != 2 || path[0].ID() != 0 && path[1].ID() != 1 {
+		t.Errorf("Wrong path in FW %v", path)
+	}
+
+	paths, cost, err := aPaths(concrete.GonumNode(0), concrete.GonumNode(1))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if math.Abs(cost-1.0) > .000001 {
+		t.Errorf("FW got wrong cost %f", cost)
+	}
+
+	if len(paths) != 1 {
+		t.Errorf("Didn't get right paths in FW %v", paths)
+	}
+
+	path = paths[0]
+	if len(path) != 2 || path[0].ID() != 0 && path[1].ID() != 1 {
+		t.Errorf("Wrong path in FW allpaths %v", path)
+	}
+}
+
+func TestFWTwoPaths(t *testing.T) {
+	dg := concrete.NewDenseGraph(5, false)
+	// Adds two paths from 0->2 of equal length
+	dg.SetEdgeCost(concrete.GonumNode(0), concrete.GonumNode(2), 2.0, true)
+	dg.SetEdgeCost(concrete.GonumNode(0), concrete.GonumNode(1), 1.0, true)
+	dg.SetEdgeCost(concrete.GonumNode(1), concrete.GonumNode(2), 1.0, true)
+
+	aPaths, sPath := search.FloydWarshall(dg, nil)
+	path, cost, err := sPath(concrete.GonumNode(0), concrete.GonumNode(2))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if math.Abs(cost-2.0) > .00001 {
+		t.Errorf("Path has incorrect cost, %f", cost)
+	}
+
+	if len(path) == 2 && path[0].ID() == 0 && path[1].ID() == 2 {
+		t.Logf("Got correct path: %v", path)
+	} else if len(path) == 3 && path[0].ID() == 0 && path[1].ID() == 1 && path[2].ID() == 2 {
+		t.Logf("Got correct path %v", path)
+	} else {
+		t.Errorf("Got wrong path %v", path)
+	}
+
+	paths, cost, err := aPaths(concrete.GonumNode(0), concrete.GonumNode(2))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if math.Abs(cost-2.0) > .00001 {
+		t.Errorf("All paths function gets incorrect cost, %f", cost)
+	}
+
+	if len(paths) != 2 {
+		t.Fatalf("Didn't get all shortest paths %v", paths)
+	}
+
+	for _, path := range paths {
+		if len(path) == 2 && path[0].ID() == 0 && path[1].ID() == 2 {
+			t.Logf("Got correct path for all paths: %v", path)
+		} else if len(path) == 3 && path[0].ID() == 0 && path[1].ID() == 1 && path[2].ID() == 2 {
+			t.Logf("Got correct path for all paths %v", path)
+		} else {
+			t.Errorf("Got wrong path for all paths %v", path)
+		}
+	}
+}

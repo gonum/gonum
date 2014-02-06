0e64eae1eb488f367fcdeb50af6af888d7107bb4
Jsor <Jragonmiris@gmail.com>
2014-02-06 02:43:33 -0700

    Totally fixed the double-add problem

--- search/floydwarshall.go
+++ search/floydwarshall.go
@@ -95,7 +95,7 @@ func genAllPathsFunc(dist []float64, next [][]int, nodes []gr.Node, graph gr.Gra
 		toReturn := make([][]gr.Node, 0, len(intermediates))
 		// Special case: if intermediates exist we need to explicitly check to see if i and j is also an optimal path
 		if isSuccessor(nodes[i], nodes[j]) && math.Abs(dist[i+j*numNodes]-cost(nodes[i], nodes[j])) < .000001 {
-			toReturn = append(toReturn, []gr.Node{nodes[i]})
+			toReturn = append(toReturn, []gr.Node{})
 		}
 
 		// This step is a tad convoluted: we have some list of intermediates.

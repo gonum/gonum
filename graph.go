a1864ef21ce9ab5351a6912eec135f583ed299a1
kortschak <dan.kortschak@adelaide.edu.au>
2014-04-08 13:45:50 +0930

    Remove DStarGraph
    DStar is not currently implemented (see issue #2).

--- graph.go
+++ graph.go
@@ -169,28 +169,6 @@ type MutableGraph interface {
 	EmptyGraph()
 }
 
-// A DStarGraph is a special interface that allows the DStarLite function to be used on a graph.
-//
-// D*-lite is an algorithm that allows for the graph representation to change when actions are
-// taken, whether this be from actions taken by the agent or simply new information gathered.
-// As such, there's a Move function, that allows the graph to take into account an agent moving
-// to the next node. This is always followed by a call to ChangedEdges.
-//
-// Traditionally in D*-lite, the algorithm would scan every edge to see if the cost changed, and
-// then update its information if it detected any changes. This slightly remixed step allows the
-// graph to provide notification of any changes, and even provide an alternate cost function if it
-// needs to. This can be used to speed up the algorithm significantly since the graph no longer has
-// to scan for changes, and only updates when told to. If changedEdges is nil or of len 0, no
-// updates will be performed. If changedEdges is not nil, it will update the internal
-// representation. If newCostFunc is non-nil it will be swapped with dStar's current cost function
-// if and only if changedEdges is non-nil/len>0, however, newCostFunc is not required to be non-nil
-// if updates are present. DStar will continue using the current cost function if that is the case.
-type DStarGraph interface {
-	Graph
-	Move(target Node)
-	ChangedEdges() (newCostFunc func(Node, Node) float64, changedEdges []Edge)
-}
-
 // A function that returns the cost of following an edge
 type CostFunc func(Edge) float64
 

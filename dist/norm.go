5ad5726ba03b1e01c9277cd99758fba273a8a780
btracey <bdtinternet@gmail.com>
2014-09-07 13:14:56 -0700

    Added TODO for improving ConjugateUpdate

--- dist/norm.go
+++ dist/norm.go
@@ -56,6 +56,9 @@ func (n Normal) CDF(x float64) float64 {
 // This function panics if len(suffStat) != 2 or len(priorStrength) != 2.
 func (n *Normal) ConjugateUpdate(suffStat []float64, nSamples float64, priorStrength []float64) {
 
+	// TODO: Support prior strength with math.Inf(1) to allow updating with
+	// a known mean/standard deviation
+
 	totalMeanSamples := nSamples + priorStrength[0]
 	totalSum := suffStat[0]*nSamples + n.Mu*priorStrength[0]
 

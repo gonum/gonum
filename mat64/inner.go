ee3a35d95bb1a45135b0bc2b210bb81b35fd80f9
btracey <bdtinternet@gmail.com>
2014-10-07 23:21:36 -0700

    Added Inner and test

---
+++ mat64/inner.go
@@ -0,0 +1,42 @@
+package mat64
+
+// Inner computes the generalized inner product between x and y with matrix A.
+//  x^T A y
+// This is only a true inner product if m is symmetric positive definite, though
+// the operation works for any matrix A.
+//
+// Inner panics if len(x) != m or len(y) != n when A is an m x n matrix.
+func Inner(x []float64, A Matrix, y []float64) float64 {
+	m, n := A.Dims()
+	if len(x) != m {
+		panic(ErrShape)
+	}
+	if len(y) != n {
+		panic(ErrShape)
+	}
+	if m == 0 || n == 0 {
+		return 0
+	}
+
+	var sum float64
+
+	switch b := A.(type) {
+	case RawMatrixer:
+		bmat := b.RawMatrix()
+		for i, xi := range x {
+			for j, yj := range y {
+				sum += xi * bmat.Data[i*bmat.Stride+j] * yj
+
+			}
+		}
+		return sum
+	default:
+		for i := 0; i < m; i++ {
+			xi := x[i]
+			for j := 0; j < n; j++ {
+				sum += xi * A.At(i, j) * y[j]
+			}
+		}
+		return sum
+	}
+}
---
+++ mat64/inner_test.go
@@ -0,0 +1,68 @@
+package mat64
+
+import (
+	"math"
+	"testing"
+)
+
+func TestInner(t *testing.T) {
+	for i, test := range []struct {
+		x   []float64
+		y   []float64
+		m   [][]float64
+		ans float64
+	}{
+		{
+			x:   []float64{5},
+			y:   []float64{10},
+			m:   [][]float64{{2}},
+			ans: 100,
+		},
+		{
+			x:   []float64{5, 6, 1},
+			y:   []float64{10},
+			m:   [][]float64{{2}, {-3}, {5}},
+			ans: -30,
+		},
+		{
+			x:   []float64{5},
+			y:   []float64{10, 15},
+			m:   [][]float64{{2, -3}},
+			ans: -125,
+		},
+		{
+			x: []float64{1, 5},
+			y: []float64{10, 15},
+			m: [][]float64{
+				{2, -3},
+				{4, -1},
+			},
+			ans: 100,
+		},
+		{
+			x: []float64{2, 3, 9},
+			y: []float64{8, 9},
+			m: [][]float64{
+				{2, 3},
+				{4, 5},
+				{6, 7},
+			},
+			ans: 1316,
+		},
+		{
+			x: []float64{2, 3},
+			y: []float64{8, 9, 9},
+			m: [][]float64{
+				{2, 3, 6},
+				{4, 5, 7},
+			},
+			ans: 614,
+		},
+	} {
+		m := NewDense(flatten(test.m))
+		ans := Inner(test.x, m, test.y)
+		if math.Abs(ans-test.ans) > 1e-14 {
+			t.Errorf("Inner product mismatch case %v. Want: %v, Got: %v", i, test.ans, ans)
+		}
+	}
+}

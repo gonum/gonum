cf1853cf810f049dbf569faf1dec0d300ce4e463
btracey <bdtinternet@gmail.com>
2014-10-08 08:57:01 -0700

    Added license headers.

--- mat64/inner.go
+++ mat64/inner.go
@@ -1,3 +1,7 @@
+// Copyright ©2014 The gonum Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package mat64
 
 // Inner computes the generalized inner product between x and y with matrix A.

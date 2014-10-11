161e16a69822480e6c8395cf3ce3f011147fc6ee
btracey <bdtinternet@gmail.com>
2014-10-11 12:53:34 -0700

    removed unnecessary parens

--- floats.go
+++ floats.go
@@ -587,7 +587,7 @@ func Same(s, t []float64) bool {
 	}
 	for i, v := range s {
 		w := t[i]
-		if (v != w) && !math.IsNaN(v) && !math.IsNaN(w) {
+		if v != w && !math.IsNaN(v) && !math.IsNaN(w) {
 			return false
 		}
 	}

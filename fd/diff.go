d55939055a476d2c4e7924e6050ba11b793f86f9
btracey <bdtinternet@gmail.com>
2014-10-14 16:46:15 -0700

    Added Gradient method and tests.
    Gradient computes finite differences of vector-valued function using finite difference

--- fd/diff.go
+++ fd/diff.go
@@ -2,7 +2,10 @@ package diff
 
 import (
 	"math"
+	"runtime"
 	"sync"
+
+	"github.com/gonum/floats"
 )
 
 // A Point is a stencil location in a difference method
@@ -26,6 +29,7 @@ type Settings struct {
 	OriginValue float64 // Value at the origin (only used if OriginKnown is true)
 	Step        float64 // step size
 	Concurrent  bool    // Should the function calls be executed concurrently
+	Workers     int     // Maximum number of concurrent executions when evaluating concurrently
 	Method      Method  // Finite difference method to use
 }
 
@@ -34,8 +38,9 @@ type Settings struct {
 // of the function.
 func DefaultSettings() *Settings {
 	return &Settings{
-		Step:   1e-6,
-		Method: Central,
+		Step:    1e-6,
+		Method:  Central,
+		Workers: runtime.GOMAXPROCS(0),
 	}
 }
 
@@ -77,6 +82,103 @@ func Derivative(f func(float64) float64, x float64, settings *Settings) float64 
 	}
 	wg.Wait()
 	return deriv / math.Pow(settings.Step, float64(method.Order))
+}
+
+// Gradient estimates the derivative of a vector-valued function f at the location
+// x. The resulting estimate is stored in-place into deriv. The order of derivative,
+// sample locations, and other options are specified by settings. Gradient panics
+// if len(deriv) != len(x).
+func Gradient(f func([]float64) float64, x []float64, settings *Settings, gradient []float64) {
+	if len(gradient) != len(x) {
+		panic("fd: location and gradient length mismatch")
+	}
+	if settings == nil {
+		settings = DefaultSettings()
+	}
+	if !settings.Concurrent {
+		xcopy := make([]float64, len(x)) // So that x is not modified during the call
+		copy(xcopy, x)
+		for i := range xcopy {
+			var deriv float64
+			for _, pt := range settings.Method.Stencil {
+				if settings.OriginKnown && pt.Loc == 0 {
+					deriv += pt.Coeff * settings.OriginValue
+					continue
+				}
+				xcopy[i] += pt.Loc * settings.Step
+				deriv += pt.Coeff * f(xcopy)
+				xcopy[i] -= pt.Loc * settings.Step
+			}
+			gradient[i] = deriv / math.Pow(settings.Step, float64(settings.Method.Order))
+		}
+		return
+	}
+	quit := make(chan struct{})
+	sendChan := make(chan fdrun)
+	ansChan := make(chan fdrun)
+
+	nWorkers := settings.Workers
+	expect := len(settings.Method.Stencil) * len(x)
+	if nWorkers > expect {
+		nWorkers = expect
+	}
+
+	// Launch workers. Workers receive an index and a step, and compute the answer
+	for i := 0; i < settings.Workers; i++ {
+		go func() {
+			xcopy := make([]float64, len(x))
+			copy(xcopy, x)
+			for {
+				select {
+				case <-quit:
+					return
+				case run := <-sendChan:
+					xcopy[run.idx] += run.pt.Loc * settings.Step
+					run.result = f(xcopy)
+					xcopy[run.idx] -= run.pt.Loc * settings.Step
+					ansChan <- run
+				}
+			}
+		}()
+	}
+
+	// Launch the distributor. Distributor sends the cases to be computed
+	go func() {
+		for i := range x {
+			for _, pt := range settings.Method.Stencil {
+				if settings.OriginKnown && pt.Loc == 0 {
+					// Answer already known. Send the answer on the answer channel
+					ansChan <- fdrun{
+						idx:    i,
+						pt:     pt,
+						result: settings.OriginValue,
+					}
+					continue
+				}
+				// Answer not known, send the answer to be computed
+				sendChan <- fdrun{
+					idx: i,
+					pt:  pt,
+				}
+			}
+		}
+	}()
+	for i := range gradient {
+		gradient[i] = 0
+	}
+	// Read in all of the results
+	for i := 0; i < expect; i++ {
+		run := <-ansChan
+		gradient[run.idx] += run.pt.Coeff * run.result
+	}
+	floats.Scale(1/math.Pow(settings.Step, float64(settings.Method.Order)), gradient)
+	close(quit)
+}
+
+type fdrun struct {
+	idx    int
+	pt     Point
+	result float64
 }
 
 // Forward represents a first-order forward difference.
---
+++ fd/gradient_test.go
@@ -0,0 +1,130 @@
+package diff
+
+import (
+	"math"
+	"math/rand"
+	"testing"
+
+	"github.com/gonum/floats"
+)
+
+type Rosenbrock struct {
+	nDim int
+}
+
+func (r Rosenbrock) F(x []float64) (sum float64) {
+	deriv := make([]float64, len(x))
+	return r.FDf(x, deriv)
+}
+
+func (r Rosenbrock) FDf(x []float64, deriv []float64) (sum float64) {
+	sum = 0
+
+	for i := range deriv {
+		deriv[i] = 0
+	}
+
+	for i := 0; i < len(x)-1; i++ {
+		sum += math.Pow(1-x[i], 2) + 100*math.Pow(x[i+1]-math.Pow(x[i], 2), 2)
+	}
+	for i := 0; i < len(x)-1; i++ {
+		deriv[i] += -1 * 2 * (1 - x[i])
+		deriv[i] += 2 * 100 * (x[i+1] - math.Pow(x[i], 2)) * (-2 * x[i])
+	}
+	for i := 1; i < len(x); i++ {
+		deriv[i] += 2 * 100 * (x[i] - math.Pow(x[i-1], 2))
+	}
+
+	return sum
+}
+
+func TestGradient(t *testing.T) {
+	for i, test := range []struct {
+		nDim   int
+		tol    float64
+		method Method
+	}{
+		{
+			nDim:   2,
+			tol:    1e-4,
+			method: Forward,
+		},
+		{
+			nDim:   2,
+			tol:    1e-6,
+			method: Central,
+		},
+		{
+			nDim:   40,
+			tol:    1e-4,
+			method: Forward,
+		},
+		{
+			nDim:   40,
+			tol:    1e-6,
+			method: Central,
+		},
+	} {
+		x := make([]float64, test.nDim)
+		for i := range x {
+			x[i] = rand.Float64()
+		}
+		xcopy := make([]float64, len(x))
+		copy(xcopy, x)
+
+		r := Rosenbrock{len(x)}
+		trueGradient := make([]float64, len(x))
+		r.FDf(x, trueGradient)
+
+		settings := DefaultSettings()
+		settings.Method = test.method
+		gradient := make([]float64, len(x))
+		for i := range gradient {
+			gradient[i] = rand.Float64()
+		}
+
+		Gradient(r.F, x, settings, gradient)
+		if !floats.EqualApprox(gradient, trueGradient, test.tol) {
+			t.Errorf("Case %v: gradient mismatch in serial. Want: %v, Got: %v.", i, trueGradient, gradient)
+		}
+		if !floats.Equal(x, xcopy) {
+			t.Errorf("Case %v: x modified during call to gradient in serial")
+		}
+
+		// Try with known value
+		for i := range gradient {
+			gradient[i] = rand.Float64()
+		}
+		settings.OriginKnown = true
+		settings.OriginValue = r.F(x)
+		Gradient(r.F, x, settings, gradient)
+		if !floats.EqualApprox(gradient, trueGradient, test.tol) {
+			t.Errorf("Case %v: gradient mismatch with known origin in serial. Want: %v, Got: %v.", i, trueGradient, gradient)
+		}
+
+		// Concurrently
+		for i := range gradient {
+			gradient[i] = rand.Float64()
+		}
+		settings.Concurrent = true
+		settings.OriginKnown = false
+		Gradient(r.F, x, settings, gradient)
+		if !floats.EqualApprox(gradient, trueGradient, test.tol) {
+			t.Errorf("Case %v: gradient mismatch with unknown origin in parallel. Want: %v, Got: %v.", i, trueGradient, gradient)
+		}
+		if !floats.Equal(x, xcopy) {
+			t.Errorf("Case %v: x modified during call to gradient in parallel")
+		}
+
+		// Concurrently with origin known
+		for i := range gradient {
+			gradient[i] = rand.Float64()
+		}
+		settings.OriginKnown = true
+		Gradient(r.F, x, settings, gradient)
+		if !floats.EqualApprox(gradient, trueGradient, test.tol) {
+			t.Errorf("Case %v: gradient mismatch with known origin in parallel. Want: %v, Got: %v.", i, trueGradient, gradient)
+		}
+
+	}
+}

e2ba9707219382fdd975101d5023f4e9384eac2b
btracey <bdtinternet@gmail.com>
2014-10-15 00:09:37 -0700

    Fixed package name and added file headers

--- fd/gradient_test.go
+++ fd/gradient_test.go
@@ -1,4 +1,8 @@
-package diff
+// Copyright ©2014 The gonum Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package fd
 
 import (
 	"math"

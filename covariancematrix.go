6a930548691e1640c7c0460b68ef46097579ea17
Jonathan J Lawlor <jonathan.lawlor@gmail.com>
2014-11-15 01:18:45 -0500

    run go fmt
    oops

--- covariancematrix.go
+++ covariancematrix.go
@@ -11,27 +11,27 @@ import (
 // CovarianceMatrix calculates a covariance matrix (also known as a
 // variance-covariance matrix) from a matrix of data.
 func CovarianceMatrix(x mat64.Matrix) *mat64.Dense {
-	
+
 	// matrix version of the two pass algorithm.  This doesn't use
 	// the correction found in the Covariance and Variance functions.
-	
+
 	r, _ := x.Dims()
 	b := ones(1, r)
 	b.Mul(b, x)
 	b.Scale(1/float64(r), b)
-	
+
 	// todo: avoid unneeded memory expansion here.
 	mu := new(mat64.Dense)
-	mu.Mul(ones(r,1),b)
-	
+	mu.Mul(ones(r, 1), b)
+
 	// this could also be done with a clone & row viewer
 	xc := mat64.DenseCopyOf(x)
 	xc.Sub(xc, mu)
-	
+
 	// todo: avoid matrix copy
 	xt := new(mat64.Dense)
 	xt.TCopy(xc)
-	
+
 	ss := new(mat64.Dense)
 	ss.Mul(xt, xc)
 	ss.Scale(1/float64(r-1), ss)

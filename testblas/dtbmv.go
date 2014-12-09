9e8f10fdfa6f5efd65967878c303075a10cccaef
btracey <bdtinternet@gmail.com>
2014-12-08 23:55:57 -0800

    Removed duplicate functions

--- testblas/dtbmv.go
+++ testblas/dtbmv.go
@@ -1,7 +1,6 @@
 package testblas
 
 import (
-	"math"
 	"testing"
 
 	"github.com/gonum/blas"
@@ -121,79 +120,4 @@ func DtbmvTest(t *testing.T, blasser Dtbmver) {
 		incTest(3, extra)
 		incTest(-2, extra)
 	}
-}
-
-// flattenBanded turns a dense banded slice of slice into the compact banded matrix format
-func flattenBanded(a [][]float64, ku, kl int) []float64 {
-	m := len(a)
-	n := len(a[0])
-	if ku < 0 || kl < 0 {
-		panic("testblas: negative band length")
-	}
-	// banded size is minimum of m and n because otherwise just have a bunch of zeros
-	nRows := m
-	if m < n {
-		nRows = n
-	}
-	nCols := (ku + kl + 1)
-	aflat := make([]float64, nRows*nCols)
-	for i := range aflat {
-		aflat[i] = math.NaN()
-	}
-	// loop over the rows, and then the bands
-	// elements in the ith row stay in the ith row
-	// order in bands is kept
-	for i := 0; i < nRows; i++ {
-		min := -kl
-		if i-kl < 0 {
-			min = -i
-		}
-		max := ku
-		if i+ku >= n {
-			max = n - i - 1
-		}
-		for j := min; j <= max; j++ {
-			col := kl + j
-			aflat[i*nCols+col] = a[i][i+j]
-		}
-	}
-	return aflat
-}
-
-// makeIncremented takes a slice with inc == 1 and makes an incremented version
-// and adds extra values on the end
-func makeIncremented(x []float64, inc int, extra int) []float64 {
-	if inc == 0 {
-		panic("zero inc")
-	}
-	absinc := inc
-	if absinc < 0 {
-		absinc = -inc
-	}
-	xcopy := make([]float64, len(x))
-	if inc > 0 {
-		copy(xcopy, x)
-	} else {
-		for i := 0; i < len(x); i++ {
-			xcopy[i] = x[len(x)-i-1]
-		}
-	}
-	// don't use NaN because it makes comparison hard
-	// Do use a weird unique value for easier debugging
-	counter := 100.0
-	var xnew []float64
-	for i, v := range xcopy {
-		xnew = append(xnew, v)
-		if i != len(x)-1 {
-			for j := 0; j < absinc-1; j++ {
-				xnew = append(xnew, counter)
-				counter++
-			}
-		}
-	}
-	for i := 0; i < extra; i++ {
-		xnew = append(xnew, counter)
-		counter++
-	}
-	return xnew
 }

d6b458162d87bfaf6d92ca3ed90cfd1f8ee37775
btracey <bdtinternet@gmail.com>
2014-12-29 22:36:34 -0500

    Added benchmark for Dger

--- testblas/level2bench.go
+++ testblas/level2bench.go
@@ -36,3 +36,26 @@ func DgemvBenchmark(b *testing.B, blasser Dgemver, tA blas.Transpose, m, n, incX
 		blasser.Dgemv(tA, m, n, 2, a, n, x, incX, 3, y, incY)
 	}
 }
+
+func DgerBenchmark(b *testing.B, blasser Dgerer, m, n, incX, incY int) {
+	xr := make([]float64, m)
+	for i := range xr {
+		xr[i] = rand.Float64()
+	}
+	x := makeIncremented(xr, incX, 0)
+	yr := make([]float64, n)
+	for i := range yr {
+		yr[i] = rand.Float64()
+	}
+	y := makeIncremented(yr, incY, 0)
+	a := make([]float64, m*n)
+	for i := range a {
+		a[i] = rand.Float64()
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		//blasser.Dgemv(tA, m, n, 2, a, n, x, incX, 3, y, incY)
+		blasser.Dger(m, n, 2, x, incX, y, incY, a, n)
+	}
+}

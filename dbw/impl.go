49feb413cb107d8b84d8585b492137b3699e24f9
Fazlul Shahriar <fshahriar@gmail.com>
2015-01-02 02:08:14 -0500

    dbw: fix typo in synopsis

--- dbw/impl.go
+++ dbw/impl.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package zbw is a wrapper for a blas.Float64 implementation.
+// Package dbw is a wrapper for a blas.Float64 implementation.
 package dbw
 
 import "github.com/gonum/blas"

cdd12619e7ac7d3e7af64637e75444129ed347f8
Fazlul Shahriar <fshahriar@gmail.com>
2015-01-03 08:51:22 -0500

    all: tweak package synopses text

--- cblas/blas.go
+++ cblas/blas.go
@@ -4,8 +4,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package cblas is a binding to a C BLAS library.
-// It implements the blas interfaces.
+// Package cblas provides bindings to a C BLAS library.
 package cblas
 
 /*
--- testblas/level1double.go
+++ testblas/level1double.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package testblas tests a blas implementation.
+// Package testblas provides tests for blas implementions.
 package testblas
 
 import (

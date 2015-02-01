f654bb78ecf9411e36722c6eeaee528b6f7232bf
Jonathan J Lawlor <jonathan.lawlor@gmail.com>
2015-02-01 12:48:57 -0500

    Add CorrelationMatrix and Cov2Corr functions
    This change includes a function to calculate correlation the
    correlation matrix of input data, and unexported functions which
    can convert between covariance matrices and correlation matrices.
    There are also tests for CorrelationMatrix, and benchmarks for
    the conversion functions.

--- covariancematrix.go
+++ covariancematrix.go
@@ -12,13 +12,13 @@ import (
 
 // CovarianceMatrix calculates a covariance matrix (also known as a
 // variance-covariance matrix) from a matrix of data, using a two-pass
-// algorithm. The matrix returned will be symmetric, square, and
-// positive-semidefinite.
+// algorithm. The matrix returned will be symmetric and square.
 //
 // The weights wts should have the length equal to the number of rows in
-// input data matrix x. cov should either be a square matrix with the same
-// number of columns as the input data matrix x, or nil in which case a new
-// Dense matrix will be constructed.  Weights cannot be negative.
+// input data matrix x. If c is nil, then a new matrix with appropriate size will
+// be constructed.  If c is not nil, it should be a square matrix with the same
+// number of columns as the input data matrix x, and it will be used as the receiver
+// for the covariance data.  Weights cannot be negative.
 func CovarianceMatrix(cov *mat64.Dense, x mat64.Matrix, wts []float64) *mat64.Dense {
 	// This is the matrix version of the two-pass algorithm. It doesn't use the
 	// additional floating point error correction that the Covariance function uses
@@ -79,4 +79,76 @@ func CovarianceMatrix(cov *mat64.Dense, x mat64.Matrix, wts []float64) *mat64.De
 	// Scale by the sample size.
 	cov.Scale(1/(n-1), cov)
 	return cov
+}
+
+// CorrelationMatrix calculates a correlation matrix from a matrix of data,
+// using a two-pass algorithm. The matrix returned will be symmetric and square.
+//
+// The weights wts should have the length equal to the number of rows in
+// input data matrix x. If c is nil, then a new matrix with appropriate size will
+// be constructed.  If c is not nil, it should be a square matrix with the same
+// number of columns as the input data matrix x, and it will be used as the receiver
+// for the correlation data.  Weights cannot be negative.
+func CorrelationMatrix(c *mat64.Dense, x mat64.Matrix, wts []float64) *mat64.Dense {
+
+	// TODO(jonlawlor): indicate that the resulting matrix is symmetric, and change
+	// the returned type from a *mat.Dense to a *mat.Symmetric.
+
+	// This will panic if the sizes don't match, or if wts is the wrong size.
+	c = CovarianceMatrix(c, x, wts)
+	covToCorr(c)
+	return c
+}
+
+// covToCorr converts a covariance matrix to a correlation matrix.
+func covToCorr(c *mat64.Dense) {
+
+	// TODO(jonlawlor): use a *mat64.Symmetric as input.
+
+	r, _ := c.Dims()
+
+	s := make([]float64, r)
+	for i := 0; i < r; i++ {
+		s[i] = 1 / math.Sqrt(c.At(i, i))
+	}
+	for i, sx := range s {
+		row := c.RawRowView(i)
+		for j, sy := range s {
+			if i == j {
+				// Ensure that the diagonal has exactly ones.
+				row[j] = 1
+				continue
+			}
+			row[j] *= sx
+			row[j] *= sy
+		}
+	}
+}
+
+// corrToCov converts a correlation matrix to a covariance matrix.
+// The input sigma should be vector of standard deviations corresponding
+// to the covariance.  It will panic if len(sigma) is not equal to the
+// number of rows in the correlation matrix.
+func corrToCov(c *mat64.Dense, sigma []float64) {
+
+	// TODO(jonlawlor): use a *mat64.Symmetric as input.
+
+	r, _ := c.Dims()
+
+	if r != len(sigma) {
+		panic(mat64.ErrShape)
+	}
+
+	for i, sx := range sigma {
+		row := c.RawRowView(i)
+		for j, sy := range sigma {
+			if i == j {
+				// Ensure that the diagonal has exactly sigma squared.
+				row[j] = sx * sx
+				continue
+			}
+			row[j] *= sx
+			row[j] *= sy
+		}
+	}
 }

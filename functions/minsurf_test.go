9396f31e0f72e8659191aba304552df10ed7f53f
Vladimir Chalupecky <vladimir.chalupecky@gmail.com>
2015-02-10 09:09:12 +0900

    Add MinimalSurface function from MINPACK-2 test problem collection

---
+++ functions/minsurf_test.go
@@ -0,0 +1,48 @@
+// Copyright ©2015 The gonum Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package functions
+
+import (
+	"math"
+	"testing"
+
+	"github.com/gonum/diff/fd"
+	"github.com/gonum/floats"
+)
+
+func TestMinimalSurface(t *testing.T) {
+	for _, size := range [][2]int{
+		{20, 30},
+		{30, 30},
+		{50, 40},
+	} {
+		f := NewMinimalSurface(size[0], size[1])
+		x0 := f.InitX()
+		grad := make([]float64, len(x0))
+		f.FDf(x0, grad)
+		fdGrad := fd.Gradient(nil, f.F, x0, nil)
+
+		// Test that the numerical and analytical gradients agree.
+		dist := floats.Distance(grad, fdGrad, math.Inf(1))
+		if dist > 1e-9 {
+			t.Errorf("grid %v x %v: numerical and analytical gradient do not match. |fdGrad - grad|_∞ = %v",
+				size[0], size[1], dist)
+		}
+
+		// Test that the gradient at the minimum is small enough.
+		// In some sense this test is not completely correct because ExactX()
+		// is the exact solution to the continuous problem projected on the
+		// grid, not the exact solution to the discrete problem which we are
+		// solving. This is the reason why a relatively loose tolerance 1e-4
+		// must be used.
+		xSol := f.ExactX()
+		f.FDf(xSol, grad)
+		norm := floats.Norm(grad, math.Inf(1))
+		if norm > 1e-4 {
+			t.Errorf("grid %v x %v: gradient at the minimum not small enough. |grad|_∞ = %v",
+				size[0], size[1], norm)
+		}
+	}
+}

4deab5d4ba5c9a7bda72b5da4d12b23580d880c1
Vladimir Chalupecky <vladimir.chalupecky@gmail.com>
2015-03-09 15:42:23 +0900

    Allocate Location based on what Method needs

--- local.go
+++ local.go
@@ -83,7 +83,7 @@ func Local(f Function, initX []float64, settings *Settings, method Method) (*Res
 	}
 
 	stats := &Stats{}
-	optLoc, evalType, err := getStartingLocation(funcInfo, initX, stats, settings)
+	optLoc, evalType, err := getStartingLocation(funcInfo, method, initX, stats, settings)
 	if err != nil {
 		return nil, err
 	}
@@ -213,15 +213,16 @@ func getDefaultMethod(funcInfo *functionInfo) Method {
 }
 
 // getStartingLocation allocates and initializes the starting location for the minimization.
-func getStartingLocation(funcInfo *functionInfo, initX []float64, stats *Stats, settings *Settings) (*Location, EvaluationType, error) {
+func getStartingLocation(funcInfo *functionInfo, method Method, initX []float64, stats *Stats, settings *Settings) (*Location, EvaluationType, error) {
 	dim := len(initX)
 	loc := &Location{
 		X: make([]float64, dim),
 	}
-	if funcInfo.IsGradient || funcInfo.IsFunctionGradient {
+	copy(loc.X, initX)
+	if method.Needs().Gradient {
 		loc.Gradient = make([]float64, dim)
 	}
-	copy(loc.X, initX)
+	// TODO(vladimir-ch): Allocate loc.Hessian when it is added to Location.
 
 	evalType := NoEvaluation
 	if settings.UseInitialData {

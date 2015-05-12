250118fbe7cd33d28718dad4129e93ab9feeb7a7
kortschak <dan.kortschak@adelaide.edu.au>
2015-05-12 10:59:46 +0930

    internal: fix doc comments

--- internal/set.go
+++ internal/set.go
@@ -4,29 +4,29 @@
 
 package internal
 
-// A set is a set of integer identifiers.
+// IntSet is a set of integer identifiers.
 type IntSet map[int]struct{}
 
 // The simple accessor methods for Set are provided to allow ease of
 // implementation change should the need arise.
 
-// add inserts an element into the set.
+// Add inserts an element into the set.
 func (s IntSet) Add(e int) {
 	s[e] = struct{}{}
 }
 
-// has reports the existence of the element in the set.
+// Has reports the existence of the element in the set.
 func (s IntSet) Has(e int) bool {
 	_, ok := s[e]
 	return ok
 }
 
-// remove deletes the specified element from the set.
+// Remove deletes the specified element from the set.
 func (s IntSet) Remove(e int) {
 	delete(s, e)
 }
 
-// count reports the number of elements stored in the set.
+// Count reports the number of elements stored in the set.
 func (s IntSet) Count() int {
 	return len(s)
 }

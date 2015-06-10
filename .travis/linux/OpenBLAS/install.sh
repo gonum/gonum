24ef5e83d76df7e7752fce6b7eb1137987edef3a
Jonathan J Lawlor <jonathan.lawlor@gmail.com>
2015-06-09 21:00:10 -0400

    cleanup .travis.yml and associated files
    As explained in
    https://groups.google.com/d/msg/gonum-dev/GKJjAtDxR9I/n0FD7WvvN5AJ
    
    Removed notifications and go tip, simplified code, added some comments,
    put all travis related code (except for .travis.yml) into .travis/

---
+++ .travis/linux/OpenBLAS/install.sh
@@ -0,0 +1,20 @@
+sudo apt-get update -qq
+sudo apt-get install -qq gfortran
+pushd ~
+sudo git clone --depth=1 git://github.com/xianyi/OpenBLAS
+pushd OpenBLAS
+sudo make FC=gfortran &> /dev/null
+sudo make PREFIX=/usr install
+popd
+curl http://www.netlib.org/blas/blast-forum/cblas.tgz | tar -zx
+pushd CBLAS
+sudo mv Makefile.LINUX Makefile.in
+sudo BLLIB=/usr/lib/libopenblas.a make alllib
+sudo mv lib/cblas_LINUX.a /usr/lib/libcblas.a
+popd
+popd
+export CGO_LDFLAGS="-L/usr/lib -lopenblas"
+go get github.com/gonum/blas
+pushd cgo
+go install -v -x
+popd

9351b1852e1ac52f2aee5078763e9f4bb1ddae3d
btracey <tracey.brendan@gmail.com>
2015-06-25 16:01:47 -0500

    Added dlarf, tests, some auxiliary routines, and missing license headers
    Added dlarf, tests, some auxiliary routines, and missing license headers
    
    PR comments

--- native/lapack_test.go
+++ native/lapack_test.go
@@ -1,3 +1,7 @@
+// Copyright ©2015 The gonum Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package native
 
 import (
@@ -8,10 +12,22 @@ import (
 
 var impl = Implementation{}
 
+func TestDlarf(t *testing.T) {
+	testlapack.DlarfTest(t, impl)
+}
+
 func TestDpotf2(t *testing.T) {
 	testlapack.Dpotf2Test(t, impl)
 }
 
 func TestDpotrf(t *testing.T) {
 	testlapack.DpotrfTest(t, impl)
+}
+
+func TestIladlc(t *testing.T) {
+	testlapack.IladlcTest(t, impl)
+}
+
+func TestIladlr(t *testing.T) {
+	testlapack.IladlrTest(t, impl)
 }
---
+++ testlapack/general.go
@@ -0,0 +1,12 @@
+// Copyright ©2015 The gonum Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package testlapack
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}

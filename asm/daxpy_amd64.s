4a8e055096a77730bb21fda6524a7cf4bf4e1470
Vladimir Chalupecky <vladimir.chalupecky@gmail.com>
2015-12-11 10:45:29 +0900

    Run asmfmt on daxpy_amd64.s

--- asm/daxpy_amd64.s
+++ asm/daxpy_amd64.s
@@ -7,11 +7,11 @@
 // which is distributed under these terms:
 //
 // Copyright (c) 2012 The Go Authors. All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
-// 
+//
 //    * Redistributions of source code must retain the above copyright
 // notice, this list of conditions and the following disclaimer.
 //    * Redistributions in binary form must reproduce the above
@@ -21,7 +21,7 @@
 //    * Neither the name of Google Inc. nor the names of its
 // contributors may be used to endorse or promote products derived from
 // this software without specific prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 // "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 // LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -37,41 +37,40 @@
 //+build !noasm,!appengine
 
 // TODO(fhs): use textflag.h after we drop Go 1.3 support
-//#include "textflag.h"
+// #include "textflag.h"
 // Don't insert stack check preamble.
 #define NOSPLIT	4
 
-
 // func DaxpyUnitary(alpha float64, x, y, z []float64)
 // This function assumes len(y) >= len(x).
-TEXT ·DaxpyUnitary(SB),NOSPLIT,$0
+TEXT ·DaxpyUnitary(SB), NOSPLIT, $0
 	MOVHPD alpha+0(FP), X7
 	MOVLPD alpha+0(FP), X7
-	MOVQ x_len+16(FP), DI	// n = len(x)
-	MOVQ x+8(FP), R8
-	MOVQ y+32(FP), R9
-	MOVQ z+56(FP), R10
-	
-	MOVQ $0, SI				// i = 0
-	SUBQ $2, DI				// n -= 2
-	JL V1					// if n < 0 goto V1
+	MOVQ   x_len+16(FP), DI // n = len(x)
+	MOVQ   x+8(FP), R8
+	MOVQ   y+32(FP), R9
+	MOVQ   z+56(FP), R10
 
-U1:	// n >= 0
+	MOVQ $0, SI // i = 0
+	SUBQ $2, DI // n -= 2
+	JL   V1     // if n < 0 goto V1
+
+U1:  // n >= 0
 	// y[i] += alpha * x[i] unrolled 2x.
 	MOVUPD 0(R8)(SI*8), X0
 	MOVUPD 0(R9)(SI*8), X1
-	MULPD X7, X0
-	ADDPD X0, X1
+	MULPD  X7, X0
+	ADDPD  X0, X1
 	MOVUPD X1, 0(R10)(SI*8)
-	
-	ADDQ $2, SI				// i += 2
-	SUBQ $2, DI				// n -= 2
-	JGE U1					// if n >= 0 goto U1
+
+	ADDQ $2, SI // i += 2
+	SUBQ $2, DI // n -= 2
+	JGE  U1     // if n >= 0 goto U1
 
 V1:
-	ADDQ $2, DI				// n += 2
-	JLE E1					// if n <= 0 goto E1
-	
+	ADDQ $2, DI // n += 2
+	JLE  E1     // if n <= 0 goto E1
+
 	// y[i] += alpha * x[i] for last iteration if n is odd.
 	MOVSD 0(R8)(SI*8), X0
 	MOVSD 0(R9)(SI*8), X1
@@ -82,53 +81,52 @@ V1:
 E1:
 	RET
 
-
 // func DaxpyInc(alpha float64, x, y []float64, n, incX, incY, ix, iy uintptr)
-TEXT ·DaxpyInc(SB),NOSPLIT,$0
+TEXT ·DaxpyInc(SB), NOSPLIT, $0
 	MOVHPD alpha+0(FP), X7
 	MOVLPD alpha+0(FP), X7
-	MOVQ x+8(FP), R8
-	MOVQ y+32(FP), R9
-	MOVQ n+56(FP), CX
-	MOVQ incX+64(FP), R11
-	MOVQ incY+72(FP), R12
-	MOVQ ix+80(FP), SI
-	MOVQ iy+88(FP), DI
+	MOVQ   x+8(FP), R8
+	MOVQ   y+32(FP), R9
+	MOVQ   n+56(FP), CX
+	MOVQ   incX+64(FP), R11
+	MOVQ   incY+72(FP), R12
+	MOVQ   ix+80(FP), SI
+	MOVQ   iy+88(FP), DI
 
-	MOVQ SI, AX				// nextX = ix
-	MOVQ DI, BX				// nextY = iy
-	ADDQ R11, AX			// nextX += incX
-	ADDQ R12, BX			// nextY += incX
-	SHLQ $1, R11			// indX *= 2
-	SHLQ $1, R12			// indY *= 2
-	
-	SUBQ $2, CX				// n -= 2
-	JL V2					// if n < 0 goto V2
+	MOVQ SI, AX  // nextX = ix
+	MOVQ DI, BX  // nextY = iy
+	ADDQ R11, AX // nextX += incX
+	ADDQ R12, BX // nextY += incX
+	SHLQ $1, R11 // indX *= 2
+	SHLQ $1, R12 // indY *= 2
 
-U2:	// n >= 0
+	SUBQ $2, CX // n -= 2
+	JL   V2     // if n < 0 goto V2
+
+U2:  // n >= 0
 	// y[i] += alpha * x[i] unrolled 2x.
 	MOVHPD 0(R8)(SI*8), X0
 	MOVHPD 0(R9)(DI*8), X1
 	MOVLPD 0(R8)(AX*8), X0
 	MOVLPD 0(R9)(BX*8), X1
-	
-	MULPD X7, X0
-	ADDPD X0, X1
+
+	MULPD  X7, X0
+	ADDPD  X0, X1
 	MOVHPD X1, 0(R9)(DI*8)
 	MOVLPD X1, 0(R9)(BX*8)
 
-	ADDQ R11, SI			// ix += incX
-	ADDQ R12, DI			// iy += incY
-	ADDQ R11, AX			// nextX += incX
-	ADDQ R12, BX			// nextY += incY
+	ADDQ R11, SI // ix += incX
+	ADDQ R12, DI // iy += incY
+	ADDQ R11, AX // nextX += incX
+	ADDQ R12, BX // nextY += incY
 
-	SUBQ $2, CX				// n -= 2
-	JGE U2					// if n >= 0 goto U2
+	SUBQ $2, CX // n -= 2
+	JGE  U2     // if n >= 0 goto U2
 
 V2:
-	ADDQ $2, CX				// n += 2
-	JLE E2					// if n <= 0 goto E2
-	
+	ADDQ $2, CX // n += 2
+	JLE  E2     // if n <= 0 goto E2
+
 	// y[i] += alpha * x[i] for the last iteration if n is odd.
 	MOVSD 0(R8)(SI*8), X0
 	MOVSD 0(R9)(DI*8), X1

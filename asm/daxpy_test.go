ea8555afe2880e4596118e1004de4f89a6f83a4b
Vladimir Chalupecky <vladimir.chalupecky@gmail.com>
2015-12-15 11:25:05 +0900

    asm: add tests for DaxpyIncTo

--- asm/daxpy_test.go
+++ asm/daxpy_test.go
@@ -261,6 +261,204 @@ func TestDaxpyInc(t *testing.T) {
 	}
 }
 
+func TestDaxpyIncTo(t *testing.T) {
+	for i, test := range []struct {
+		alpha float64
+		xData []float64
+		yData []float64
+
+		want    []float64
+		wantRev []float64 // Result when one vector is traversed in reverse direction.
+	}{
+		{
+			alpha:   0,
+			xData:   []float64{2},
+			yData:   []float64{-3},
+			want:    []float64{-3},
+			wantRev: []float64{-3},
+		},
+		{
+			alpha:   1,
+			xData:   []float64{2},
+			yData:   []float64{-3},
+			want:    []float64{-1},
+			wantRev: []float64{-1},
+		},
+		{
+			alpha:   3,
+			xData:   []float64{2},
+			yData:   []float64{-3},
+			want:    []float64{3},
+			wantRev: []float64{3},
+		},
+		{
+			alpha:   -3,
+			xData:   []float64{2},
+			yData:   []float64{-3},
+			want:    []float64{-9},
+			wantRev: []float64{-9},
+		},
+		{
+			alpha:   0,
+			xData:   []float64{0, 0, 1, 1, 2, -3, -4},
+			yData:   []float64{0, 1, 0, 3, -4, 5, -6},
+			want:    []float64{0, 1, 0, 3, -4, 5, -6},
+			wantRev: []float64{0, 1, 0, 3, -4, 5, -6},
+		},
+		{
+			alpha:   1,
+			xData:   []float64{0, 0, 1, 1, 2, -3, -4},
+			yData:   []float64{0, 1, 0, 3, -4, 5, -6},
+			want:    []float64{0, 1, 1, 4, -2, 2, -10},
+			wantRev: []float64{-4, -2, 2, 4, -3, 5, -6},
+		},
+		{
+			alpha:   3,
+			xData:   []float64{0, 0, 1, 1, 2, -3, -4},
+			yData:   []float64{0, 1, 0, 3, -4, 5, -6},
+			want:    []float64{0, 1, 3, 6, 2, -4, -18},
+			wantRev: []float64{-12, -8, 6, 6, -1, 5, -6},
+		},
+		{
+			alpha:   -3,
+			xData:   []float64{0, 0, 1, 1, 2, -3, -4},
+			yData:   []float64{0, 1, 0, 3, -4, 5, -6},
+			want:    []float64{0, 1, -3, 0, -10, 14, 6},
+			wantRev: []float64{12, 10, -6, 0, -7, 5, -6},
+		},
+		{
+			alpha:   -5,
+			xData:   []float64{0, 0, 1, 1, 2, -3, -4, 5},
+			yData:   []float64{0, 1, 0, 3, -4, 5, -6, 7},
+			want:    []float64{0, 1, -5, -2, -14, 20, 14, -18},
+			wantRev: []float64{-25, 21, 15, -7, -9, 0, -6, 7},
+		},
+	} {
+		const msgGuard = "%v: out-of-bounds write to %v argument\nfront guard: %v\nback guard: %v"
+		n := len(test.xData)
+		want := make([]float64, n)
+
+		for _, incX := range []int{-7, -4, -3, -2, -1, 1, 2, 3, 4, 7} {
+			for _, incY := range []int{-7, -4, -3, -2, -1, 1, 2, 3, 4, 7} {
+				var ix, iy int
+				if incX < 0 {
+					ix = (-n + 1) * incX
+				}
+				if incY < 0 {
+					iy = (-n + 1) * incY
+				}
+
+				// Test y = alpha * x + y.
+				prefix := fmt.Sprintf("test %v (y=a*x+y), incX = %v, incY = %v", i, incX, incY)
+				x, xFront, xBack := newGuardedVector(test.xData, incX)
+				y, yFront, yBack := newGuardedVector(test.yData, incY)
+				DaxpyIncTo(y, uintptr(incY), uintptr(iy),
+					test.alpha, x, y,
+					uintptr(n), uintptr(incX), uintptr(incY), uintptr(ix), uintptr(iy))
+
+				if !allNaN(xFront) || !allNaN(xBack) {
+					t.Errorf(msgGuard, prefix, "x", xFront, xBack)
+				}
+				if !allNaN(yFront) || !allNaN(yBack) {
+					t.Errorf(msgGuard, prefix, "y", yFront, yBack)
+				}
+				if !equalStrided(test.xData, x, incX) {
+					t.Errorf("%v: modified read-only x argument", prefix)
+				}
+
+				if incX*incY < 0 {
+					copy(want, test.wantRev)
+				} else {
+					copy(want, test.want)
+				}
+				if !equalStrided(want, y, incY) {
+					t.Errorf("%v: unexpected result:\nwant: %v\ngot: %v", prefix, want, y)
+				}
+
+				// Test x = alpha * x + y.
+				prefix = fmt.Sprintf("test %v (x=a*x+y), incX = %v, incY = %v", i, incX, incY)
+				x, xFront, xBack = newGuardedVector(test.xData, incX)
+				y, yFront, yBack = newGuardedVector(test.yData, incY)
+
+				DaxpyIncTo(x, uintptr(incX), uintptr(ix),
+					test.alpha, x, y,
+					uintptr(n), uintptr(incX), uintptr(incY), uintptr(ix), uintptr(iy))
+
+				if !allNaN(xFront) || !allNaN(xBack) {
+					t.Errorf(msgGuard, prefix, "x", xFront, xBack)
+				}
+				if !allNaN(yFront) || !allNaN(yBack) {
+					t.Errorf(msgGuard, prefix, "y", yFront, yBack)
+				}
+				if !equalStrided(test.yData, y, incY) {
+					t.Errorf("%v: modified read-only y argument", prefix)
+				}
+
+				if incX*incY < 0 {
+					copy(want, test.wantRev)
+					for i := 0; i < n/2; i++ {
+						want[i], want[n-i-1] = want[n-i-1], want[i]
+					}
+				} else {
+					copy(want, test.want)
+				}
+				if !equalStrided(want, x, incX) {
+					t.Errorf("%v: unexpected result:\nwant: %v\ngot: %v", prefix, want, x)
+				}
+
+				for _, incDst := range []int{-7, -4, -3, -2, -1, 1, 2, 3, 4, 7} {
+					var idst int
+					if incDst < 0 {
+						idst = (-n + 1) * incDst
+					}
+
+					// Test dst = alpha * x + y.
+					prefix = fmt.Sprintf("test %v (dst=a*x+y), incX = %v, incY = %v, incDst =%v", i, incX, incY, incDst)
+					x, xFront, xBack = newGuardedVector(test.xData, incX)
+					y, yFront, yBack = newGuardedVector(test.yData, incY)
+					dst, dstFront, dstBack := newGuardedVector(test.xData, incDst)
+					DaxpyIncTo(dst, uintptr(incDst), uintptr(idst),
+						test.alpha, x, y,
+						uintptr(n), uintptr(incX), uintptr(incY), uintptr(ix), uintptr(iy))
+
+					if !allNaN(xFront) || !allNaN(xBack) {
+						t.Errorf(msgGuard, prefix, "x", xFront, xBack)
+					}
+					if !allNaN(yFront) || !allNaN(yBack) {
+						t.Errorf(msgGuard, prefix, "y", yFront, yBack)
+					}
+					if !allNaN(dstFront) || !allNaN(dstBack) {
+						t.Errorf(msgGuard, prefix, "dst", dstFront, dstBack)
+					}
+					if nonStridedWrite(x, incX) || !equalStrided(test.xData, x, incX) {
+						t.Errorf("%v: modified read-only x argument", prefix)
+					}
+					if nonStridedWrite(y, incY) || !equalStrided(test.yData, y, incY) {
+						t.Errorf("%v: modified read-only y argument", prefix)
+					}
+					if nonStridedWrite(dst, incDst) {
+						t.Errorf("%v: modified dst argument at non-stride position", prefix)
+					}
+
+					if incX*incY < 0 {
+						copy(want, test.wantRev)
+					} else {
+						copy(want, test.want)
+					}
+					if incY*incDst < 0 {
+						for i := 0; i < n/2; i++ {
+							want[i], want[n-i-1] = want[n-i-1], want[i]
+						}
+					}
+					if !equalStrided(want, dst, incDst) {
+						t.Errorf("%v: unexpected result:\nwant: %v\ngot: %v", prefix, want, dst)
+					}
+				}
+			}
+		}
+	}
+}
+
 func BenchmarkDaxpyUnitaryToN1(b *testing.B)      { daxpyUnitaryToBenchmark(b, 1) }
 func BenchmarkDaxpyUnitaryToN2(b *testing.B)      { daxpyUnitaryToBenchmark(b, 2) }
 func BenchmarkDaxpyUnitaryToN3(b *testing.B)      { daxpyUnitaryToBenchmark(b, 3) }

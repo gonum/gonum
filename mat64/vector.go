1cbf22d605c010ad7c40e33561605fad9665e1b9
Vladimir Chalupecky <vladimir.chalupecky@gmail.com>
2015-12-17 15:27:01 +0900

    mat64: use asm.DaxpyIncTo in Vector.AddScaledVec

--- mat64/vector.go
+++ mat64/vector.go
@@ -154,36 +154,28 @@ func (v *Vector) AddScaledVec(a *Vector, alpha float64, b *Vector) {
 
 	v.reuseAs(ar)
 
-	if alpha == 0 {
-		v.CopyVec(a)
-		return
-	}
-
 	switch {
+	case alpha == 0: // v <- a
+		v.CopyVec(a)
 	case v == a && v == b: // v <- v + alpha * v = (alpha + 1) * v
 		blas64.Scal(ar, alpha+1, v.mat)
 	case v == a && v != b: // v <- v + alpha * b
-		blas64.Axpy(ar, alpha, b.mat, v.mat)
-	case v != a && v == b: // v <- a + alpha * v
-		if v.mat.Inc == 1 && a.mat.Inc == 1 {
+		if v.mat.Inc == 1 && b.mat.Inc == 1 {
 			// Fast path for a common case.
-			v := v.mat.Data
-			for i, a := range a.mat.Data {
-				v[i] *= alpha
-				v[i] += a
-			}
-			return
+			asm.DaxpyUnitaryTo(v.mat.Data, alpha, b.mat.Data, a.mat.Data)
+		} else {
+			asm.DaxpyInc(alpha, b.mat.Data, v.mat.Data,
+				uintptr(ar), uintptr(b.mat.Inc), uintptr(v.mat.Inc), 0, 0)
 		}
-		blas64.Scal(ar, alpha, v.mat)
-		blas64.Axpy(ar, 1, a.mat, v.mat)
-	default: // v <- a + alpha * b
+	default: // v <- a + alpha * b or v <- a + alpha * v
 		if v.mat.Inc == 1 && a.mat.Inc == 1 && b.mat.Inc == 1 {
 			// Fast path for a common case.
 			asm.DaxpyUnitaryTo(v.mat.Data, alpha, b.mat.Data, a.mat.Data)
-			return
+		} else {
+			asm.DaxpyIncTo(v.mat.Data, uintptr(v.mat.Inc), 0,
+				alpha, b.mat.Data, a.mat.Data,
+				uintptr(ar), uintptr(b.mat.Inc), uintptr(a.mat.Inc), 0, 0)
 		}
-		blas64.Copy(ar, a.mat, v.mat)
-		blas64.Axpy(ar, alpha, b.mat, v.mat)
 	}
 }
 

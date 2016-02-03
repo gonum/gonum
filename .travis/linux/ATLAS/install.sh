55e3ef96fc325a8b7d4f13a10761d67f7c579fad
Jonathan J Lawlor <jonathan.lawlor@gmail.com>
2016-02-02 23:03:54 -0500

    pass through exit status on install commands in ATLAS
    same as in OpenBLAS

--- .travis/linux/ATLAS/install.sh
+++ .travis/linux/ATLAS/install.sh
@@ -1,5 +1,6 @@
-sudo apt-get update -qq
-sudo apt-get install -qq libatlas-base-dev
+sudo apt-get update -qq && sudo apt-get install -qq libatlas-base-dev
+if [ $? != 0 ]; then exit 1; fi
 
 export CGO_LDFLAGS="-L/usr/lib -lblas"
 go get github.com/gonum/blas
+if [ $? != 0 ]; then exit 1; fi

ec97589733e113bf9d67341aee241451bb25e7db
Jonathan J Lawlor <jonathan.lawlor@gmail.com>
2016-02-02 21:41:14 -0500

    pass through exit status from install cmds
    By checking the exit status from the various install commands (apt-get,
    git, curl, etc.) we can set the travis exit status to failure in the
    right spot, so that it is obvious when there is a network failure or
    other temporary problem.

--- .travis/linux/OpenBLAS/install.sh
+++ .travis/linux/OpenBLAS/install.sh
@@ -1,13 +1,15 @@
-sudo apt-get update -qq
-sudo apt-get install -qq gfortran
+sudo apt-get update -qq && sudo apt-get install -qq gfortran
+if [ $? != 0 ]; then exit 1; fi
 pushd ~
 sudo git clone --depth=1 git://github.com/xianyi/OpenBLAS
+if [ $? != 0 ]; then popd; exit 1; fi
 pushd OpenBLAS
 echo OpenBLAS $(git rev-parse HEAD)
-sudo make FC=gfortran &> /dev/null
-sudo make PREFIX=/usr install
+sudo make FC=gfortran &> /dev/null && sudo make PREFIX=/usr install
+if [ $? != 0 ]; then popd; popd; exit 1; fi
 popd
 curl http://www.netlib.org/blas/blast-forum/cblas.tgz | tar -zx
+if [ $? != 0 ]; then popd; exit 1; fi
 pushd CBLAS
 sudo mv Makefile.LINUX Makefile.in
 sudo BLLIB=/usr/lib/libopenblas.a make alllib
@@ -16,6 +18,8 @@ popd
 popd
 export CGO_LDFLAGS="-L/usr/lib -lopenblas"
 go get github.com/gonum/blas
+if [ $? != 0 ]; then exit 1; fi
 pushd cgo
 go install -v -x
+if [ $? != 0 ]; then popd; exit 1; fi
 popd

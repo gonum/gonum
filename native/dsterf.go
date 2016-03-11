1b1a9b3595580628531f96b7806bf84bc4643bf5
kortschak <dan.kortschak@adelaide.edu.au>
2016-03-11 14:26:17 +1030

    native: restructure dsterf.go part 1
    Remove gotos.

--- native/dsterf.go
+++ native/dsterf.go
@@ -60,236 +60,225 @@ func (impl Implementation) Dsterf(n int, d, e []float64) (ok bool) {
 	var iscale, l, lend, lendsv, lsv, m int
 	var el []float64
 
-	// TOOD(btracey): Replace these goto statements with imperative flow control
-	// structures.
-Ten:
-	if l1 > n-1 {
-		goto OneSeventy
-	}
-	if l1 > 0 {
-		e[l1-1] = 0
-	}
-	for m = l1; m < n-1; m++ {
-		if math.Abs(e[m]) <= math.Sqrt(math.Abs(d[m]))*math.Sqrt(math.Abs(d[m+1]))*eps {
-			e[m] = 0
-			goto Thirty
+	for {
+		if l1 > n-1 {
+			impl.Dlasrt(lapack.SortIncreasing, n, d)
+			return true
 		}
-	}
-	m = n - 1
+		if l1 > 0 {
+			e[l1-1] = 0
+		}
+		for m = l1; m < n-1; m++ {
+			if math.Abs(e[m]) <= math.Sqrt(math.Abs(d[m]))*math.Sqrt(math.Abs(d[m+1]))*eps {
+				e[m] = 0
+				break
+			}
+		}
 
-Thirty:
-	l = l1
-	lsv = l
-	lend = m
-	lendsv = lend
-	l1 = m + 1
-	if lend == 0 {
-		goto Ten
-	}
+		l = l1
+		lsv = l
+		lend = m
+		lendsv = lend
+		l1 = m + 1
+		if lend == 0 {
+			continue
+		}
 
-	// Scale submatrix in rows and columns l to lend.
-	anorm = impl.Dlanst(lapack.MaxAbs, lend-l+1, d[l:], e[l:])
-	iscale = none
-	if anorm == 0 {
-		goto Ten
-	}
-	if anorm > ssfmax {
-		iscale = down
-		impl.Dlascl(lapack.General, 0, 0, anorm, ssfmax, lend-l+1, 1, d[l:], n)
-		impl.Dlascl(lapack.General, 0, 0, anorm, ssfmax, lend-l, 1, e[l:], n)
-	} else if anorm < ssfmin {
-		iscale = up
-		impl.Dlascl(lapack.General, 0, 0, anorm, ssfmin, lend-l+1, 1, d[l:], n)
-		impl.Dlascl(lapack.General, 0, 0, anorm, ssfmin, lend-l, 1, e[l:], n)
-	}
+		// Scale submatrix in rows and columns l to lend.
+		anorm = impl.Dlanst(lapack.MaxAbs, lend-l+1, d[l:], e[l:])
+		iscale = none
+		if anorm == 0 {
+			continue
+		}
+		if anorm > ssfmax {
+			iscale = down
+			impl.Dlascl(lapack.General, 0, 0, anorm, ssfmax, lend-l+1, 1, d[l:], n)
+			impl.Dlascl(lapack.General, 0, 0, anorm, ssfmax, lend-l, 1, e[l:], n)
+		} else if anorm < ssfmin {
+			iscale = up
+			impl.Dlascl(lapack.General, 0, 0, anorm, ssfmin, lend-l+1, 1, d[l:], n)
+			impl.Dlascl(lapack.General, 0, 0, anorm, ssfmin, lend-l, 1, e[l:], n)
+		}
 
-	el = e[l:lend]
-	for i, v := range el {
-		el[i] *= v
-	}
+		el = e[l:lend]
+		for i, v := range el {
+			el[i] *= v
+		}
 
-	// Choose between QL and QR iteration.
-	if math.Abs(d[lend]) < math.Abs(d[l]) {
-		lend = lsv
-		l = lendsv
-	}
-	if lend >= l {
-		// QL Iteration.
-		// Look for small sub-diagonal element.
-	Fifty:
-		if l != lend {
-			for m = l; m < lend; m++ {
-				if math.Abs(e[m]) <= eps2*(math.Abs(d[m]*d[m+1])) {
-					goto Seventy
+		// Choose between QL and QR iteration.
+		if math.Abs(d[lend]) < math.Abs(d[l]) {
+			lend = lsv
+			l = lendsv
+		}
+		if lend >= l {
+			// QL Iteration.
+			// Look for small sub-diagonal element.
+			for {
+				if l != lend {
+					for m = l; m < lend; m++ {
+						if math.Abs(e[m]) <= eps2*(math.Abs(d[m]*d[m+1])) {
+							break
+						}
+					}
+				} else {
+					m = lend
 				}
+				if m < lend {
+					e[m] = 0
+				}
+				p := d[l]
+				if m == l {
+					// Eigenvalue found.
+					d[l] = p
+					l++
+					if l > lend {
+						break
+					}
+					continue
+				}
+				// If remaining matrix is 2 by 2, use Dlae2 to compute its eigenvalues.
+				if m == l+1 {
+					rte = math.Sqrt(e[l])
+					d[l], d[l+1] = impl.Dlae2(d[l], rte, d[l+1])
+					e[l] = 0
+					l += 2
+					if l > lend {
+						break
+					}
+					continue
+				}
+				if jtot == nmaxit {
+					break
+				}
+				jtot++
+
+				// Form shift.
+				rte = math.Sqrt(e[l])
+				sigma = (d[l+1] - p) / (2 * rte)
+				r = impl.Dlapy2(sigma, 1)
+				sigma = p - (rte / (sigma + math.Copysign(r, sigma)))
+
+				c = 1
+				s = 0
+				gamma = d[m] - sigma
+				p = gamma * gamma
+
+				// Inner loop.
+				for i := m - 1; i >= l; i-- {
+					bb := e[i]
+					r := p + bb
+					if i != m-1 {
+						e[i+1] = s * r
+					}
+					oldc := c
+					c = p / r
+					s = bb / r
+					oldgam := gamma
+					alpha := d[i]
+					gamma = c*(alpha-sigma) - s*oldgam
+					d[i+1] = oldgam + (alpha - gamma)
+					if c != 0 {
+						p = (gamma * gamma) / c
+					} else {
+						p = oldc * bb
+					}
+				}
+				e[l] = s * p
+				d[l] = sigma + gamma
+			}
+		} else {
+			for {
+				// QR Iteration.
+				// Look for small super-diagonal element.
+				for m = l; m > lend; m-- {
+					if math.Abs(e[m-1]) <= eps2*math.Abs(d[m]*d[m-1]) {
+						break
+					}
+				}
+				if m > lend {
+					e[m-1] = 0
+				}
+				p := d[l]
+				if m == l {
+					// Eigenvalue found.
+					d[l] = p
+					l--
+					if l < lend {
+						break
+					}
+					continue
+				}
+
+				// If remaining matrix is 2 by 2, use Dlae2 to compute its eigenvalues.
+				if m == l-1 {
+					rte = math.Sqrt(e[l-1])
+					d[l], d[l-1] = impl.Dlae2(d[l], rte, d[l-1])
+					e[l-1] = 0
+					l -= 2
+					if l < lend {
+						break
+					}
+					continue
+				}
+				if jtot == nmaxit {
+					break
+				}
+				jtot++
+
+				// Form shift.
+				rte = math.Sqrt(e[l-1])
+				sigma = (d[l-1] - p) / (2 * rte)
+				r = impl.Dlapy2(sigma, 1)
+				sigma = p - (rte / (sigma + math.Copysign(r, sigma)))
+
+				c = 1
+				s = 0
+				gamma = d[m] - sigma
+				p = gamma * gamma
+
+				// Inner loop.
+				for i := m; i < l; i++ {
+					bb := e[i]
+					r := p + bb
+					if i != m {
+						e[i-1] = s * r
+					}
+					oldc := c
+					c = p / r
+					s = bb / r
+					oldgam := gamma
+					alpha := d[i+1]
+					gamma = c*(alpha-sigma) - s*oldgam
+					d[i] = oldgam + alpha - gamma
+					if c != 0 {
+						p = (gamma * gamma) / c
+					} else {
+						p = oldc * bb
+					}
+				}
+				e[l-1] = s * p
+				d[l] = sigma + gamma
 			}
 		}
-		m = lend
-	Seventy:
-		if m < lend {
-			e[m] = 0
-		}
-		p := d[l]
-		if m == l {
-			goto Ninety
-		}
-		// If remaining matrix is 2 by 2, use Dlae2 to compute its eigenvalues.
-		if m == l+1 {
-			rte = math.Sqrt(e[l])
-			d[l], d[l+1] = impl.Dlae2(d[l], rte, d[l+1])
-			e[l] = 0
-			l += 2
-			if l <= lend {
-				goto Fifty
-			}
-			goto OneFifty
-		}
-		if jtot == nmaxit {
-			goto OneFifty
-		}
-		jtot++
 
-		// Form shift.
-		rte = math.Sqrt(e[l])
-		sigma = (d[l+1] - p) / (2 * rte)
-		r = impl.Dlapy2(sigma, 1)
-		sigma = p - (rte / (sigma + math.Copysign(r, sigma)))
-
-		c = 1
-		s = 0
-		gamma = d[m] - sigma
-		p = gamma * gamma
-
-		// Inner loop.
-		for i := m - 1; i >= l; i-- {
-			bb := e[i]
-			r := p + bb
-			if i != m-1 {
-				e[i+1] = s * r
-			}
-			oldc := c
-			c = p / r
-			s = bb / r
-			oldgam := gamma
-			alpha := d[i]
-			gamma = c*(alpha-sigma) - s*oldgam
-			d[i+1] = oldgam + (alpha - gamma)
-			if c != 0 {
-				p = (gamma * gamma) / c
-			} else {
-				p = oldc * bb
-			}
-		}
-		e[l] = s * p
-		d[l] = sigma + gamma
-		goto Fifty
-
-	Ninety:
-		// Eigenvalue found.
-		d[l] = p
-		l++
-		if l <= lend {
-			goto Fifty
-		}
-		goto OneFifty
-	} else {
-	OneHundred:
-		// QR Iteration.
-		// Look for small super-diagonal element.
-		for m = l; m >= lend+1; m-- {
-			if math.Abs(e[m-1]) <= eps2*math.Abs(d[m]*d[m-1]) {
-				goto OneTwenty
-			}
-		}
-		m = lend
-	OneTwenty:
-		if m > lend {
-			e[m-1] = 0
-		}
-		p := d[l]
-		if m == l {
-			goto OneFourty
+		// Undo scaling if necessary
+		switch iscale {
+		case down:
+			impl.Dlascl(lapack.General, 0, 0, ssfmax, anorm, lendsv-lsv+1, 1, d[lsv:], n)
+		case up:
+			impl.Dlascl(lapack.General, 0, 0, ssfmin, anorm, lendsv-lsv+1, 1, d[lsv:], n)
 		}
 
-		// If remaining matrix is 2 by 2, use Dlae2 to compute its eigenvalues.
-		if m == l-1 {
-			rte = math.Sqrt(e[l-1])
-			d[l], d[l-1] = impl.Dlae2(d[l], rte, d[l-1])
-			e[l-1] = 0
-			l -= 2
-			if l >= lend {
-				goto OneHundred
-			}
-			goto OneFifty
+		// Check for no convergence to an eigenvalue after a total of n*maxit iterations.
+		if jtot >= nmaxit {
+			break
 		}
-		if jtot == nmaxit {
-			goto OneFifty
-		}
-		jtot++
-
-		// Form shift.
-		rte = math.Sqrt(e[l-1])
-		sigma = (d[l-1] - p) / (2 * rte)
-		r = impl.Dlapy2(sigma, 1)
-		sigma = p - (rte / (sigma + math.Copysign(r, sigma)))
-
-		c = 1
-		s = 0
-		gamma = d[m] - sigma
-		p = gamma * gamma
-
-		// Inner loop.
-		for i := m; i < l; i++ {
-			bb := e[i]
-			r := p + bb
-			if i != m {
-				e[i-1] = s * r
-			}
-			oldc := c
-			c = p / r
-			s = bb / r
-			oldgam := gamma
-			alpha := d[i+1]
-			gamma = c*(alpha-sigma) - s*oldgam
-			d[i] = oldgam + alpha - gamma
-			if c != 0 {
-				p = (gamma * gamma) / c
-			} else {
-				p = oldc * bb
-			}
-		}
-		e[l-1] = s * p
-		d[l] = sigma + gamma
-		goto OneHundred
-
-	OneFourty:
-		// Eigenvalue found.
-		d[l] = p
-		l--
-		if l >= lend {
-			goto OneHundred
-		}
-		goto OneFifty
-	}
-OneFifty:
-	// Undo scaling if necessary
-	switch iscale {
-	case down:
-		impl.Dlascl(lapack.General, 0, 0, ssfmax, anorm, lendsv-lsv+1, 1, d[lsv:], n)
-	case up:
-		impl.Dlascl(lapack.General, 0, 0, ssfmin, anorm, lendsv-lsv+1, 1, d[lsv:], n)
-	}
-
-	// Check for no convergence to an eigenvalue after a total of n*maxit iterations.
-	if jtot < nmaxit {
-		goto Ten
 	}
 	for _, v := range e[:n-1] {
 		if v != 0 {
 			return false
 		}
 	}
-OneSeventy:
 	impl.Dlasrt(lapack.SortIncreasing, n, d)
 	return true
 }

92a6cbfa9483762391ff5316e2a2e6306f8892ee
Vladimir Chalupecky <vladimir.chalupecky@gmail.com>
2016-03-16 11:08:07 +0900

    fd: add package-level doc comment

--- fd/diff.go
+++ fd/diff.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Package fd provides functions to approximate derivatives using finite differences.
 package fd
 
 import (

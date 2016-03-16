ecece58210b25d7a4c0730e614f1cef4f2148508
Vladimir Chalupecky <vladimir.chalupecky@gmail.com>
2016-03-16 17:09:09 +0900

    fd: add example for Derivative

---
+++ fd/example_test.go
@@ -0,0 +1,47 @@
+// Copyright ©2016 The gonum Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package fd_test
+
+import (
+	"fmt"
+	"math"
+
+	"github.com/gonum/diff/fd"
+)
+
+func ExampleDerivative() {
+	f := func(x float64) float64 {
+		return math.Sin(x)
+	}
+	// Compute the first derivative of f at 0 using the default settings.
+	fmt.Println("f'(0) ≅", fd.Derivative(f, 0, nil))
+	// Compute the first derivative of f at 0 using the forward approximation
+	// with a custom step size.
+	df := fd.Derivative(f, 0, &fd.Settings{
+		Formula: fd.Forward,
+		Step:    1e-8,
+	})
+	fmt.Println("f'(0) ≅", df)
+
+	f = func(x float64) float64 {
+		return math.Pow(math.Cos(x), 3)
+	}
+	// Compute the second derivative of f at 0 using the centered
+	// approximation, a custom step size, concurrent evaluation, and a known
+	// function value at x.
+	df = fd.Derivative(f, 0, &fd.Settings{
+		Formula:     fd.Central2nd,
+		Step:        1e-4,
+		Concurrent:  2,
+		OriginKnown: true,
+		OriginValue: f(0),
+	})
+	fmt.Println("f''(0) ≅", df)
+
+	// Output:
+	// f'(0) ≅ 0.9999999999998334
+	// f'(0) ≅ 1
+	// f''(0) ≅ -2.999999981767587
+}

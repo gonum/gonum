f988c6263a01d7cd643244d44b2e002e5edde7e8
Vladimir Chalupecky <vladimir.chalupecky@gmail.com>
2016-04-13 16:50:15 +0900

    fd: make evaluations in Jacobian completely concurrent
    Previously, for simplicity f was evaluated serially even when
    JacobianSettings.Concurrent == true.

--- fd/jacobian.go
+++ fd/jacobian.go
@@ -73,25 +73,12 @@ func Jacobian(dst *mat64.Dense, f func(y, x []float64), m int, x []float64, sett
 		step = formula.Step
 	}
 
-	var hasOrigin bool
+	evals := n * len(formula.Stencil)
 	for _, pt := range formula.Stencil {
 		if pt.Loc == 0 {
-			hasOrigin = true
+			evals -= n - 1
 			break
 		}
-	}
-
-	xcopy := make([]float64, n)
-	origin := settings.OriginValue
-	if hasOrigin && origin == nil {
-		origin = make([]float64, m)
-		copy(xcopy, x)
-		f(origin, xcopy)
-	}
-
-	evals := n * len(formula.Stencil)
-	if hasOrigin {
-		evals -= n
 	}
 	nWorkers := 1
 	if settings.Concurrent {
@@ -100,25 +87,31 @@ func Jacobian(dst *mat64.Dense, f func(y, x []float64), m int, x []float64, sett
 			nWorkers = evals
 		}
 	}
-
 	if nWorkers == 1 {
-		jacobianSerial(dst, f, x, xcopy, origin, formula, step)
+		jacobianSerial(dst, f, x, settings.OriginValue, formula, step)
 	} else {
-		jacobianConcurrent(dst, f, x, origin, formula, step, nWorkers)
+		jacobianConcurrent(dst, f, x, settings.OriginValue, formula, step, nWorkers)
 	}
+
 	return dst
 }
 
-func jacobianSerial(dst *mat64.Dense, f func([]float64, []float64), x, xcopy, origin []float64, formula Formula, step float64) {
-	r, c := dst.Dims()
-	y := make([]float64, r)
-	col := make([]float64, r)
-	for j := 0; j < c; j++ {
+func jacobianSerial(dst *mat64.Dense, f func([]float64, []float64), x, origin []float64, formula Formula, step float64) {
+	m, n := dst.Dims()
+	xcopy := make([]float64, n)
+	y := make([]float64, m)
+	col := make([]float64, m)
+	for j := 0; j < n; j++ {
 		for i := range col {
 			col[i] = 0
 		}
 		for _, pt := range formula.Stencil {
 			if pt.Loc == 0 {
+				if origin == nil {
+					origin = make([]float64, m)
+					copy(xcopy, x)
+					f(origin, xcopy)
+				}
 				floats.AddScaled(col, pt.Coeff, origin)
 			} else {
 				copy(xcopy, x)
@@ -133,50 +126,73 @@ func jacobianSerial(dst *mat64.Dense, f func([]float64, []float64), x, xcopy, or
 }
 
 func jacobianConcurrent(dst *mat64.Dense, f func([]float64, []float64), x, origin []float64, formula Formula, step float64, nWorkers int) {
-	r, c := dst.Dims()
-	originVec := mat64.NewVector(r, origin)
-	for i := 0; i < r; i++ {
-		for j := 0; j < c; j++ {
+	m, n := dst.Dims()
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
 			dst.Set(i, j, 0)
 		}
 	}
 
-	var wg sync.WaitGroup
+	var (
+		wg sync.WaitGroup
+		mu = make([]sync.Mutex, n) // Guard access to individual columns.
+	)
+	worker := func(jobs <-chan jacJob) {
+		defer wg.Done()
+		xcopy := make([]float64, n)
+		y := make([]float64, m)
+		yVec := mat64.NewVector(m, y)
+		for job := range jobs {
+			copy(xcopy, x)
+			xcopy[job.j] += job.pt.Loc * step
+			f(y, xcopy)
+			col := dst.ColView(job.j)
+			mu[job.j].Lock()
+			col.AddScaledVec(col, job.pt.Coeff, yVec)
+			mu[job.j].Unlock()
+		}
+	}
 	jobs := make(chan jacJob, nWorkers)
-	mus := make([]sync.Mutex, c) // Guard access to individual columns.
 	for i := 0; i < nWorkers; i++ {
 		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			xcopy := make([]float64, len(x))
-			y := make([]float64, r)
-			yVec := mat64.NewVector(r, y)
-			for job := range jobs {
-				if job.pt.Loc != 0 {
-					// Not an origin, we need to evaluate.
-					copy(xcopy, x)
-					xcopy[job.j] += job.pt.Loc * step
-					f(y, xcopy)
-				}
-
-				col := dst.ColView(job.j)
-				mus[job.j].Lock()
-				if job.pt.Loc == 0 {
-					col.AddScaledVec(col, job.pt.Coeff, originVec)
-				} else {
-					col.AddScaledVec(col, job.pt.Coeff, yVec)
-				}
-				mus[job.j].Unlock()
-			}
-		}()
+		go worker(jobs)
 	}
+	var (
+		hasOrigin   bool
+		originCoeff float64
+	)
 	for _, pt := range formula.Stencil {
-		for j := 0; j < c; j++ {
+		if pt.Loc == 0 {
+			hasOrigin = true
+			originCoeff = pt.Coeff
+			continue
+		}
+		for j := 0; j < n; j++ {
 			jobs <- jacJob{j, pt}
 		}
 	}
+	if hasOrigin && origin == nil {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			origin = make([]float64, m)
+			xcopy := make([]float64, n)
+			copy(xcopy, x)
+			f(origin, xcopy)
+		}()
+	}
 	close(jobs)
 	wg.Wait()
+
+	if hasOrigin {
+		// The formula evaluated at x, we need to add scaled origin to
+		// all columns of dst.
+		originVec := mat64.NewVector(m, origin)
+		for j := 0; j < n; j++ {
+			col := dst.ColView(j)
+			col.AddScaledVec(col, originCoeff, originVec)
+		}
+	}
 
 	dst.Scale(1/step, dst)
 }

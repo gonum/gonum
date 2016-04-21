5bd3f94ba09c19cef55d452615e42512e7765afc
Brendan Tracey <tracey.brendan@gmail.com>
2016-04-20 23:10:58 -0600

    fix package imports

--- airy/airy.go
+++ airy/airy.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package mathext
+package airy
 
 import "github.com/gonum/mathext/airy/internal/amos"
 

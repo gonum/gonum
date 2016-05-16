1f34fc3dd396a20b41c2d8000644e7156fbc8022
Vladimir Chalupecky <vladimir.chalupecky@gmail.com>
2016-05-16 18:30:27 +0900

    testlapack: update test for Dlaqr5 to read testing data in JSON format

--- testlapack/dlaqr5.go
+++ testlapack/dlaqr5.go
@@ -5,10 +5,12 @@
 package testlapack
 
 import (
-	"archive/zip"
+	"compress/gzip"
+	"encoding/json"
 	"fmt"
-	"io"
 	"log"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -16,20 +18,40 @@ type Dlaqr5er interface {
 	Dlaqr5(wantt, wantz bool, kacc22 int, n, ktop, kbot, nshfts int, sr, si []float64, h []float64, ldh int, iloz, ihiz int, z []float64, ldz int, v []float64, ldv int, u []float64, ldu int, nh int, wh []float64, ldwh int, nv int, wv []float64, ldwv int)
 }
 
+type Dlaqr5test struct {
+	WantT          bool
+	N              int
+	NShifts        int
+	KTop, KBot     int
+	ShiftR, ShiftI []float64
+	H              []float64
+
+	HWant []float64
+	ZWant []float64
+}
+
 func Dlaqr5Test(t *testing.T, impl Dlaqr5er) {
-	r, err := zip.OpenReader("../internal/testdata/dlaqr5test/dlaqr5data.zip")
+	file, err := os.Open(filepath.FromSlash("../testlapack/testdata/dlaqr5data.json.gz"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	r, err := gzip.NewReader(file)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer r.Close()
 
-	for _, f := range r.File {
-		tc, err := f.Open()
-		if err != nil {
-			log.Fatal(err)
-		}
-		wantt, n, nshfts, ktop, kbot, sr, si, hOrig, hwant, zwant := readDlaqr5Case(tc)
-		tc.Close()
+	var tests []Dlaqr5test
+	json.NewDecoder(r).Decode(&tests)
+	for _, test := range tests {
+		wantt := test.WantT
+		n := test.N
+		nshfts := test.NShifts
+		ktop := test.KTop
+		kbot := test.KBot
+		sr := test.ShiftR
+		si := test.ShiftI
 
 		v := make([]float64, nshfts/2*3)
 		u := make([]float64, (3*nshfts-3)*(3*nshfts-3))
@@ -40,8 +62,9 @@ func Dlaqr5Test(t *testing.T, impl Dlaqr5er) {
 
 		for _, ldh := range []int{n, n + 1, n + 10} {
 			h := make([]float64, (n-1)*ldh+n)
+
 			for _, kacc22 := range []int{0, 1, 2} {
-				copyMatrix(n, n, h, ldh, hOrig)
+				copyMatrix(n, n, h, ldh, test.H)
 				z := eye(n, ldh)
 
 				impl.Dlaqr5(wantt, true, kacc22,
@@ -54,66 +77,15 @@ func Dlaqr5Test(t *testing.T, impl Dlaqr5er) {
 					nh, wh, nh,
 					nv, wv, 3*nshfts-3)
 
-				if !equalApprox(n, n, h, ldh, hwant, 1e-13) {
-					t.Errorf("Case %v, kacc22=%v: unexpected matrix H\nh    =%v\nhwant=%v", f.Name, kacc22, h, hwant)
+				prefix := fmt.Sprintf("wantt=%v, n=%v, nshfts=%v, ktop=%v, kbot=%v, ldh=%v, kacc22=%v",
+					wantt, n, nshfts, ktop, kbot, ldh, kacc22)
+				if !equalApprox(n, n, h, ldh, test.HWant, 1e-13) {
+					t.Errorf("Case %v: unexpected matrix H\nh    =%v\nhwant=%v", prefix, h, test.HWant)
 				}
-				if !equalApprox(n, n, z, ldh, zwant, 1e-13) {
-					t.Errorf("Case %v, kacc22=%v: unexpected matrix Z\nz    =%v\nzwant=%v", f.Name, kacc22, z, zwant)
+				if !equalApprox(n, n, z, ldh, test.ZWant, 1e-13) {
+					t.Errorf("Case %v: unexpected matrix Z\nz    =%v\nzwant=%v", prefix, z, test.ZWant)
 				}
 			}
 		}
 	}
-}
-
-// readDlaqr5Case reads and returns test data written by internal/testdata/dlaqr5test/main.go.
-func readDlaqr5Case(r io.Reader) (wantt bool, n, nshfts, ktop, kbot int, sr, si []float64, h, hwant, zwant []float64) {
-	_, err := fmt.Fscanln(r, &wantt, &n, &nshfts, &ktop, &kbot)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	sr = make([]float64, nshfts)
-	si = make([]float64, nshfts)
-	h = make([]float64, n*n)
-	hwant = make([]float64, n*n)
-	zwant = make([]float64, n*n)
-
-	for i := range sr {
-		_, err = fmt.Fscanln(r, &sr[i])
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-	for i := range si {
-		_, err = fmt.Fscanln(r, &si[i])
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-	for i := 0; i < n; i++ {
-		for j := 0; j < n; j++ {
-			_, err = fmt.Fscanln(r, &h[i*n+j])
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-	}
-	for i := 0; i < n; i++ {
-		for j := 0; j < n; j++ {
-			_, err = fmt.Fscanln(r, &hwant[i*n+j])
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-	}
-	for i := 0; i < n; i++ {
-		for j := 0; j < n; j++ {
-			_, err = fmt.Fscanln(r, &zwant[i*n+j])
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-	}
-
-	return wantt, n, nshfts, ktop, kbot, sr, si, h, hwant, zwant
 }

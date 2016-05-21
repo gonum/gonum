d1838e665463695960b9527f5328866db54380d8
Chad Kunde <Kunde21@gmail.com>
2016-05-21 14:14:31 -0700

    f64 Add, AddConst, Div, and Divto asm routines

---
+++ asm/f64/add_amd64.s
@@ -0,0 +1,40 @@
+// Copyright ©2016 The gonum Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include "textflag.h"
+
+// func Daddconst(dst, s []float64)
+TEXT ·Add(SB), NOSPLIT, $0
+	MOVQ 	dst_base+0(FP), DI
+	MOVQ 	dst_len+8(FP), DX
+	MOVQ 	s_base+24(FP), SI
+	CMPQ	s_len+32(FP), DX
+	CMOVLEQ	s_len+32(FP), DX
+	CMPQ	DX, $0
+	JE	add_end
+	XORQ	AX, AX
+	CMPQ	DX, $4
+	JL	add_tail
+add_loop:
+	MOVUPS	(SI)(AX*8), X0
+	MOVUPS	16(SI)(AX*8), X1
+	ADDPD	(DI)(AX*8), X0
+	MOVUPS	X0, (DI)(AX*8)
+	ADDPD	16(DI)(AX*8), X1
+	MOVUPS	X1, 16(DI)(AX*8)
+	ADDQ	$4, AX
+	SUBQ	$4, DX
+	CMPQ	DX, $4
+	JGE	add_loop
+	CMPQ	DX, $0
+	JE	add_end
+add_tail:
+	MOVSD	(DI)(AX*8), X0
+	DIVSD	(SI)(AX*8), X0
+	MOVSD	X0, (DI)(AX*8)
+	INCQ	AX
+	DECQ	DX
+	JNZ	add_tail
+add_end:
+	RET
---
+++ asm/f64/div_amd64.s
@@ -0,0 +1,40 @@
+// Copyright ©2016 The gonum Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include "textflag.h"
+
+// func Daddconst(dst, s []float64)
+TEXT ·Div(SB), NOSPLIT, $0
+	MOVQ 	dst_base+0(FP), DI
+	MOVQ 	dst_len+8(FP), DX
+	MOVQ 	s_base+24(FP), SI
+	CMPQ	s_len+32(FP), DX
+	CMOVLEQ	s_len+32(FP), DX
+	CMPQ	DX, $0
+	JE	div_end
+	XORQ	AX, AX
+	CMPQ	DX, $4
+	JL	div_tail
+div_loop:	//Unroll 4x
+	MOVUPS	(DI)(AX*8), X0
+	DIVPD	(SI)(AX*8), X0
+	MOVUPS	X0, (DI)(AX*8)
+	MOVUPS	16(DI)(AX*8), X0
+	DIVPD	16(SI)(AX*8), X0
+	MOVUPS	X0, 16(DI)(AX*8)
+	ADDQ	$4, AX
+	SUBQ	$4, DX
+	CMPQ	DX, $4
+	JGE	div_loop
+	CMPQ	DX, $0
+	JE	div_end
+div_tail:
+	MOVSD	(DI)(AX*8), X0
+	DIVSD	(SI)(AX*8), X0
+	MOVSD	X0, (DI)(AX*8)
+	INCQ	AX
+	DECQ	DX
+	JNZ	div_tail
+div_end:
+	RET

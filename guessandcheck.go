977c9898cb372bec21c3c82995f5fbfacd4b517f
Brendan Tracey <tracey.brendan@gmail.com>
2016-09-02 16:44:15 -0600

    Combine implementations of Global and Local

--- guessandcheck.go
+++ guessandcheck.go
@@ -4,7 +4,12 @@
 
 package optimize
 
-import "github.com/gonum/stat/distmv"
+import (
+	"math"
+	"sync"
+
+	"github.com/gonum/stat/distmv"
+)
 
 // GuessAndCheck is a global optimizer that evaluates the function at random
 // locations. Not a good optimizer, but useful for comparison and debugging.
@@ -12,6 +17,10 @@ type GuessAndCheck struct {
 	Rander distmv.Rander
 
 	eval []bool
+
+	mux   *sync.Mutex
+	bestF float64
+	bestX []float64
 }
 
 func (g *GuessAndCheck) Needs() struct{ Gradient, Hessian bool } {
@@ -22,14 +31,27 @@ func (g *GuessAndCheck) Done() {
 	// No cleanup needed
 }
 
-func (g *GuessAndCheck) InitGlobal(tasks int) int {
+func (g *GuessAndCheck) InitGlobal(dim, tasks int) int {
 	g.eval = make([]bool, tasks)
+	g.bestF = math.Inf(1)
+	g.bestX = resize(g.bestX, dim)
+	g.mux = &sync.Mutex{}
 	return tasks
 }
 
 func (g *GuessAndCheck) IterateGlobal(task int, loc *Location) (Operation, error) {
+	// Task is true if it contains a new function evaluation.
 	if g.eval[task] {
 		g.eval[task] = false
+		g.mux.Lock()
+		if loc.F < g.bestF {
+			g.bestF = loc.F
+			copy(g.bestX, loc.X)
+		} else {
+			loc.F = g.bestF
+			copy(loc.X, g.bestX)
+		}
+		g.mux.Unlock()
 		return MajorIteration, nil
 	}
 	g.eval[task] = true

95032e6816a4f3e6e3dda677d7b0fc8caad31d22
Brendan Tracey <tracey.brendan@gmail.com>
2016-10-25 11:43:23 -0600

    Expand definition of Student's T

--- distuv/studentst_test.go
+++ distuv/studentst_test.go
@@ -15,15 +15,16 @@ import (
 
 func TestStudentsTProb(t *testing.T) {
 	for _, test := range []struct {
-		x, nu, want float64
+		x, mu, sigma, nu, want float64
 	}{
 		// Values comparison with scipy.
-		{0.01, 2.74, 0.364778548181318},
-		{-0.01, 2.74, 0.364778548181318},
-		{0.4, 1.6, 0.30376391362582678},
-		{-0.4, 1.6, 0.30376391362582678},
+		{0.01, 0, 1, 2.74, 0.364778548181318},
+		{-0.01, 0, 1, 2.74, 0.364778548181318},
+		{0.4, 0, 1, 1.6, 0.30376391362582678},
+		{-0.4, 0, 1, 1.6, 0.30376391362582678},
+		{0.2, 15, 5, 10, 0.0024440848858034393},
 	} {
-		pdf := StudentsT{test.nu, nil}.Prob(test.x)
+		pdf := StudentsT{test.mu, test.sigma, test.nu, nil}.Prob(test.x)
 		if !floats.EqualWithinAbsOrRel(pdf, test.want, 1e-10, 1e-10) {
 			t.Errorf("Pdf mismatch, x = %v, Nu = %v. Got %v, want %v", test.x, test.nu, pdf, test.want)
 		}
@@ -33,9 +34,10 @@ func TestStudentsTProb(t *testing.T) {
 func TestStudentsT(t *testing.T) {
 	src := rand.New(rand.NewSource(1))
 	for i, b := range []StudentsT{
-		{3.3, src},
-		{7.2, src},
-		{12, src},
+		{0, 1, 3.3, src},
+		{0, 1, 7.2, src},
+		{0, 1, 12, src},
+		{0.9, 0.8, 6, src},
 	} {
 		testStudentsT(t, b, i)
 	}
@@ -53,9 +55,6 @@ func testStudentsT(t *testing.T, c StudentsT, i int) {
 	checkMean(t, i, x, c, tol)
 	if c.Nu > 2 {
 		checkVarAndStd(t, i, x, c, tol)
-	}
-	if c.Nu > 4 {
-		checkExKurtosis(t, i, x, c, 5e-2)
 	}
 	checkProbContinuous(t, i, x, c, 1e-3)
 }

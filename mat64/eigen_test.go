d46b737b4cf14b6772a9bd9efd64126da0f6d192
Brendan Tracey <tracey.brendan@gmail.com>
2016-11-11 11:09:52 -0700

    Replace Eigen with lapack-based implementation

--- mat64/eigen_test.go
+++ mat64/eigen_test.go
@@ -5,9 +5,7 @@
 package mat64
 
 import (
-	"math"
 	"math/rand"
-	"reflect"
 	"testing"
 
 	"github.com/gonum/floats"
@@ -16,101 +14,52 @@ import (
 func TestEigen(t *testing.T) {
 	for i, test := range []struct {
 		a *Dense
-
-		epsilon float64
-
-		e, d []float64
-		v    *Dense
 	}{
-		{
-			a: NewDense(3, 3, []float64{
-				1, 2, 1,
-				6, -1, 0,
-				-1, -2, -1,
-			}),
-
-			epsilon: math.Pow(2, -52.0),
-
-			d: []float64{3.0000000000000044, -4.000000000000003, -1.0980273383714707e-16},
-			e: []float64{0, 0, 0},
-			v: NewDense(3, 3, []float64{
-				-0.48507125007266627, 0.41649656391752204, 0.11785113019775795,
-				-0.7276068751089995, -0.8329931278350428, 0.7071067811865481,
-				0.48507125007266627, -0.4164965639175216, -1.5320646925708532,
-			}),
-		},
-		{
-			a: NewDense(3, 3, []float64{
-				1, 6, -1,
-				6, -1, -2,
-				-1, -2, -1,
-			}),
-
-			epsilon: math.Pow(2, -52.0),
-
-			d: []float64{-6.240753470718579, -1.3995889142010132, 6.640342384919599},
-			e: []float64{0, 0, 0},
-			v: NewDense(3, 3, []float64{
-				-0.6134279348516111, -0.31411097261113, -0.7245967607083111,
-				0.7697297716508223, -0.03251534945303795, -0.6375412384185983,
-				0.17669818159240022, -0.9488293044247931, 0.2617263908869383,
-			}),
-		},
-		{ // Jama pvals
-			a: NewDense(3, 3, []float64{
-				4, 1, 1,
-				1, 2, 3,
-				1, 3, 6,
-			}),
-
-			epsilon: math.Pow(2, -52.0),
-		},
-		{ // Jama evals
-			a: NewDense(4, 4, []float64{
-				0, 1, 0, 0,
-				1, 0, 2e-7, 0,
-				0, -2e-7, 0, 1,
-				0, 0, 1, 0,
-			}),
-
-			epsilon: math.Pow(2, -52.0),
-		},
-		{ // Jama badeigs
-			a: NewDense(5, 5, []float64{
-				0, 0, 0, 0, 0,
-				0, 0, 0, 0, 1,
-				0, 0, 0, 1, 0,
-				1, 1, 0, 0, 1,
-				1, 0, 1, 0, 1,
-			}),
-
-			epsilon: math.Pow(2, -52.0),
-		},
+		{a: NewDense(3, 3, []float64{
+			1, 0, 0,
+			0, 1, 0,
+			0, 0, 1,
+		})},
 	} {
-		ef := eigen(DenseCopyOf(test.a), test.epsilon)
-		if test.d != nil {
-			if !reflect.DeepEqual(ef.d, test.d) {
-				t.Errorf("unexpected d for test %d", i)
-			}
+		var e1, e2, e3, e4 Eigen
+		ok := e1.Factorize(test.a, true, true)
+		if !ok {
+			panic("bad factorization")
 		}
-		if test.e != nil {
-			if !reflect.DeepEqual(ef.e, test.e) {
-				t.Errorf("unexpected e for test %d", i)
-			}
+		e2.Factorize(test.a, false, true)
+		e3.Factorize(test.a, true, false)
+		e4.Factorize(test.a, false, false)
+
+		// Check that the eigenvectors and values are the same in all combinations.
+		v1 := e1.Values(nil)
+		if !cmplxEqual(v1, e2.Values(nil)) {
+			t.Errorf("eigenvector mismatch. Case %v", i)
+		}
+		if !cmplxEqual(v1, e3.Values(nil)) {
+			t.Errorf("eigenvector mismatch. Case %v", i)
+		}
+		if !cmplxEqual(v1, e4.Values(nil)) {
+			t.Errorf("eigenvector mismatch. Case %v", i)
+		}
+		if !Equal(e1.Vectors(), e2.Vectors()) {
+			t.Errorf("right eigenvector mismatch. Case %v", i)
+		}
+		if !Equal(e1.LeftVectors(), e3.LeftVectors()) {
+			t.Errorf("right eigenvector mismatch. Case %v", i)
 		}
 
-		if test.v != nil {
-			if !Equal(ef.V, test.v) {
-				t.Errorf("unexpected v for test %d", i)
-			}
-		}
+		// TODO(btracey): Also add in a test for correctness when #308 is
+		// resolved and we have a CMat.Mul().
+	}
+}
 
-		test.a.Mul(test.a, ef.V)
-		ef.V.Mul(ef.V, ef.D())
-		if !EqualApprox(test.a, ef.V, 1e-12) {
-			t.Errorf("unexpected factor product for test %d", i)
+func cmplxEqual(v1, v2 []complex128) bool {
+	for i, v := range v1 {
+		if v != v2[i] {
+			return false
 		}
 	}
+	return true
 }
 
 func TestSymEigen(t *testing.T) {

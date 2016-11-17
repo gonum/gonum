e9caa4fd767d6a47e0203ad7b0c5a6ef50f5ace9
Kent English <kent.english@gmail.com>
2016-11-17 11:55:05 -0500

    Adding ratevl for rational function evaluation

--- internal/cephes/polevl.go
+++ internal/cephes/polevl.go
@@ -8,7 +8,14 @@
  * Direct inquiries to 30 Frost Street, Cambridge, MA 02140
  */
 
+/* Sources:
+ * [1] Holin et. al., "Polynomial and Rational Function Evaluation",
+ *     http://www.boost.org/doc/libs/1_61_0/libs/math/doc/html/math_toolkit/roots/rational.html
+ */
+
 package cephes
+
+import "math"
 
 // polevl evaluates a polynomial of degree N
 //  y = c_0 + c_1 x_1 + c_2 x_2^2 ...
@@ -30,4 +37,50 @@ func p1evl(x float64, coef []float64, n int) float64 {
 		ans = ans*x + coef[i]
 	}
 	return ans
+}
+
+// ratevl evaluates a rational function. See [1].
+func ratevl(x float64, num []float64, m int, denom []float64, n int) float64 {
+	absx := math.Abs(x)
+
+	var dir, idx int
+	var y float64
+	if absx > 1 {
+		// Evaluate as a polynomial in 1/x
+		dir = -1
+		idx = m
+		y = 1 / x
+	} else {
+		dir = 1
+		idx = 0
+		y = x
+	}
+
+	// Evaluate the numerator
+	numAns := num[idx]
+	idx += dir
+	for i := 0; i < m; i++ {
+		numAns = numAns*y + num[idx]
+		idx += dir
+	}
+
+	// Evaluate the denominator
+	if absx > 1 {
+		idx = n
+	} else {
+		idx = 0
+	}
+
+	denomAns := denom[idx]
+	idx += dir
+	for i := 0; i < n; i++ {
+		denomAns = denomAns*y + denom[idx]
+		idx += dir
+	}
+
+	if absx > 1 {
+		pow := float64(n - m)
+		return math.Pow(x, pow) * numAns / denomAns
+	}
+	return numAns / denomAns
 }

c14ef6b98d0ca2ea30bd02c3d95d1ad5e6bc4d4c
Chad Kunde <Kunde21@gmail.com>
2017-01-22 06:05:12 -0800

    asm: Remove benchmark script and relate .gitignore.

--- asm/benchRptAxpy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-go get -u github.com/Kunde21/sift
-
-go test ./...  -bench Axpy | tee >( sift markL | sed s/markL/mark/ > old.tst ) |  sift mark[^L] >new.tst
-benchcmp old.tst new.tst

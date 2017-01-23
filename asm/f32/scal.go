d807112835e54278fd885c1c43a7093ed9411e04
kortschak <dan.kortschak@adelaide.edu.au>
2017-01-23 12:28:25 +1030

    asm: fix build retry

--- asm/f32/scal.go
+++ asm/f32/scal.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//+build !amd64 noasm appengine
-
 package f32
 
 func ScalUnitary(alpha float32, x []float32) {

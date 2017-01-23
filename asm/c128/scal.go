5d77e9b08ad79f7f7bf7640304eb7046ce8972ad
kortschak <dan.kortschak@adelaide.edu.au>
2017-01-23 11:57:07 +1030

    all: remove tests and references to generated code

--- asm/c128/scal.go
+++ asm/c128/scal.go
@@ -1,5 +1,3 @@
-// Generated code do not edit. Run `go generate`.
-
 // Copyright ©2016 The gonum Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.

7a97baedff6c7bd249c54935f8f9f54bdc96c617
Brendan Tracey <tracey.brendan@gmail.com>
2017-06-14 09:07:02 -0600

    optimize/functions: implement the many local minima functions in the virtual library of simulation experiments

--- optimize/functions/validate.go
+++ optimize/functions/validate.go
@@ -2,13 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package functions provides objective functions for testing optimization
-// algorithms.
-//
-// We encourage outside contributions of additional test functions that exhibit
-// properties not already covered in the testing suite or that have
-// significance due to prior use as benchmark cases.
-package functions // import "gonum.org/v1/gonum/optimize/functions"
+package functions
 
 import (
 	"math"

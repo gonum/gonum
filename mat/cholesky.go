9660b32cbcca4c9654d4da10a12f4d01164c234b
kortschak <dan.kortschak@adelaide.edu.au>
2017-06-25 08:39:48 +0930

    mat: remove outdated attributions to Jama code

--- mat/cholesky.go
+++ mat/cholesky.go
@@ -1,7 +1,6 @@
 // Copyright ©2013 The gonum Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-// Based on the CholeskyDecomposition class from Jama 1.0.3.
 
 package mat
 

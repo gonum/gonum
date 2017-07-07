b48729bc56c7294eb4fb6fc191638fab44e22511
kortschak <dan.kortschak@adelaide.edu.au>
2017-07-07 21:52:14 +0930

    stat/spatial: use mat.RowNonZeroDoer

--- stat/spatial/spatial_example_test.go
+++ stat/spatial/spatial_example_test.go
@@ -38,6 +38,35 @@ func ExampleGlobalMoransI_linear() {
 	// Moran's I=0.1111 z-score=0.6335
 }
 
+func ExampleGlobalMoransI_banded() {
+	data := []float64{0, 0, 0, 1, 1, 1, 0, 1, 0, 0}
+
+	// The locality here describes spatial neighbor
+	// relationships.
+	// This example uses the band matrix representation
+	// to improve time and space efficiency.
+	locality := mat.NewBandDense(10, 10, 1, 1, []float64{
+		0, 0, 1,
+		1, 0, 1,
+		1, 0, 1,
+		1, 0, 1,
+		1, 0, 1,
+		1, 0, 1,
+		1, 0, 1,
+		1, 0, 1,
+		1, 0, 1,
+		1, 0, 0,
+	})
+
+	i, _, z := spatial.GlobalMoransI(data, nil, locality)
+
+	fmt.Printf("Moran's I=%.4v z-score=%.4v\n", i, z)
+
+	// Output:
+	//
+	// Moran's I=0.1111 z-score=0.6335
+}
+
 func ExampleGetisOrdGStar() {
 	data := []float64{0, 0, 0, 1, 1, 1, 0, 1, 0, 0}
 
@@ -54,6 +83,44 @@ func ExampleGetisOrdGStar() {
 		0, 0, 0, 0, 0, 0, 1, 1, 1, 0,
 		0, 0, 0, 0, 0, 0, 0, 1, 1, 1,
 		0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+	})
+
+	for i, v := range data {
+		fmt.Printf("v=%v G*i=% .4v\n", v, spatial.GetisOrdGStar(i, data, nil, locality))
+	}
+
+	// Output:
+	//
+	// v=0 G*i=-1.225
+	// v=0 G*i=-1.604
+	// v=0 G*i=-0.2673
+	// v=1 G*i= 1.069
+	// v=1 G*i= 2.405
+	// v=1 G*i= 1.069
+	// v=0 G*i= 1.069
+	// v=1 G*i=-0.2673
+	// v=0 G*i=-0.2673
+	// v=0 G*i=-1.225
+}
+
+func ExampleGetisOrd_band() {
+	data := []float64{0, 0, 0, 1, 1, 1, 0, 1, 0, 0}
+
+	// The locality here describes spatial neighbor
+	// relationships including self.
+	// This example uses the band matrix representation
+	// to improve time and space efficiency.
+	locality := mat.NewBandDense(10, 10, 1, 1, []float64{
+		0, 1, 1,
+		1, 1, 1,
+		1, 1, 1,
+		1, 1, 1,
+		1, 1, 1,
+		1, 1, 1,
+		1, 1, 1,
+		1, 1, 1,
+		1, 1, 1,
+		1, 1, 0,
 	})
 
 	for i, v := range data {

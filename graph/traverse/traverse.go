00292b2d57096d082fef2675eef5e011ee034c01
mewmew <rnd0x00@gmail.com>
2017-08-16 13:56:50 +0200

    all: use fixdocs tool to store package documentation in doc.go
    Add copyright header to doc.go

--- graph/traverse/traverse.go
+++ graph/traverse/traverse.go
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package traverse provides basic graph traversal primitives.
 package traverse // import "gonum.org/v1/gonum/graph/traverse"
 
 import (

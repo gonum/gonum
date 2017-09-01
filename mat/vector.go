8adc7a1039d330f6d0df705182a0e4a4141fec94
james-bowman <james.edward.bowman@gmail.com>
2017-09-01 17:46:20 +0100

    updated documentation to reflect change in behaviour

--- mat/vector.go
+++ mat/vector.go
@@ -484,8 +484,8 @@ func (v *VecDense) asGeneral() blas64.General {
 }
 
 // ColViewOf reflects the column j of the RawMatrixer m, into the receiver
-// backed by the same underlying data. The length of the receiver does not need
-// to match the number of rows in m.
+// backed by the same underlying data. The length of the receiver must either be
+// zero or match the number of rows in m.
 func (v *VecDense) ColViewOf(m RawMatrixer, j int) {
 	rm := m.RawMatrix()
 
@@ -502,8 +502,8 @@ func (v *VecDense) ColViewOf(m RawMatrixer, j int) {
 }
 
 // RowViewOf reflects the row i of the RawMatrixer m, into the receiver
-// backed by the same underlying data. The length of the receiver does not need
-// to match the number of columns in m.
+// backed by the same underlying data. The length of the receiver must either be
+// zero or match the number of columns in m.
 func (v *VecDense) RowViewOf(m RawMatrixer, i int) {
 	rm := m.RawMatrix()
 

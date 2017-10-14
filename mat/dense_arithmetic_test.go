35d89bcffe0991f2fe1da16e92497f84c363b043
Jeremy Atkinson <jchatkinson@gmail.com>
2017-10-13 18:48:04 -0700

    updated usage examples based on code review

--- mat/dense_arithmetic_test.go
+++ mat/dense_arithmetic_test.go
@@ -11,32 +11,34 @@ import (
 )
 
 func ExampleDense_Add() {
-	// Initialize two matrices, a and b
+	// Initialize two matrices, a and b.
 	a := mat.NewDense(2, 2, []float64{1, 0, 1, 0})
 	b := mat.NewDense(2, 2, []float64{0, 1, 0, 1})
 
-	// Add a and b, placing the result into b
-	b.Add(a, b)
+	// Add a and b, placing the result into c.
+	// ...Notice that the size is automatically adjusted when the receiver has zero size.
+	var c mat.Dense
+	c.Add(a, b)
 
-	// print the result using the formatter.
-	fb := mat.Formatted(b, mat.Prefix("    "), mat.Squeeze())
-	fmt.Printf("Result:\nb = %v\n\n", fb)
+	// Print the result using the formatter.
+	fc := mat.Formatted(&c, mat.Prefix("    "), mat.Squeeze())
+	fmt.Printf("Result:\nc = %v\n\n", fc)
 	// Output:
 	// Result:
-	// b = ⎡1  1⎤
+	// c = ⎡1  1⎤
 	//     ⎣1  1⎦
 	//
 }
 
 func ExampleDense_Sub() {
-	// Initialize two matrices, a and b
+	// Initialize two matrices, a and b.
 	a := mat.NewDense(2, 2, []float64{1, 1, 1, 1})
 	b := mat.NewDense(2, 2, []float64{1, 0, 0, 1})
 
-	// Subtract b from a, placing the result into a
+	// Subtract b from a, placing the result into a.
 	a.Sub(a, b)
 
-	// print the result using the formatter.
+	// Print the result using the formatter.
 	fa := mat.Formatted(a, mat.Prefix("    "), mat.Squeeze())
 	fmt.Printf("Result:\na = %v\n\n", fa)
 	// Output:
@@ -47,14 +49,14 @@ func ExampleDense_Sub() {
 }
 
 func ExampleDense_MulElem() {
-	// Initialize two matrices, a and b
+	// Initialize two matrices, a and b.
 	a := mat.NewDense(2, 2, []float64{1, 2, 3, 4})
 	b := mat.NewDense(2, 2, []float64{1, 2, 3, 4})
 
-	// Muliply the elements of a and b, placing the result into a
+	// Multiply the elements of a and b, placing the result into a.
 	a.MulElem(a, b)
 
-	// print the result using the formatter.
+	// Print the result using the formatter.
 	fa := mat.Formatted(a, mat.Prefix("    "), mat.Squeeze())
 	fmt.Printf("Result:\na = %v\n\n", fa)
 	// Output:
@@ -65,11 +67,11 @@ func ExampleDense_MulElem() {
 }
 
 func ExampleDense_DivElem() {
-	// Initialize two matrices, a and b
+	// Initialize two matrices, a and b.
 	a := mat.NewDense(2, 2, []float64{5, 10, 15, 20})
 	b := mat.NewDense(2, 2, []float64{5, 5, 5, 5})
 
-	// Divide the elements of a by b, placing the result into a
+	// Divide the elements of a by b, placing the result into a.
 	a.DivElem(a, b)
 
 	// Print the result using the formatter.
@@ -83,27 +85,44 @@ func ExampleDense_DivElem() {
 }
 
 func ExampleDense_Inverse() {
-	// Initialize two matrices, a and ia
+	// Initialize two matrices, a and ia.
 	a := mat.NewDense(2, 2, []float64{4, 0, 0, 4})
-	ia := mat.NewDense(2, 2, []float64{0, 0, 0, 0})
+	var ia mat.Dense
 
-	// Take the inverse of a and place the result in ia
+	// Take the inverse of a and place the result in ia.
 	ia.Inverse(a)
 
-	// print the result using the formatter.
-	fa := mat.Formatted(ia, mat.Prefix("     "), mat.Squeeze())
+	// Print the result using the formatter.
+	fa := mat.Formatted(&ia, mat.Prefix("     "), mat.Squeeze())
 	fmt.Printf("Result:\nia = %.2g\n\n", fa)
 
 	// Show that [A] * [A]^(-1) = [I]
-	ia.Mul(a, ia)
-	fmt.Printf("Result:\nia = %.2g\n\n", fa)
+	var r mat.Dense
+	r.Mul(a, &ia)
+	fr := mat.Formatted(&r, mat.Prefix("    "), mat.Squeeze())
+	fmt.Printf("Result:\nr = %v\n\n", fr)
+
+	// An alternative to using Inverse is to solve [A] * [X] = [I].
+	// ...Note that matrix inversion should generally be accomplished in this way, rather than using Inverse.
+	i := mat.NewDense(2, 2, []float64{1, 0, 0, 1})
+	var x mat.Dense
+	x.Solve(a, i)
+
+	// Print the result using the formatter.
+	fx := mat.Formatted(&x, mat.Prefix("    "), mat.Squeeze())
+	fmt.Printf("Result:\nx = %v\n\n", fx)
+
 	// Output:
 	// Result:
 	// ia = ⎡0.25    -0⎤
 	//      ⎣   0  0.25⎦
 	//
 	// Result:
-	// ia = ⎡1  0⎤
-	//      ⎣0  1⎦
+	// r = ⎡1  0⎤
+	//     ⎣0  1⎦
+	//
+	// Result:
+	// x = ⎡0.25     0⎤
+	//     ⎣   0  0.25⎦
 	//
 }

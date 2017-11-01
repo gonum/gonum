6d3fb2fde960ff8b5ff309587a8fb123329b968e
kortschak <dan.kortschak@adelaide.edu.au>
2017-11-01 10:30:53 +1030

    all: change capitalization of gonum in license header

--- graph/formats/dot/internal/paste_copyright.bash
+++ graph/formats/dot/internal/paste_copyright.bash
@@ -6,7 +6,7 @@ find . -type f -name '*.go' \
 \n\
 // This file is dual licensed under CC0 and The gonum license.\n\
 //\n\
-// Copyright ©2017 The gonum Authors. All rights reserved.\n\
+// Copyright ©2017 The Gonum Authors. All rights reserved.\n\
 // Use of this source code is governed by a BSD-style\n\
 // license that can be found in the LICENSE file.\n\
 //\n\

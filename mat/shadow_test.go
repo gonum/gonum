f6019db1d0e79f94530a7cc537a1de87b3c5fa6c
kortschak <dan.kortschak@adelaide.edu.au>
2017-12-05 16:20:05 +1030

    mat: use generalised rectangular shadow check
    This reduces code and regularises the checks at the cost of users being
    able to hide multiple triangles in a single rectangle.

--- mat/shadow_test.go
+++ mat/shadow_test.go
@@ -8,9 +8,6 @@ import (
 	"testing"
 
 	"golang.org/x/exp/rand"
-
-	"gonum.org/v1/gonum/blas"
-	"gonum.org/v1/gonum/blas/blas64"
 )
 
 func TestDenseOverlaps(t *testing.T) {
@@ -86,86 +83,6 @@ func TestDenseOverlaps(t *testing.T) {
 	}
 }
 
-func TestTriDenseOverlaps(t *testing.T) {
-	type view struct {
-		i, j, n int
-		*TriDense
-	}
-
-	rnd := rand.New(rand.NewSource(1))
-
-	for _, parentKind := range []TriKind{Upper, Lower} {
-		for n := 1; n < 20; n++ {
-			data := make([]float64, n*n)
-			for i := range data {
-				data[i] = float64(i + 1)
-			}
-			m := NewDense(n, n, data)
-			mt := denseAsTriDense(m, parentKind)
-			panicked, message := panics(func() { mt.checkOverlap(mt.RawTriangular()) })
-			if !panicked {
-				t.Error("expected matrix overlap with self")
-			}
-			if message != regionIdentity {
-				t.Errorf("unexpected panic message for self overlap: got: %q want: %q", message, regionIdentity)
-			}
-
-			for i := 0; i < 1000; i++ {
-				var views [2]view
-				for k := range views {
-					if n > 1 {
-						views[k].i = rnd.Intn(n - 1)
-						views[k].j = rnd.Intn(n - 1)
-						views[k].n = rnd.Intn(n-max(views[k].i, views[k].j)-1) + 1
-					} else {
-						views[k].n = 1
-					}
-					viewKind := []TriKind{Upper, Lower}[rnd.Intn(2)]
-					views[k].TriDense = denseAsTriDense(
-						m.Slice(views[k].i, views[k].i+views[k].n, views[k].j, views[k].j+views[k].n).(*Dense),
-						viewKind)
-
-					wantPanick := overlapsParentTriangle(views[k].i, views[k].j, views[k].n, parentKind, viewKind)
-
-					panicked, _ = panics(func() { mt.checkOverlap(views[k].RawTriangular()) })
-					if panicked != wantPanick {
-						t.Errorf("unexpected (%d×%d)%s overlap with view {rows=%d:%d, cols=%d:%d}%s got:%t want:%t\n% v\n\n% v\n",
-							n, n, kindString(parentKind),
-							views[k].i, views[k].i+views[k].n, views[k].j, views[k].j+views[k].n, kindString(viewKind),
-							panicked, wantPanick,
-							Formatted(mt), Formatted(views[k].TriDense))
-					}
-					panicked, _ = panics(func() { views[k].checkOverlap(mt.RawTriangular()) })
-					if panicked != wantPanick {
-						t.Errorf("unexpected {rows=%d:%d, cols=%d:%d}%s overlap with parent (%d×%d)%s got:%t want:%t\n% v\n\n% v\n",
-							views[k].i, views[k].i+views[k].n, views[k].j, views[k].j+views[k].n, kindString(viewKind),
-							n, n, kindString(parentKind),
-							panicked, wantPanick,
-							Formatted(views[k].TriDense), Formatted(mt))
-					}
-				}
-
-				want := overlapSiblingTriangles(
-					views[0].i, views[0].j, views[0].n, views[0].mat.Uplo == blas.Upper,
-					views[1].i, views[1].j, views[1].n, views[1].mat.Uplo == blas.Upper,
-				)
-
-				for k, v := range views {
-					w := views[1-k]
-					got, _ := panics(func() { v.checkOverlap(w.RawTriangular()) })
-					if got != want {
-						t.Errorf("unexpected result for overlap test for {rows=%d:%d, cols=%d:%d}%s with {rows=%d:%d, cols=%d:%d}%s: got:%t want:%t\n% v\n\n% v\n",
-							v.i, v.i+v.n, v.j, v.j+v.n, kindString(v.mat.Uplo == blas.Upper),
-							w.i, w.i+w.n, w.j, w.j+w.n, kindString(w.mat.Uplo == blas.Upper),
-							got, want,
-							Formatted(v.TriDense), Formatted(w.TriDense))
-					}
-				}
-			}
-		}
-	}
-}
-
 type interval struct{ from, to int }
 
 func intervalsOverlap(a, b interval) bool {
@@ -217,13 +134,6 @@ func overlapSiblingTriangles(ai, aj, an int, aKind TriKind, bi, bj, bn int, bKin
 	return false
 }
 
-func kindString(k TriKind) string {
-	if k == Upper {
-		return "U"
-	}
-	return "L"
-}
-
 // See https://github.com/gonum/matrix/issues/359 for details.
 func TestIssue359(t *testing.T) {
 	for xi := 0; xi < 2; xi++ {
@@ -247,29 +157,5 @@ func TestIssue359(t *testing.T) {
 				}
 			}
 		}
-	}
-}
-
-// denseAsTriDense returns a triangular matrix derived from the
-// square matrix m, with the orientation specified by kind.
-func denseAsTriDense(m *Dense, kind TriKind) *TriDense {
-	r, c := m.Dims()
-	if r != c {
-		panic(ErrShape)
-	}
-	n := r
-	uplo := blas.Lower
-	if kind == Upper {
-		uplo = blas.Upper
-	}
-	return &TriDense{
-		mat: blas64.Triangular{
-			N:      n,
-			Stride: m.mat.Stride,
-			Data:   m.mat.Data,
-			Uplo:   uplo,
-			Diag:   blas.NonUnit,
-		},
-		cap: n,
 	}
 }

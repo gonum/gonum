720f0f202eef1c360ad7dc725e3abcd881f51dfd
Vladimir Chalupecky <vladimir.chalupecky@gmail.com>
2017-12-12 18:14:03 +0100

    blas/gonum: enable test for Zhpmv

--- blas/gonum/level2cmplx128_test.go
+++ blas/gonum/level2cmplx128_test.go
@@ -34,6 +34,10 @@ func TestZher2(t *testing.T) {
 	testblas.Zher2Test(t, impl)
 }
 
+func TestZhpmv(t *testing.T) {
+	testblas.ZhpmvTest(t, impl)
+}
+
 func TestZhpr(t *testing.T) {
 	testblas.ZhprTest(t, impl)
 }

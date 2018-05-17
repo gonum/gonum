dd368688a5c963b761237365b0f0b044b94ca659
Sebastien Binet <binet@cern.ch>
2018-05-17 14:45:44 +0200

    travis,internal/imports: check imports with a command, not a test

---
+++ .travis/check-imports.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+set -e
+go run ./internal/imports/check-imports.go
---
+++ internal/imports/check-imports.go
@@ -0,0 +1,37 @@
+// Copyright ©2018 The Gonum Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build ignore
+
+package main
+
+import (
+	"log"
+	"os"
+
+	"gonum.org/v1/gonum/internal/imports"
+)
+
+var (
+	blacklist = []string{
+		"github.com/gonum/.*", // prefer gonum.org/v1/gonum
+		"math/rand",           // prefer golang.org/x/exp/rand
+	}
+)
+
+func main() {
+	log.SetPrefix("check-imports: ")
+	log.SetFlags(0)
+
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("could not retrieve current working directory: %v", err)
+	}
+	log.Printf("analyzing imports under %q...", dir)
+	err = imports.CheckBlacklisted(dir, blacklist)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("analyzing imports under %q... [OK]", dir)
+}

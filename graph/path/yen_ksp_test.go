53580f35e8235f2b42d99735dc0e3549c0a9f0bd
Rishi Desai <desai.rishi1@gmail.com>
2018-07-08 20:29:43 -0700

    updated a simple test case, need to change to TableDrivenTests

--- graph/path/yen_ksp_test.go
+++ graph/path/yen_ksp_test.go
@@ -5,11 +5,11 @@
 package path
 
 import (
-	"fmt"
 	"math"
 	"testing"
 
 	"gonum.org/v1/gonum/graph/simple"
+	"gonum.org/v1/gonum/graph"
 )
 
 
@@ -32,7 +32,33 @@ func TestYenKSP(t *testing.T) {
 		g.SetWeightedEdge(edge)
 	}
 
-	shortests := YenKSP(simple.Node(0), simple.Node(5), g, 3)
-	
-	fmt.Printf("%v", shortests)
+	shortests := YenKShortestPath(g, 3, simple.Node(0), simple.Node(5))
+	expected := make([][]graph.Node, 3)
+
+	expected[0] = make([]graph.Node, 4)
+	expected[0][0] = simple.Node(0)
+	expected[0][1] = simple.Node(2)
+	expected[0][2] = simple.Node(3)
+	expected[0][3] = simple.Node(5)
+
+	expected[1] = make([]graph.Node, 4)
+	expected[1][0] = simple.Node(0)
+	expected[1][1] = simple.Node(2)
+	expected[1][2] = simple.Node(4)
+	expected[1][3] = simple.Node(5)
+
+	expected[2] = make([]graph.Node, 4)
+	expected[2][0] = simple.Node(0)
+	expected[2][1] = simple.Node(1)
+	expected[2][2] = simple.Node(3)
+	expected[2][3] = simple.Node(5)
+
+	for i, sp := range shortests {
+		e := expected[i]
+		for n, p := range sp {
+			if (e[n].ID() != p.ID()) {
+				t.Errorf("ERROR: path #%d expected: %d, got: %d", i+1, e[n].ID(), p.ID())
+			}
+		}
+	}
 }

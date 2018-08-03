bb0faa4858985c56a0283326d43151f8f1ff37a9
Dan Kortschak <dan@kortschak.io>
2018-08-03 20:06:15 +0930

    graph/path: clean up yen and add additional tests

--- graph/path/yen_ksp.go
+++ graph/path/yen_ksp.go
@@ -10,12 +10,13 @@ import (
 	"gonum.org/v1/gonum/graph"
 )
 
-// YenKShortestPath returns the k-shortest loopless paths from s to t in g. YenKShortestPath will
-// panic if g contains a negative edge weight.
-func YenKShortestPath(g graph.Graph, k int, s, t graph.Node) [][]graph.Node {
+// YenKShortestPaths returns the k-shortest loopless paths from s to t in g.
+// YenKShortestPaths will panic if g contains a negative edge weight.
+func YenKShortestPaths(g graph.Graph, k int, s, t graph.Node) [][]graph.Node {
+	_, isDirected := g.(graph.Directed)
 	yk := yenKSPAdjuster{
-		g:       g,
-		visited: make(map[[2]int64]bool),
+		Graph:      g,
+		isDirected: isDirected,
 	}
 
 	if wg, ok := g.(Weighted); ok {
@@ -25,15 +26,20 @@ func YenKShortestPath(g graph.Graph, k int, s, t graph.Node) [][]graph.Node {
 	}
 
 	shortest, _ := DijkstraFrom(s, yk).To(t.ID())
-	if len(shortest) == 0 {
+	switch len(shortest) {
+	case 0:
 		return nil
+	case 1:
+		return [][]graph.Node{shortest}
 
 	}
 	paths := [][]graph.Node{shortest}
 
 	var pot []yenShortest
 	for i := int64(1); i < int64(k); i++ {
-		for n := 0; n < (len(paths[i-1]) - 1); n++ {
+		for n := 0; n < len(paths[i-1])-1; n++ {
+			yk.reset()
+
 			spur := paths[i-1][n]
 			root := make([]graph.Node, len(paths[i-1][:n+1]))
 			copy(root, paths[i-1][:n+1])
@@ -54,7 +60,7 @@ func YenKShortestPath(g graph.Graph, k int, s, t graph.Node) [][]graph.Node {
 						}
 					}
 					if ok {
-						yk.addVisited(path[n].ID(), path[n+1].ID())
+						yk.removeEdge(path[n].ID(), path[n+1].ID())
 					}
 				}
 			}
@@ -66,8 +72,6 @@ func YenKShortestPath(g graph.Graph, k int, s, t graph.Node) [][]graph.Node {
 			} else {
 				pot = append(pot, yenShortest{spath, weight})
 			}
-
-			yk.visited = make(map[[2]int64]bool)
 		}
 
 		if len(pot) == 0 {
@@ -75,13 +79,18 @@ func YenKShortestPath(g graph.Graph, k int, s, t graph.Node) [][]graph.Node {
 		}
 
 		sort.Sort(byPathWeight(pot))
-		paths = append(paths, pot[0].path)
+		best := pot[0].path
+		if len(best) <= 1 {
+			break
+		}
+		paths = append(paths, best)
 		pot = pot[1:]
 	}
 
 	return paths
 }
 
+// yenShortest holds a path and its weight for sorting.
 type yenShortest struct {
 	path   []graph.Node
 	weight float64
@@ -93,46 +102,52 @@ func (s byPathWeight) Len() int           { return len(s) }
 func (s byPathWeight) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
 func (s byPathWeight) Less(i, j int) bool { return s[i].weight < s[j].weight }
 
-// A wrapper for the graph imported to the YenKShotestPath functions.
-// This wrapper modifies the From function by not returning edges in the visited map.
-// The visited map is updated through each iteration of the YenKShortestPath function.
+// yenKSPAdjuster allows walked edges to be omitted from a graph
+// without altering the embedded graph.
 type yenKSPAdjuster struct {
-	g       graph.Graph
-	visited map[[2]int64]bool
-	weight  Weighting
+	graph.Graph
+	isDirected bool
+
+	// weight is the edge weight function
+	// used for shortest path calculation.
+	weight Weighting
+
+	// visitedEdges holds the edges that have
+	// been removed by Yen's algorithm.
+	visitedEdges map[[2]int64]struct{}
 }
 
 func (g yenKSPAdjuster) From(id int64) []graph.Node {
-	nodes := g.g.From(id)
+	nodes := g.Graph.From(id)
 
 	for i := 0; i < len(nodes); i++ {
-		if g.visited[[2]int64{id, nodes[i].ID()}] {
-			nodes[int64(i)] = nodes[len(nodes)-1]
-			nodes = nodes[:len(nodes)-1]
-			i--
+		if !g.canWalk(id, nodes[i].ID()) {
+			continue
 		}
+		nodes[int64(i)] = nodes[len(nodes)-1]
+		nodes = nodes[:len(nodes)-1]
+		i--
 	}
 
 	return nodes
 }
 
-func (g yenKSPAdjuster) addVisited(u, v int64) {
-	g.visited[[2]int64{u, v}] = true
-
-	/*
-		if g.isDirected {
-			g.visited[[2]int64{v, u}] = true
-		}*/
+func (g yenKSPAdjuster) canWalk(u, v int64) bool {
+	_, ok := g.visitedEdges[[2]int64{u, v}]
+	return ok
 }
 
-func (g yenKSPAdjuster) Edge(uid, vid int64) graph.Edge {
-	return g.g.Edge(uid, vid)
+func (g yenKSPAdjuster) removeEdge(u, v int64) {
+	g.visitedEdges[[2]int64{u, v}] = struct{}{}
+	if g.isDirected {
+		g.visitedEdges[[2]int64{v, u}] = struct{}{}
+	}
+}
+
+func (g *yenKSPAdjuster) reset() {
+	g.visitedEdges = make(map[[2]int64]struct{})
 }
 
 func (g yenKSPAdjuster) Weight(xid, yid int64) (w float64, ok bool) {
 	return g.weight(xid, yid)
-}
-
-func (g yenKSPAdjuster) HasEdgeBetween(xid, yid int64) bool {
-	return g.g.HasEdgeBetween(xid, yid)
 }
--- graph/path/yen_ksp_test.go
+++ graph/path/yen_ksp_test.go
@@ -7,9 +7,11 @@ package path
 import (
 	"math"
 	"reflect"
+	"sort"
 	"testing"
 
 	"gonum.org/v1/gonum/graph"
+	"gonum.org/v1/gonum/graph/internal/ordered"
 	"gonum.org/v1/gonum/graph/simple"
 )
 
@@ -21,11 +23,11 @@ var yenShortestPathTests = []struct {
 	query     simple.Edge
 	k         int
 	wantPaths [][]int64
+
+	relaxed bool
 }{
 	{
-		// Positive weighted graphs.
 		// https://en.wikipedia.org/w/index.php?title=Yen%27s_algorithm&oldid=841018784#Example
-
 		name:  "wikipedia example",
 		graph: func() graph.WeightedEdgeAdder { return simple.NewWeightedDirectedGraph(0, math.Inf(1)) },
 		edges: []simple.WeightedEdge{
@@ -48,27 +50,27 @@ var yenShortestPathTests = []struct {
 		},
 	},
 	{
-		name:  "1 Edge graph",
+		name:  "1 edge graph",
 		graph: func() graph.WeightedEdgeAdder { return simple.NewWeightedDirectedGraph(0, math.Inf(1)) },
 		edges: []simple.WeightedEdge{
 			{F: simple.Node(0), T: simple.Node(1), W: 3},
 		},
 		query: simple.Edge{F: simple.Node(0), T: simple.Node(1)},
-		k:     1,
+		k:     10,
 		wantPaths: [][]int64{
 			{0, 1},
 		},
 	},
 	{
-		name:      "Empty Graph",
+		name:      "empty graph",
 		graph:     func() graph.WeightedEdgeAdder { return simple.NewWeightedDirectedGraph(0, math.Inf(1)) },
 		edges:     []simple.WeightedEdge{},
 		query:     simple.Edge{F: simple.Node(0), T: simple.Node(1)},
 		k:         1,
-		wantPaths: [][]int64{},
+		wantPaths: nil,
 	},
 	{
-		name:  "N Star Graph",
+		name:  "n-star graph",
 		graph: func() graph.WeightedEdgeAdder { return simple.NewWeightedDirectedGraph(0, math.Inf(1)) },
 		edges: []simple.WeightedEdge{
 			{F: simple.Node(0), T: simple.Node(1), W: 3},
@@ -81,11 +83,93 @@ var yenShortestPathTests = []struct {
 			{0, 1},
 		},
 	},
+	{
+		name:  "bipartite small",
+		graph: func() graph.WeightedEdgeAdder { return simple.NewWeightedDirectedGraph(0, math.Inf(1)) },
+		edges: bipartite(5, 3, 0),
+		query: simple.Edge{F: simple.Node(-1), T: simple.Node(1)},
+		k:     10,
+		wantPaths: [][]int64{
+			{-1, 2, 1},
+			{-1, 3, 1},
+			{-1, 4, 1},
+			{-1, 5, 1},
+			{-1, 6, 1},
+		},
+	},
+	{
+		name:  "bipartite parity",
+		graph: func() graph.WeightedEdgeAdder { return simple.NewWeightedDirectedGraph(0, math.Inf(1)) },
+		edges: bipartite(5, 3, 0),
+		query: simple.Edge{F: simple.Node(-1), T: simple.Node(1)},
+		k:     5,
+		wantPaths: [][]int64{
+			{-1, 2, 1},
+			{-1, 3, 1},
+			{-1, 4, 1},
+			{-1, 5, 1},
+			{-1, 6, 1},
+		},
+	},
+	{
+		name:    "bipartite large",
+		graph:   func() graph.WeightedEdgeAdder { return simple.NewWeightedDirectedGraph(0, math.Inf(1)) },
+		edges:   bipartite(10, 3, 0),
+		query:   simple.Edge{F: simple.Node(-1), T: simple.Node(1)},
+		k:       5,
+		relaxed: true,
+	},
+	{
+		name:  "bipartite inc",
+		graph: func() graph.WeightedEdgeAdder { return simple.NewWeightedDirectedGraph(0, math.Inf(1)) },
+		edges: bipartite(5, 10, 1),
+		query: simple.Edge{F: simple.Node(-1), T: simple.Node(1)},
+		k:     5,
+		wantPaths: [][]int64{
+			{-1, 2, 1},
+			{-1, 3, 1},
+			{-1, 4, 1},
+			{-1, 5, 1},
+			{-1, 6, 1},
+		},
+	},
+	{
+		name:  "bipartite dec",
+		graph: func() graph.WeightedEdgeAdder { return simple.NewWeightedDirectedGraph(0, math.Inf(1)) },
+		edges: bipartite(5, 10, -1),
+		query: simple.Edge{F: simple.Node(-1), T: simple.Node(1)},
+		k:     5,
+		wantPaths: [][]int64{
+			{-1, 6, 1},
+			{-1, 5, 1},
+			{-1, 4, 1},
+			{-1, 3, 1},
+			{-1, 2, 1},
+		},
+	},
+}
+
+func bipartite(n int, weight, inc float64) []simple.WeightedEdge {
+	var edges []simple.WeightedEdge
+	for i := 2; i < n+2; i++ {
+		edges = append(edges,
+			simple.WeightedEdge{F: simple.Node(-1), T: simple.Node(i), W: weight},
+			simple.WeightedEdge{F: simple.Node(i), T: simple.Node(1), W: weight},
+		)
+		weight += inc
+	}
+	return edges
 }
 
 func pathIDs(paths [][]graph.Node) [][]int64 {
+	if paths == nil {
+		return nil
+	}
 	ids := make([][]int64, len(paths))
 	for i, p := range paths {
+		if p == nil {
+			continue
+		}
 		ids[i] = make([]int64, len(p))
 		for j, n := range p {
 			ids[i][j] = n.ID()
@@ -101,11 +185,53 @@ func TestYenKSP(t *testing.T) {
 			g.SetWeightedEdge(e)
 		}
 
-		got := YenKShortestPath(g.(graph.Graph), test.k, test.query.From(), test.query.To())
+		got := YenKShortestPaths(g.(graph.Graph), test.k, test.query.From(), test.query.To())
 		gotIDs := pathIDs(got)
+
+		paths := make(byPathWeight, len(gotIDs))
+		for i, p := range got {
+			paths[i] = yenShortest{path: p, weight: pathWeight(p, g.(graph.Weighted))}
+		}
+		if !sort.IsSorted(paths) {
+			t.Errorf("unexpected result for %q: got:%+v", test.name, paths)
+		}
+		if test.relaxed {
+			continue
+		}
+
+		if len(gotIDs) != 0 {
+			first := 0
+			last := pathWeight(got[0], g.(graph.Weighted))
+			for i := 1; i < len(got); i++ {
+				w := pathWeight(got[i], g.(graph.Weighted))
+				if w == last {
+					continue
+				}
+				sort.Sort(ordered.BySliceValues(gotIDs[first:i]))
+				first = i
+				last = w
+			}
+			sort.Sort(ordered.BySliceValues(gotIDs[first:]))
+		}
 
 		if !reflect.DeepEqual(test.wantPaths, gotIDs) {
 			t.Errorf("unexpected result for %q:\ngot: %v\nwant:%v", test.name, gotIDs, test.wantPaths)
 		}
+	}
+}
+
+func pathWeight(path []graph.Node, g graph.Weighted) float64 {
+	switch len(path) {
+	case 0:
+		return math.NaN()
+	case 1:
+		return 0
+	default:
+		var w float64
+		for i, u := range path[:len(path)-1] {
+			_w, _ := g.Weight(u.ID(), path[i+1].ID())
+			w += _w
+		}
+		return w
 	}
 }

0d303f20712796ddda9ec09ea22a6facc5cb10d5
vthiery <vjmthiery@gmail.com>
2018-10-05 09:01:55 +0200

    Use more succint and clearer comment

--- stat/stat.go
+++ stat/stat.go
@@ -1076,8 +1076,7 @@ func empiricalQuantile(p float64, x, weights []float64, sumWeights float64) floa
 // Skew computes the skewness of the sample data.
 // If weights is nil then all of the weights are 1. If weights is not nil, then
 // len(x) must equal len(weights).
-// Note that in case of weights summing to less than (or equal) to 1,
-// a biased variance estimator must be used instead
+// When weights sum to 1 or less, a biased variance estimator should be used.
 func Skew(x, weights []float64) float64 {
 
 	mean, std := MeanStdDev(x, weights)
@@ -1198,8 +1197,7 @@ func StdDev(x, weights []float64) float64 {
 }
 
 // MeanStdDev returns the sample mean and unbiased standard deviation
-// Note that in case of weights summing to less than (or equal) to 1,
-// a biased variance estimator must be used instead
+// When weights sum to 1 or less, a biased variance estimator should be used.
 func MeanStdDev(x, weights []float64) (mean, std float64) {
 	mean, variance := MeanVariance(x, weights)
 	return mean, math.Sqrt(variance)
@@ -1221,8 +1219,7 @@ func StdScore(x, mean, std float64) float64 {
 //  \sum_i w_i (x_i - mean)^2 / (sum_i w_i - 1)
 // If weights is nil then all of the weights are 1. If weights is not nil, then
 // len(x) must equal len(weights).
-// Note that in case of weights summing to less than (or equal) to 1,
-// a biased variance estimator must be used instead
+// When weights sum to 1 or less, a biased variance estimator should be used.
 func Variance(x, weights []float64) float64 {
 	_, variance := MeanVariance(x, weights)
 	return variance
@@ -1234,8 +1231,7 @@ func Variance(x, weights []float64) float64 {
 // respectively.
 // If weights is nil then all of the weights are 1. If weights is not nil, then
 // len(x) must equal len(weights).
-// Note that in case of weights summing to less than (or equal) to 1,
-// a biased variance estimator must be used instead
+// When weights sum to 1 or less, a biased variance estimator should be used.
 func MeanVariance(x, weights []float64) (mean, variance float64) {
 	// This uses the corrected two-pass algorithm (1.7), from "Algorithms for computing
 	// the sample variance: Analysis and recommendations" by Chan, Tony F., Gene H. Golub,

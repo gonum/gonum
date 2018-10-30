f7ac3c7d17d575b0e524f89cdba059f9d4c0b125
Dan Kortschak <dan@kortschak.io>
2018-10-31 09:37:29 +1030

    quat: add link to eater.net/quaternions

--- num/quat/doc.go
+++ num/quat/doc.go
@@ -3,4 +3,8 @@
 // license that can be found in the LICENSE file.
 
 // Package quat provides the quaternion numeric type and functions.
+//
+// For a good treatment of uses and behaviors of quaternions, see
+// the interactive videos by Ben Eater and Grant Sanderson here
+// https://eater.net/quaternions.
 package quat // imports "gonum.org/v1/gonum/num/quat"

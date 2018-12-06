b68aa0d675f37738c7a3281bce51eab4a594c416
Dezmond Goff <goff.dezmond@gmail.com>
2018-12-06 13:08:53 -0800

    stat/distuv: improve Binomial documentation

--- stat/distuv/binomial.go
+++ stat/distuv/binomial.go
@@ -16,13 +16,13 @@ import (
 // Binomial implements the binomial distribution, a discrete probability distribution
 // that expresses the probability of a given number of successful Bernoulli trials
 // out of a total of n, each with successs probability p.
-// The binomial distribution has density function:
+// The binomial distribution has the density function:
 //  f(k) = (n choose k) p^k (1-p)^(n-k)
 // For more information, see https://en.wikipedia.org/wiki/Binomial_distribution.
 type Binomial struct {
 	// N is the total number of Bernoulli trials. N must be greater than 0.
 	N float64
-	// P is the probablity of success in any given trial. P must be on [0, 1].
+	// P is the probablity of success in any given trial. P must be in [0, 1].
 	P float64
 
 	Src rand.Source
@@ -75,24 +75,23 @@ func (b Binomial) Prob(x float64) float64 {
 func (b Binomial) Rand() float64 {
 	// NUMERICAL RECIPES IN C: THE ART OF SCIENTIFIC COMPUTING (ISBN 0-521-43108-5)
 	// p. 295-6
-	// <http://www.aip.de/groups/soe/local/numres/bookcpdf/c7-3.pdf>
+	// http://www.aip.de/groups/soe/local/numres/bookcpdf/c7-3.pdf
 
 	runif := rand.Float64
 	rexp := rand.ExpFloat64
-	var rng *rand.Rand
 	if b.Src != nil {
-		rng = rand.New(b.Src)
-		runif = rng.Float64
-		rexp = rng.ExpFloat64
+		rnd := rand.New(b.Src)
+		runif = rnd.Float64
+		rexp = rnd.ExpFloat64
 	}
 
 	p := b.P
 	if p > 0.5 {
-		p = 1.0 - p
+		p = 1 - p
 	}
 	am := b.N * p
 
-	if b.N < 25.0 {
+	if b.N < 25 {
 		// Use direct method.
 		bnl := 0.0
 		for i := 0; i < int(b.N); i++ {
@@ -106,12 +105,12 @@ func (b Binomial) Rand() float64 {
 		return bnl
 	}
 
-	if am < 1.0 {
+	if am < 1 {
 		// Use rejection method with Poisson proposal.
+		const logM = 2.6e-2 // constant for rejection sampling (https://en.wikipedia.org/wiki/Rejection_sampling)
 		var bnl float64
 		z := -p
-		pclog := (1.0 + 0.5*z) * z / (1.0 + (1.0+1.0/6*z)*z)
-
+		pclog := (1 + 0.5*z) * z / (1 + (1+1.0/6*z)*z) // Padé approximant of log(1 + x)
 		for {
 			bnl = 0.0
 			t := 0.0
@@ -124,8 +123,8 @@ func (b Binomial) Rand() float64 {
 			}
 			bnlc := b.N - bnl
 			z = -bnl / b.N
-			log1p := (1.0 + 0.5*z) * z / (1.0 + (1.0+1.0/6*z)*z)
-			t = (bnlc+0.5)*log1p + bnl - bnlc*pclog + 1/(12*bnlc) - am + 2.6e-02
+			log1p := (1 + 0.5*z) * z / (1 + (1+1.0/6*z)*z)
+			t = (bnlc+0.5)*log1p + bnl - bnlc*pclog + 1/(12*bnlc) - am + logM // Uses Stirling's expansion of log(n!)
 			if rexp() >= t {
 				break
 			}
@@ -135,25 +134,30 @@ func (b Binomial) Rand() float64 {
 		}
 		return bnl
 	}
+	// Original algorithm samples from a Poisson distribution with the
+	// appropriate expected value. However, the Poisson approximation is
+	// asymptotic such that the absolute deviation in probability is O(1/n).
+	// Rejection sampling produces exact variates with at worst less than 3%
+	// rejection with miminal additional computation.
 
 	// Use rejection method with Cauchy proposal.
-	g, _ := math.Lgamma(b.N + 1.0)
+	g, _ := math.Lgamma(b.N + 1)
 	plog := math.Log(p)
 	pclog := math.Log1p(-p)
-	sq := math.Sqrt(2.0 * am * (1.0 - p))
+	sq := math.Sqrt(2 * am * (1 - p))
 	for {
 		var em, y float64
 		for {
 			y = math.Tan(math.Pi * runif())
 			em = sq*y + am
-			if em >= 0 && em < b.N+1.0 {
+			if em >= 0 && em < b.N+1 {
 				break
 			}
 		}
 		em = math.Floor(em)
 		lg1, _ := math.Lgamma(em + 1)
 		lg2, _ := math.Lgamma(b.N - em + 1)
-		t := 1.2 * sq * (1.0 + y*y) * math.Exp(g-lg1-lg2+em*plog+(b.N-em)*pclog)
+		t := 1.2 * sq * (1 + y*y) * math.Exp(g-lg1-lg2+em*plog+(b.N-em)*pclog)
 		if runif() <= t {
 			if p != b.P {
 				return b.N - em

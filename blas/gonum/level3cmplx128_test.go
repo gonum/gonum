a7c15425b33ad4b547c662caeda3ebcab97884b7
Vladimir Chalupecky <vladimir.chalupecky@gmail.com>
2019-01-06 21:45:31 +0100

    blas/gonum: add Zgemm with test

---
+++ blas/gonum/level3cmplx128_test.go
@@ -0,0 +1,15 @@
+// Copyright ©2019 The Gonum Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package gonum
+
+import (
+	"testing"
+
+	"gonum.org/v1/gonum/blas/testblas"
+)
+
+func TestZgemm(t *testing.T) {
+	testblas.ZgemmTest(t, impl)
+}

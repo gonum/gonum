14c5f9c2d58503e07a4ebf3f90984c9bed8aa7f8
Vladimir Chalupecky <vladimir.chalupecky@gmail.com>
2019-01-10 21:28:07 +0100

    blas/gonum: add Zher2k with test

--- blas/gonum/level3cmplx128_test.go
+++ blas/gonum/level3cmplx128_test.go
@@ -12,5 +12,6 @@ import (
 
 func TestZgemm(t *testing.T)  { testblas.ZgemmTest(t, impl) }
 func TestZherk(t *testing.T)  { testblas.ZherkTest(t, impl) }
+func TestZher2k(t *testing.T) { testblas.Zher2kTest(t, impl) }
 func TestZsyrk(t *testing.T)  { testblas.ZsyrkTest(t, impl) }
 func TestZsyr2k(t *testing.T) { testblas.Zsyr2kTest(t, impl) }

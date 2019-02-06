66532af93a713088d1eceeebea1765f43a8d850c
Dan Kortschak <dan@kortschak.io>
2019-02-06 19:46:49 +1030

    travis: ignore changes to go.mod during diff checks

--- .travis/check-generate.sh
+++ .travis/check-generate.sh
@@ -5,6 +5,7 @@ go generate gonum.org/v1/gonum/blas/gonum
 go generate gonum.org/v1/gonum/unit
 go generate gonum.org/v1/gonum/graph/formats/dot
 if [ -n "$(git diff)" ]; then
+	git checkout -- go.mod # Discard changes to go.mod that have been made.
 	git diff
 	exit 1
 fi

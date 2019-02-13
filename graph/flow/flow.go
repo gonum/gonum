3d87b32d6dfd2db9bc2f1930c642500dcbd08068
Robin Eklind <rnd0x00@gmail.com>
2019-02-14 00:15:34 +0900

    cfa: make Graph a concrete type instead of an interface

--- graph/flow/flow.go
+++ graph/flow/flow.go
@@ -7,29 +7,14 @@ package flow
 import "gonum.org/v1/gonum/graph"
 
 // Graph is a control flow graph, a directed graph rooted at an entry node.
-type Graph interface {
-	// Entry returns the entry node of the control flow graph.
-	Entry() graph.Node
+type Graph struct {
+	// Underlying directed graph.
 	graph.Directed
+	// Root node of the control flow graph.
+	RootID int64
 }
 
-// NewGraph returns a new control flow graph based on the given directed graph
-// and entry node ID.
-func NewGraph(g graph.Directed, entryid int64) Graph {
-	return &cfg{
-		Directed: g,
-		entry:    g.Node(entryid),
-	}
-}
-
-// cfg is a control flow graph rooted at an entry node.
-type cfg struct {
-	// Entry node of control flow graph.
-	entry graph.Node
-	graph.Directed
-}
-
-// Entry returns the entry node of the control flow graph.
-func (g *cfg) Entry() graph.Node {
-	return g.entry
+// Root returns the root node of the control flow graph.
+func (g Graph) Root() graph.Node {
+	return g.Directed.Node(g.RootID)
 }

5c4cea69639dec6a6f4f552d8cf182e0e2bc10e3
Dan Kortschak <dan@kortschak.io>
2019-02-22 08:50:35 +1030

    travis: check that we work with modules

--- .travis/check-generate.sh
+++ .travis/check-generate.sh
@@ -4,8 +4,13 @@ go generate gonum.org/v1/gonum/blas
 go generate gonum.org/v1/gonum/blas/gonum
 go generate gonum.org/v1/gonum/unit
 go generate gonum.org/v1/gonum/graph/formats/dot
-if [ -n "$(git diff)" ]; then
-	git checkout -- go.mod # Discard changes to go.mod that have been made.
+
+# Discard changes to go.mod that have been made.
+# FIXME(kortschak): Sort out a policy of what we should do with go.mod changes.
+# If we want to check changes we should set `GOFLAGS=-mod=readonly` in .travis.yml.
+git checkout -- go.mod
+
+if [ -n "$(git diff)" ]; then	
 	git diff
 	exit 1
 fi

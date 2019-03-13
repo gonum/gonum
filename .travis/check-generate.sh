5950af8e58b66e844a919a2181357da98e72f55d
Dan Kortschak <dan@kortschak.io>
2019-03-13 21:37:45 +1030

    modules: add gonum.org/v1/netlib to go.mod and commit go.sum

--- .travis/check-generate.sh
+++ .travis/check-generate.sh
@@ -8,7 +8,7 @@ go generate gonum.org/v1/gonum/graph/formats/dot
 # Discard changes to go.mod that have been made.
 # FIXME(kortschak): Sort out a policy of what we should do with go.mod changes.
 # If we want to check changes we should set `GOFLAGS=-mod=readonly` in .travis.yml.
-git checkout -- go.mod
+git checkout -- go.{mod,sum}
 
 if [ -n "$(git diff)" ]; then	
 	git diff

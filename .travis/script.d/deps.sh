3bf5a6b3c2f3a5115eeb32e55c70cb5cdf21f11a
Dan Kortschak <dan@kortschak.io>
2019-04-27 20:38:42 +0930

    travis: check for needed changes in go.mod and go.sum

--- .travis/script.d/deps.sh
+++ .travis/script.d/deps.sh
@@ -2,6 +2,13 @@
 
 set -ex
 
+# Avoid contaminating the go.mod/go.sum files.
+pushd ~
+
+# Work around go1.11 issue where go get will not work outside a module.
+# TODO(kortschak): Remove when go1.11 is no longer supported.
+go mod init fake || true
+
 # Required for format check.
 go get golang.org/x/tools/cmd/goimports
 # Required for imports check.
@@ -13,3 +20,9 @@ go get golang.org/x/tools/cmd/cover
 go get github.com/mattn/goveralls
 # Required for dot parser checks.
 ${TRAVIS_BUILD_DIR}/.travis/script.d/install-gocc.sh 66c61e91b3657c517a6f89d2837d370e61fb9430
+
+# Clean up.
+# TODO(kortschak): Remove when go1.11 is no longer supported.
+rm go.{mod,sum} || true
+
+popd

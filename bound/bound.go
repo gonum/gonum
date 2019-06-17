ad28666ca8853fe8272c9bdafd61c2dd76c3a604
Dan Kortschak <dan@kortschak.io>
2019-06-17 11:24:21 +0930

    Revert "bound: rewrite tests to use tables and fix unsorted input handling for Union"
    This reverts commit b869779d1d53963e9e9eafca2d8aac6d4dcfa663.
    
    Reason for reversion: It is not clear that we know how this should be done yet.

--- bound/bound.go
+++ bound/bound.go
@@ -4,24 +4,20 @@
 
 package bound
 
-import (
-	"math"
-	"sort"
-)
+import "math"
 
 // Bound represents [Min, Max] bounds.
 type Bound struct {
 	Min, Max float64
 }
 
-// IsValid returns whether the bound is valid. A valid bound will have
-// the minimum less than or equal to the maximum.
+// IsValid returns whether the bound is valid
 func (b Bound) IsValid() bool {
 	return b.Min <= b.Max
 }
 
-// Intersection returns the intersection of the input bounds. If the
-// intersection is empty an invalid Bound is returned.
+// Intersection returns the intersection of the input bounds if possible.
+// Otherwise a NaN Bound is returned.
 func Intersection(bounds ...Bound) Bound {
 	if len(bounds) == 0 {
 		return Bound{Min: math.NaN(), Max: math.NaN()}
@@ -41,15 +37,10 @@ func Intersection(bounds ...Bound) Bound {
 }
 
 // Union returns the contiguous union of the input bounds if possible.
-// Otherwise an invalid Bound is returned. If bounds is a slice of Bound
-// and is not sorted, the order of elements will be changed so that
-// they are ordered ascending by Min.
+// Otherwise a NaN Bound is returned.
 func Union(bounds ...Bound) Bound {
 	if len(bounds) == 0 {
 		return Bound{Min: math.NaN(), Max: math.NaN()}
-	}
-	if len(bounds) > 1 && !sort.IsSorted(byMin(bounds)) {
-		sort.Sort(byMin(bounds))
 	}
 
 	union := Bound{Min: bounds[0].Min, Max: bounds[0].Max}
@@ -67,9 +58,3 @@ func Union(bounds ...Bound) Bound {
 
 	return union
 }
-
-type byMin []Bound
-
-func (b byMin) Len() int           { return len(b) }
-func (b byMin) Less(i, j int) bool { return b[i].Min < b[j].Max }
-func (b byMin) Swap(i, j int)      { b[i], b[j] = b[j], b[i] }

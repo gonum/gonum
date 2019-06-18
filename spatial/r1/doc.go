83b5944b73820544212b4ee0b08b71a61b2ca60f
Sebastien Binet <binet@cern.ch>
2019-06-18 12:50:15 +0200

    spatial/r1: PR-2

--- spatial/r1/doc.go
+++ spatial/r1/doc.go
@@ -2,5 +2,5 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package r1 provides 1D vectors and boxes and operations on them.
+// Package r1 provides 1D vectors and intervals and operations on them.
 package r1 // import "gonum.org/v1/gonum/spatial/r1"

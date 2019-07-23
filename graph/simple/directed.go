6d17b310dadef26babfb85c5166afb2d793806aa
Dan Kortschak <dan@kortschak.io>
2019-07-23 15:17:48 +0930

    graph/simple: don't allocate edge maps before they are needed
    Also fix incorrect optimisation in WeightedDirected.Nodes.

--- graph/simple/directed.go
+++ graph/simple/directed.go
@@ -49,8 +49,6 @@ func (g *DirectedGraph) AddNode(n graph.Node) {
 		panic(fmt.Sprintf("simple: node ID collision: %d", n.ID()))
 	}
 	g.nodes[n.ID()] = n
-	g.from[n.ID()] = make(map[int64]graph.Edge)
-	g.to[n.ID()] = make(map[int64]graph.Edge)
 	g.nodeIDs.Use(n.ID())
 }
 
@@ -212,8 +210,16 @@ func (g *DirectedGraph) SetEdge(e graph.Edge) {
 		g.nodes[tid] = to
 	}
 
-	g.from[fid][tid] = e
-	g.to[tid][fid] = e
+	if fm, ok := g.from[fid]; ok {
+		fm[tid] = e
+	} else {
+		g.from[fid] = map[int64]graph.Edge{tid: e}
+	}
+	if tm, ok := g.to[tid]; ok {
+		tm[fid] = e
+	} else {
+		g.to[tid] = map[int64]graph.Edge{fid: e}
+	}
 }
 
 // To returns all nodes in g that can reach directly to n.

fcfcbaadebfeca2116b427e8f1bfdb93df488266
Sebastian Waisbrot <seppo0010@gmail.com>
2019-08-29 10:52:32 -0300

    Feedback on normality coding style and comments

--- stat/testuv/norm.go
+++ stat/testuv/norm.go
@@ -1,26 +1,28 @@
+// Copyright ©2014 The Gonum Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package testuv
 
 import (
-	"fmt"
 	"math"
 
 	"gonum.org/v1/gonum/stat"
 )
 
-// NormalSkewTest test whether the skew is different from the normal distribution.
+// NormalSkew test whether the skew is different from the normal distribution.
 // This function tests the null hypothesis that the skewness of population that the
 // sample was drawn from is the same as that of a corresponding normal distribution.
 // The length of a must be at least 8.
-func NormalSkewTest(a []float64) float64 {
-	// Implementation based on scipy's: https://github.com/scipy/scipy/blob/v1.3.0/scipy/stats/stats.py#L1481-L1549
+func NormalSkew(a []float64) float64 {
+	// Implementation based on scipy's: https://github.com/scipy/scipy/blob/v1.3.0/scipy/stats/stats.py#L1307-L1382
 	b2 := stat.Skew(a, nil)
-	n := len(a)
-	if n < 8 {
-		panic(fmt.Sprintf("testuv: skewtest is not valid with less than 8 samples, %d samples were given", n))
+	if len(a) < 8 {
+		panic("testuv: not enough samples for normal skew test")
 	}
-	nf := float64(n)
-	y := b2 * math.Sqrt(((nf+1)*(nf+3))/(6*(nf-2)))
-	beta2 := (3 * (math.Pow(nf, 2) + 27*nf - 70) * (nf + 1) * (nf + 3) / ((nf - 2) * (nf + 5) * (nf + 7) * (nf + 9)))
+	n := float64(len(a))
+	y := b2 * math.Sqrt(((n+1)*(n+3))/(6*(n-2)))
+	beta2 := (3 * (math.Pow(n, 2) + 27*n - 70) * (n + 1) * (n + 3) / ((n - 2) * (n + 5) * (n + 7) * (n + 9)))
 	w2 := -1 + math.Sqrt(2*(beta2-1))
 	delta := 1 / math.Sqrt(0.5*math.Log(w2))
 	alpha := math.Sqrt(2 / (w2 - 1))
@@ -30,23 +32,22 @@ func NormalSkewTest(a []float64) float64 {
 	return delta * math.Log(y/alpha+math.Sqrt(math.Pow(y/alpha, 2)+1))
 }
 
-// NormalKurtosisTest test whether a dataset has normal kurtosis.
+// NormalKurtosis test whether a dataset has normal kurtosis.
 // This function tests the null hypothesis that the kurtosis of the population from
 // which the sample was drawn is that of the normal distribution:
 // `kurtosis = 3(n-1)/(n+1)`.
 // The length of a must be at least 5.
-func NormalKurtosisTest(a []float64) float64 {
-	// Implementation based on scipy's: https://github.com/scipy/scipy/blob/v1.3.0/scipy/stats/stats.py#L1481-L1549
-	n := len(a)
-	if n < 5 {
-		panic(fmt.Sprintf("testuv: kurtosistest requires at least 5 observations; %d observations were given", n))
+func NormalKurtosis(a []float64) float64 {
+	// Implementation based on scipy's: https://github.com/scipy/scipy/blob/v1.3.0/scipy/stats/stats.py#L1388-L1475
+	if len(a) < 5 {
+		panic("testuv: not enough samples for normal kurtosis test")
 	}
-	nf := float64(n)
+	n := float64(len(a))
 	b2 := stat.ExKurtosis(a, nil) + 3
-	e := 3.0 * (nf - 1) / (nf + 1)
-	varb2 := 24 * nf * (nf - 2) * (nf - 3) / ((nf + 1) * (nf + 1.) * (nf + 3) * (nf + 5))
+	e := 3.0 * (n - 1) / (n + 1)
+	varb2 := 24 * n * (n - 2) * (n - 3) / ((n + 1) * (n + 1.) * (n + 3) * (n + 5))
 	x := (b2 - e) / math.Sqrt(varb2)
-	sqrtbeta1 := 6 * (nf*nf - 5*nf + 2) / ((nf + 7) * (nf + 9)) * math.Sqrt((6*(nf+3)*(nf+5))/(nf*(nf-2)*(nf-3)))
+	sqrtbeta1 := 6 * (n*n - 5*n + 2) / ((n + 7) * (n + 9)) * math.Sqrt((6*(n+3)*(n+5))/(n*(n-2)*(n-3)))
 	a2 := 6 + 8/sqrtbeta1*(2/sqrtbeta1+math.Sqrt(1+4/math.Pow(sqrtbeta1, 2)))
 	term1 := 1 - 2/(9*a2)
 	denom := 1 + x*math.Sqrt(2/(a2-4))
@@ -57,7 +58,7 @@ func NormalKurtosisTest(a []float64) float64 {
 	return (term1 - term2) / math.Sqrt(2/(9*a2))
 }
 
-// NormalTest test whether a sample differs from a normal distribution.
+// Normal test whether a sample differs from a normal distribution.
 // This function tests the null hypothesis that a sample comes from a normal distribution.
 // It is based on D'Agostino and Pearson's [1]_, [2]_ test that combines skew and kurtosis to
 // produce an omnibus test of normality.
@@ -67,11 +68,11 @@ func NormalKurtosisTest(a []float64) float64 {
 // ----------
 // .. [1] D'Agostino, R. B. (1971), "An omnibus test of normality for
 //        moderate and large sample size", Biometrika, 58, 341-348
-// .. [2] D'Agostino, R. and Pearson, E. S. (1973), "Tests for departure from
+// .. [2] D'Agostino, R. and Pearson, E. S. (1973), "s for departure from
 //        normality", Biometrika, 60, 613-622
-func NormalTest(a []float64) float64 {
+func Normal(a []float64) float64 {
 	// Implementation based on scipy's: https://github.com/scipy/scipy/blob/v1.3.0/scipy/stats/stats.py#L1481-L1549
-	s := NormalSkewTest(a)
-	k := NormalKurtosisTest(a)
+	s := NormalSkew(a)
+	k := NormalKurtosis(a)
 	return s*s + k*k
 }
--- stat/testuv/norm_test.go
+++ stat/testuv/norm_test.go
@@ -1,3 +1,7 @@
+// Copyright ©2014 The Gonum Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package testuv
 
 import (
@@ -5,73 +9,75 @@ import (
 	"testing"
 
 	"golang.org/x/exp/rand"
+	"gonum.org/v1/gonum/floats"
 	"gonum.org/v1/gonum/stat/distuv"
 )
 
-func getNormalValues() []float64 {
+const tol = 1e-7
+
+func rnorm(n int) []float64 {
 	normal := distuv.Normal{Mu: 0, Sigma: 1, Src: rand.New(rand.NewSource(1))}
-	size := 100
-	nums := make([]float64, size)
+	nums := make([]float64, n)
 	for i := range nums {
 		nums[i] = normal.Rand()
 	}
 	return nums
 }
 
-func TestNormalSkewTest(t *testing.T) {
+func TestNormalSkew(t *testing.T) {
 	// Even though the skew test and kurtosis test are a copy from the scipy's the results
 	// are not exactly the same because the skew/kurtosis corrections are not exactly the same
 	// but the statistic value is close enough.
 	for i, test := range []struct {
-		values   []float64
-		expected float64
+		values []float64
+		want   float64
 	}{
 		{
-			values:   []float64{0, 1, 2, 3, 4, 5, 6, 7, 8},
-			expected: 1.018464355396213,
+			values: []float64{0, 1, 2, 3, 4, 5, 6, 7, 8},
+			want:   1.018464355396213,
 		},
 		{
-			values:   []float64{2, 8, 0, 4, 1, 9, 9, 0},
-			expected: 0.5562585562766172,
+			values: []float64{2, 8, 0, 4, 1, 9, 9, 0},
+			want:   0.5562585562766172,
 		},
 		{
-			values:   []float64{1, 2, 3, 4, 5, 6, 7, 8000},
-			expected: 4.319816401673864,
+			values: []float64{1, 2, 3, 4, 5, 6, 7, 8000},
+			want:   4.319816401673864,
 		},
 		{
-			values:   []float64{100, 100, 100, 100, 100, 100, 100, 101},
-			expected: 4.319820025201098,
+			values: []float64{100, 100, 100, 100, 100, 100, 100, 101},
+			want:   4.319820025201098,
 		},
 	} {
-		z := NormalSkewTest(test.values)
-		if math.Abs(z-test.expected) > 1e-7 {
-			t.Errorf("NormalSkewTest mismatch case %d. Expected %v, Found %v", i, test.expected, z)
+		z := NormalSkew(test.values)
+		if floats.EqualWithinAbsOrRel(z, test.want, tol, tol) {
+			t.Errorf("NormalSkew mismatch case %d. Expected %v, Found %v", i, test.want, z)
 		}
 	}
 }
 
-func TestNormalKurtosisTest(t *testing.T) {
+func TestNormalKurtosis(t *testing.T) {
 	for i, test := range []struct {
-		values   []float64
-		expected float64
+		values []float64
+		want   float64
 	}{
 		{
-			values:   []float64{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19},
-			expected: -1.6867033202073243,
+			values: []float64{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19},
+			want:   -1.6867033202073243,
 		},
 		{
-			values:   getNormalValues(),
-			expected: -1.437284826863465,
+			values: rnorm(100),
+			want:   -1.437284826863465,
 		},
 	} {
-		z := NormalKurtosisTest(test.values)
-		if math.Abs(z-test.expected) > 1e-7 {
-			t.Errorf("NormalKurtosisTest mismatch case %d. Expected %v, Found %v", i, test.expected, z)
+		z := NormalKurtosis(test.values)
+		if math.Abs(z-test.want) > 1e-7 {
+			t.Errorf("NormalKurtosis mismatch case %d. Expected %v, Found %v", i, test.want, z)
 		}
 	}
 }
 
-func TestNormalTest(t *testing.T) {
+func TestNormal(t *testing.T) {
 	// normal test with scipy yield similar results:
 	// ```
 	// from scipy.stats import normaltest
@@ -83,37 +89,37 @@ func TestNormalTest(t *testing.T) {
 	// ```
 
 	for i, test := range []struct {
-		values   []float64
-		expected float64
+		values []float64
+		want   float64
 	}{
 		{
-			values:   []float64{0, 1, 2, 3, 4, 5, 6, 7, 8},
-			expected: 1.7509245653153176,
+			values: []float64{0, 1, 2, 3, 4, 5, 6, 7, 8},
+			want:   1.7509245653153176,
 		},
 		{
-			values:   []float64{2, 8, 0, 4, 1, 9, 9, 0},
-			expected: 11.454757293481551,
+			values: []float64{2, 8, 0, 4, 1, 9, 9, 0},
+			want:   11.454757293481551,
 		},
 		{
-			values:   []float64{1, 2, 3, 4, 5, 6, 7, 8000},
-			expected: 40.53534243515444,
+			values: []float64{1, 2, 3, 4, 5, 6, 7, 8000},
+			want:   40.53534243515444,
 		},
 		{
-			values:   []float64{100, 100, 100, 100, 100, 100, 100, 101},
-			expected: 40.53539760601764,
+			values: []float64{100, 100, 100, 100, 100, 100, 100, 101},
+			want:   40.53539760601764,
 		},
 		{
-			values:   []float64{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19},
-			expected: 3.9272951079276743,
+			values: []float64{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19},
+			want:   3.9272951079276743,
 		},
 		{
-			values:   getNormalValues(),
-			expected: 2.083696972397116,
+			values: rnorm(100),
+			want:   2.083696972397116,
 		},
 	} {
-		z := NormalTest(test.values)
-		if math.Abs(z-test.expected) > 1e-7 {
-			t.Errorf("NormalTest mismatch case %d. Expected %v, Found %v", i, test.expected, z)
+		z := Normal(test.values)
+		if floats.EqualWithinAbsOrRel(z, test.want, tol, tol) {
+			t.Errorf("Normal mismatch case %d. Expected %v, Found %v", i, test.want, z)
 		}
 	}
 }

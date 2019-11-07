e27eddbd008f7346f31f5f982c2362b824cd90f6
Dan Kortschak <dan@kortschak.io>
2019-11-07 19:50:40 +1030

    spectral: new package for spectral graph analysis
    Initial commit to this is just a move of the Laplacian type from network.

---
+++ graph/spectral/doc.go
@@ -0,0 +1,6 @@
+// Copyright ©2019 The Gonum Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package spectral provides graph spectral analysis functions.
+package spectral // import "gonum.org/v1/gonum/graph/spectral"

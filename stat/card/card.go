649430eb46da9a4ba3cf818ac60b54f9b4e983cd
Dan Kortschak <dan@kortschak.io>
2019-11-10 11:40:38 +1030

    stat/card: new package for cardinality estimation

---
+++ stat/card/card.go
@@ -0,0 +1,38 @@
+// Copyright ©2019 The Gonum Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:generate ./generate_64bit.sh
+
+package card
+
+import "math"
+
+const (
+	w32 = 32
+	w64 = 64
+)
+
+func alpha(m uint64) float64 {
+	if m < 128 {
+		return alphaValues[m]
+	}
+	return 0.7213 / (1 + 1.079/float64(m))
+}
+
+var alphaValues = [...]float64{
+	16: 0.673,
+	32: 0.697,
+	64: 0.709,
+}
+
+func linearCounting(m, v float64) float64 {
+	return m * math.Log(m/v)
+}
+
+func max(a, b uint8) uint8 {
+	if a > b {
+		return a
+	}
+	return b
+}

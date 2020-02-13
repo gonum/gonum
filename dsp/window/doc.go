2d94a32a3d601d95985e989394c144949c60ec1f
Blinnikov Andrei <goofinator@mail.ru>
2020-02-13 14:40:04 +0300

    docs(dsp/window/doc.go): apply review's demands on documentation.

--- dsp/window/doc.go
+++ dsp/window/doc.go
@@ -2,12 +2,17 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package window provides a set of functions to perform the transformation of sequence by different window functions.
-// The main purpose of the window function is to control spectral leakage parameters when performing Fourier transform on a signal of limit length.
+// Package window provides a set of functions
+// to perform the transformation of sequence by different window functions.
 //
-// Indicators of quality
+// Window functions can be used to control spectral leakage parameters
+// when performing a Fourier transform on a signal of limit length.
 //
-// To ensure the selection criteria for a window function, each function is documented by a set of Indicators of quality. Here is a description.
+// Spectral leakage parameters
+//
+// To ensure the selection criteria for a window function,
+// each function is documented by a set of spectral leakage parameters.
+// Here is a description.
 //
 // ΔF_0 -
 // Normalized width of the main AFC lobe at zero amplitude level.
@@ -17,15 +22,23 @@
 //
 // K -
 // For a rectangular window, ΔF_0 has the smallest value and is equal to 2.
-// K Shows how many times the normalized width ΔF_0 of the main lobe by the zero level of the specified window is wider than the rectangular window.
-// Depending on the parameter, the windows are divided into high resolution windows (K≤3) and low resolution windows (K>3).
+// K Shows how many times the normalized width ΔF_0
+// of the main lobe by the zero level of the specified window
+// is wider than the rectangular window.
+// Depending on the parameter, the windows are divided into
+// high resolution windows (K≤3) and low resolution windows (K>3).
 //
 // ɣ_max -
 // The maximum level of side lobes.
-// If the maximum level of side lobes divided by maximum level of main lobe is a g, the ɣ_max is a g expressed in logarithmic scale:  ɣ_max=20*log_10(g), [dB].
-//
+// If the maximum level of side lobes divided by maximum level of main lobe
+// is a g, the ɣ_max is a g expressed in logarithmic scale:
+// ɣ_max=20*log_10(g), [dB].
 //
 // β -
 // The reduction coefficient.
-// The meaning of the reduction coefficient is that the amplitudes of all spectral components after multiplying by the window function are reduced by b times compared to the rectangular window. The reduction coefficient β is a b expressed in logarithmic scale:  β=20*log_10(b), [dB].
+// The meaning of the reduction coefficient
+// is that the amplitudes of all spectral components
+// after multiplying by the window function are reduced by b times
+// compared to the rectangular window. The reduction coefficient β
+// is a b expressed in logarithmic scale:  β=20*log_10(b), [dB].
 package window // import "gonum.org/v1/gonum/dsp/window"

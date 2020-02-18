54b6ee192ebbd30bf640099c24efb3470a70f596
Andrei Blinnikov <goofinator@mail.ru>
2020-02-19 02:06:40 +0300

    dsp/window: new package for functions to control spectral leakage of FFT

---
+++ dsp/window/doc.go
@@ -0,0 +1,43 @@
+// Copyright ©2020 The Gonum Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package window provides a set of functions to perform the transformation
+// of sequence by different window functions.
+//
+// Window functions can be used to control spectral leakage parameters
+// when performing a Fourier transform on a signal of limited length.
+// See https://en.wikipedia.org/wiki/Window_function for more details.
+//
+// Spectral leakage parameters
+//
+// Application of window functions to an input will result in changes
+// to the frequency content of the signal in an effect called spectral
+// leakage. See https://en.wikipedia.org/wiki/Spectral_leakage.
+//
+// The characteristic changes associated with each window function may
+// be described using a set of spectral leakage parameters; β, ΔF_0, ΔF_0.5,
+// K and ɣ_max.
+//
+// The β, attenuation, coefficient of a window is the ratio of the
+// constant component of the spectrum resulting from use of the window
+// compared to that produced using the rectangular window, expressed in
+// a logarithmic scale.
+//  β_w = 20 log10(A_w / A_rect) dB
+//
+// The ΔF_0 parameter describes the normalized width of the main lobe of
+// the frequency spectrum at zero amplitude.
+//
+// The ΔF_0.5 parameter describes the normalized width of the main lobe of
+// the frequency spectrum at -3 dB (half maximum amplitude).
+//
+// The K parameter describes the relative width of the main lobe of the
+// frequency spectrum produced by the window compared with the rectangular
+// window. The rectangular window has the lowest ΔF_0 at a value of 2.
+//  K_w = ΔF_0_w/ΔF_0_rect.
+// The value of K divides windows into high resolution windows (K≤3) and
+// low resolution windows (K>3).
+//
+// The ɣ_max parameter is the maximum level of the side lobes of the
+// normalized spectrum, in decibels.
+package window // import "gonum.org/v1/gonum/dsp/window"
---
+++ dsp/window/window_example_test.go
@@ -0,0 +1,105 @@
+// Copyright ©2020 The Gonum Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package window_test
+
+import (
+	"fmt"
+	"math"
+	"math/cmplx"
+
+	"gonum.org/v1/gonum/dsp/window"
+	"gonum.org/v1/gonum/fourier"
+)
+
+func Example() {
+	// The input sequence is a 2.5 period of the Sin function.
+	src := make([]float64, 20)
+	k := 5 * math.Pi / float64(len(src)-1)
+	for i := range src {
+		src[i] = math.Sin(k * float64(i))
+	}
+
+	// Initialize an FFT and perform the analysis.
+	fft := fourier.NewFFT(len(src))
+	coeff := fft.Coefficients(nil, src)
+
+	// The result shows that width of the main lobe with center
+	// between frequencies 0.1 and 0.15 is small, but that the
+	// height of the side lobes is large.
+	fmt.Println("Rectangular window (or no window):")
+	for i, c := range coeff {
+		fmt.Printf("freq=%.4f\tcycles/period, magnitude=%.4f,\tphase=%.4f\n",
+			fft.Freq(i), cmplx.Abs(c), cmplx.Phase(c))
+	}
+
+	// Initialize an FFT and perform the analysis on a sequence
+	// transformed by the Hamming window function.
+	fft = fourier.NewFFT(len(src))
+	coeff = fft.Coefficients(nil, window.Hamming(src))
+
+	// The result shows that width of the main lobe is wider,
+	// but height of the side lobes is lower.
+	fmt.Println("Hamming window:")
+	// The magnitude of all bins has been decreased by β.
+	// Generally in an analysis amplification may be omitted, but to
+	// make a comparable data, the result should be amplified by -β
+	// of the window function — +5.37 dB for the Hamming window.
+	//  -β = 20 log_10(amplifier).
+	amplifier := math.Pow(10, 5.37/20.0)
+	for i, c := range coeff {
+		fmt.Printf("freq=%.4f\tcycles/period, magnitude=%.4f,\tphase=%.4f\n",
+			fft.Freq(i), amplifier*cmplx.Abs(c), cmplx.Phase(c))
+	}
+	// Output:
+	//
+	// Rectangular window (or no window):
+	// freq=0.0000	cycles/period, magnitude=2.2798,	phase=0.0000
+	// freq=0.0500	cycles/period, magnitude=2.6542,	phase=0.1571
+	// freq=0.1000	cycles/period, magnitude=5.3115,	phase=0.3142
+	// freq=0.1500	cycles/period, magnitude=7.3247,	phase=-2.6704
+	// freq=0.2000	cycles/period, magnitude=1.6163,	phase=-2.5133
+	// freq=0.2500	cycles/period, magnitude=0.7681,	phase=-2.3562
+	// freq=0.3000	cycles/period, magnitude=0.4385,	phase=-2.1991
+	// freq=0.3500	cycles/period, magnitude=0.2640,	phase=-2.0420
+	// freq=0.4000	cycles/period, magnitude=0.1530,	phase=-1.8850
+	// freq=0.4500	cycles/period, magnitude=0.0707,	phase=-1.7279
+	// freq=0.5000	cycles/period, magnitude=0.0000,	phase=0.0000
+	// Hamming window:
+	// freq=0.0000	cycles/period, magnitude=0.0218,	phase=3.1416
+	// freq=0.0500	cycles/period, magnitude=0.8022,	phase=-2.9845
+	// freq=0.1000	cycles/period, magnitude=7.1723,	phase=0.3142
+	// freq=0.1500	cycles/period, magnitude=8.6285,	phase=-2.6704
+	// freq=0.2000	cycles/period, magnitude=2.0420,	phase=0.6283
+	// freq=0.2500	cycles/period, magnitude=0.0702,	phase=0.7854
+	// freq=0.3000	cycles/period, magnitude=0.0217,	phase=-2.1991
+	// freq=0.3500	cycles/period, magnitude=0.0259,	phase=-2.0420
+	// freq=0.4000	cycles/period, magnitude=0.0184,	phase=-1.8850
+	// freq=0.4500	cycles/period, magnitude=0.0092,	phase=-1.7279
+	// freq=0.5000	cycles/period, magnitude=0.0000,	phase=0.0000
+}
+
+func ExampleHamming() {
+	src := []float64{1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+
+	// Window functions change data in place. So, if input data
+	// needs to stay unchanged, it must be copied.
+	srcCpy := append([]float64(nil), src...)
+	// Apply window function to srcCpy.
+	dst := window.Hamming(srcCpy)
+
+	// src is unchanged.
+	fmt.Printf("src:    %f\n", src)
+	// srcCpy is altered.
+	fmt.Printf("srcCpy: %f\n", srcCpy)
+	// dst mirrors the srcCpy slice.
+	fmt.Printf("dst:    %f\n", dst)
+
+	// Output:
+	//
+	// src:    [1.000000 1.000000 1.000000 1.000000 1.000000 1.000000 1.000000 1.000000 1.000000 1.000000 1.000000 1.000000 1.000000 1.000000 1.000000 1.000000 1.000000 1.000000 1.000000 1.000000]
+	// srcCpy: [0.086957 0.111692 0.183218 0.293785 0.431409 0.581178 0.726861 0.852672 0.944977 0.993774 0.993774 0.944977 0.852672 0.726861 0.581178 0.431409 0.293785 0.183218 0.111692 0.086957]
+	// dst:    [0.086957 0.111692 0.183218 0.293785 0.431409 0.581178 0.726861 0.852672 0.944977 0.993774 0.993774 0.944977 0.852672 0.726861 0.581178 0.431409 0.293785 0.183218 0.111692 0.086957]
+}

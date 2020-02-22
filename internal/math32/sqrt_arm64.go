a272e7fbf8f1425917913993b96fa8fe6d23706e
Dan Kortschak <dan@kortschak.io>
2020-02-22 19:45:49 +1030

    math32: add arm64 assembly implementation for Sqrt

--- internal/math32/sqrt_arm64.go
+++ internal/math32/sqrt_arm64.go
@@ -1,14 +1,14 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Copyright ©2015 The Gonum Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build !amd64,!arm64 noasm appengine safe
+// +build !noasm,!appengine,!safe
 
 package math32
-
-import (
-	"math"
-)
 
 // Sqrt returns the square root of x.
 //
@@ -17,8 +17,4 @@ import (
 //	Sqrt(±0) = ±0
 //	Sqrt(x < 0) = NaN
 //	Sqrt(NaN) = NaN
-func Sqrt(x float32) float32 {
-	// FIXME(kortschak): Direct translation of the math package
-	// asm code for 386 fails to build.
-	return float32(math.Sqrt(float64(x)))
-}
+func Sqrt(x float32) float32

5f268d93942fb58c375b7181f5126d0ca7d8dfeb
Vladimir Chalupecky <vladimir@chalupecky.dev>
2020-03-07 23:15:31 +0100

    mat: rename CloneVec to CloneFromVec

--- optimize/bfgs.go
+++ optimize/bfgs.go
@@ -91,8 +91,8 @@ func (b *BFGS) InitDirection(loc *Location, dir []float64) (stepSize float64) {
 
 	x := mat.NewVecDense(dim, loc.X)
 	grad := mat.NewVecDense(dim, loc.Gradient)
-	b.x.CloneVec(x)
-	b.grad.CloneVec(grad)
+	b.x.CloneFromVec(x)
+	b.grad.CloneFromVec(grad)
 
 	b.y.Reset()
 	b.s.Reset()

6ded9ce79f37a9f41cdd29cd732ac2965b03c8d9
Dan Kortschak <dan@kortschak.io>
2020-03-16 15:22:44 +1030

    floats: fix formatting and grammar

--- floats/parse_example_test.go
+++ floats/parse_example_test.go
@@ -1,6 +1,6 @@
 // Copyright ©2017 The Gonum Authors. All rights reserved.
 // Use of this code is governed by a BSD-style
-// license that can be found in the LICENSE file
+// license that can be found in the LICENSE file.
 
 package floats_test
 

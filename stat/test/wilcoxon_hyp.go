2b934ceffb8f10e58003915ee50ef9c69bb029dd
Chilaka Ramakrishna <ramakrishna@nference.net>
2020-05-12 16:37:11 +0530

    stat: refactoring and changing the code style

--- stat/test/wilcoxon_hyp.go
+++ stat/test/wilcoxon_hyp.go
@@ -1,4 +1,4 @@
-// Copyright ©2019 The Gonum Authors. All rights reserved.
+// Copyright ©2020 The Gonum Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -13,52 +13,53 @@ import (
 )
 
 const badLengthMismatch = "test: slice length mismatch"
+const badLengthIsZero = "test: slice length is zero"
+const badNumberTooLarge = "test: number too large"
 
-func ensureDataConformance(x []float64, y []float64) {
-	if len(x) == 0 || len(y) == 0 || len(x) != len(y) {
+type pairStruct struct {
+	Position int
+	Value    float64
+}
+
+func ensureDataConformance(x, y []float64) {
+	if len(x) == 0 || len(y) == 0 {
+		panic(badLengthIsZero)
+	}
+	if len(x) != len(y) {
 		panic(badLengthMismatch)
 	}
 }
 
-func calculateDifferences(dst []float64, x []float64, y []float64) {
-	ensureDataConformance(x, y)
-	ensureDataConformance(x, dst)
+func calculateDifference(dst, x, y []float64) {
 	floats.SubTo(dst, x, y)
 }
 
-func calculateAbsDifference(dst []float64, x []float64, y []float64) {
-	ensureDataConformance(x, y)
-	ensureDataConformance(x, dst)
-	for i := 0; i < len(x); i++ {
-		dst[i] = math.Abs(y[i] - x[i])
+func calculateAbsDifference(dst, x, y []float64) {
+	for index, xVal := range x {
+		dst[index] = math.Abs(y[index] - xVal)
 	}
 }
 
-func rank(in []float64, out []float64) ([]float64, int) {
-	ensureDataConformance(in, out)
-
-	var tieAdjust = 0
-	type pairStruct struct {
-		Position int
-		Value    float64
-	}
-	var lengthOfIn = len(in)
-	var pairStructArr = make([]pairStruct, lengthOfIn)
-	for i := 0; i < lengthOfIn; i++ {
-		pairStructArr[i] = pairStruct{
-			Position: i,
-			Value:    in[i],
+func rank(out, in []float64) int {
+	tieAdjust := 0
+	lengthOfIn := len(in)
+	pairStructArr := make([]pairStruct, lengthOfIn)
+	for index, inVal := range in {
+		pairStructArr[index] = pairStruct{
+			Position: index,
+			Value:    inVal,
 		}
 	}
 	sort.Slice(pairStructArr, func(i, j int) bool {
 		return pairStructArr[i].Value < pairStructArr[j].Value
 	})
 
-	var mpOut = make(map[float64]int, len(out))   // tracks the rank
-	var mpCount = make(map[float64]int, len(out)) // tracks the count
-	var rank = 1
+	mpOut := make(map[float64]int, len(out))   // tracks the rank
+	mpCount := make(map[float64]int, len(out)) // tracks the count
+	tmpRank := make(map[float64]struct{})      // tracks the tied ranks
+	rank := 1
 	for i := 0; i < lengthOfIn; i++ {
-		var val = pairStructArr[i].Value
+		val := pairStructArr[i].Value
 		if val != 0 {
 			if _, ok := mpOut[val]; !ok {
 				mpOut[val] = rank
@@ -74,10 +75,11 @@ func rank(in []float64, out []float64) ([]float64, int) {
 		}
 	}
 
-	var tmpRank = make(map[float64]struct{})
+	var pos int
+	var val float64
 	for i := 0; i < lengthOfIn; i++ {
-		var pos = pairStructArr[i].Position
-		var val = pairStructArr[i].Value
+		pos = pairStructArr[i].Position
+		val = pairStructArr[i].Value
 		if val != 0 {
 			out[pos] = float64(mpOut[val]) / float64(mpCount[val])
 		}
@@ -91,28 +93,25 @@ func rank(in []float64, out []float64) ([]float64, int) {
 		tieAdjust += (mpCount[val] * mpCount[val] * mpCount[val]) - (mpCount[val])
 
 	}
-	return out, tieAdjust
+	return tieAdjust
 }
 
-func wilCoxonSignedRankTest(x []float64, y []float64) (float64, int, int) {
-	ensureDataConformance(x, y)
-
-	var z = make([]float64, len(x))
-	var absZ = make([]float64, len(x))
-	calculateDifferences(z, x, y)
+func wilCoxonSignedRankTest(x, y []float64) (float64, int, int) {
+	z := make([]float64, len(x))
+	absZ := make([]float64, len(x))
+	floats.SubTo(z, x, y)
 	calculateAbsDifference(absZ, x, y)
 
-	var ranks = make([]float64, len(x))
-	var tieAdj = 0
-	ranks, tieAdj = rank(absZ, ranks)
-	var tmpLenOfX = len(x)
+	ranks := make([]float64, len(x))
+	tieAdj := rank(ranks, absZ)
+	tmpLenOfX := len(x)
 
-	var WPlus = 0.0
-	var WMinus = 0.0
-	for i := 0; i < len(x); i++ {
-		if z[i] > 0 {
-			WPlus += ranks[i]
-		} else if z[i] == 0 {
+	WPlus := 0.0
+	WMinus := 0.0
+	for index, rank := range ranks {
+		if z[index] > 0 {
+			WPlus += rank
+		} else if z[index] == 0 {
 			tmpLenOfX--
 		}
 	}
@@ -120,62 +119,63 @@ func wilCoxonSignedRankTest(x []float64, y []float64) (float64, int, int) {
 	return math.Max(WMinus, WPlus), tmpLenOfX, tieAdj
 }
 
-func calculateExactPValue(Wmax float64, N int) float64 {
-	var m = 1 << N
-
+func calculateExactPValue(Wmax float64, NZ int) float64 {
+	m := 1 << NZ
 	largerRankSums := 0
 
 	for i := 0; i < m; i++ {
 		rankSum := 0
-
 		// Generate all possible rank sums
-		for j := 0; j < N; j++ {
-
+		for j := 0; j < NZ; j++ {
 			// (i >> j) & 1 extract i's j-th bit from the right
 			if ((i >> j) & 1) == 1 {
 				rankSum += j + 1
 			}
 		}
-
 		if float64(rankSum) >= Wmax {
 			largerRankSums++
 		}
 	}
 
-	/*
-	 * largerRankSums / m gives the one-sided p-value, so it's multiplied
-	 * with 2 to get the two-sided p-value
-	 */
+	// largerRankSums / m gives the one-sided p-value, so it's multiplied
+	// with 2 to get the two-sided p-value
 	return 2 * (float64(largerRankSums) / float64(m))
 }
 
 func calculateAsymptoticPValue(Wmin float64, NZ int, tieAdj int) float64 {
-
-	// n should be number of non zeros
-
+	// n should be number of non-zero absolute difference pairs
 	ES := float64(NZ*(NZ+1)) / 4.0
-
-	VarS := ES * (float64(2*NZ + 1)) / 6.0
-
-	VarS -= float64(tieAdj) / 48.0
+	VarS := (ES * (float64(2*NZ + 1)) / 6.0) - (float64(tieAdj) / 48.0)
 
 	// - 0.5 is a continuity correction
 	z := (Wmin - ES - 0.5) / math.Sqrt(VarS)
 
 	standardNormal := distuv.UnitNormal
-
 	return 2 * standardNormal.CDF(z)
 }
 
+/*
+	WilcoxonSignedRankTest implements Wilcoxon signed rank test (https://en.wikipedia.org/wiki/Wilcoxon_signed-rank_test)
 
-// Perform a Wilcoxon signed rank test of the null hypothesis that the distribution of `x`
-// (or the difference `x - y` if `y` is provided) has zero median against the alternative
-// hypothesis that the median is non-zero.
+	The Wilcoxon signed-rank test tests the null hypothesis that two related paired samples come from the same distribution. In particular, it tests whether the distribution of the differences x - y is symmetric about zero. It is a non-parametric version of the paired T-test.
+
+	Parameters:
+		x:
+			The first set of measurements.
+		y:
+			The second set of measurements.
+		exactPValue:
+			Exact P value computation is expensive, it is only available for the measurements with dimension less than 30.
+			Exact P Value as true for measurements greater than 30 panics.
+
+    Returns:
+		The two sided p-value for the test
+*/
 func WilcoxonSignedRankTest(x []float64, y []float64, exactPValue bool) float64 {
 	ensureDataConformance(x, y)
 	Wmax, N, tieAdj := wilCoxonSignedRankTest(x, y)
 	if exactPValue && N > 30 {
-		panic("number too large")
+		panic(badNumberTooLarge)
 	}
 	if exactPValue {
 		return calculateExactPValue(Wmax, N)

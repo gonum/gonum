c596234bd18c33cfde5b89fc794610d3a7545422
Roman Werpachowski <romanw@google.com>
2020-05-20 22:18:50 +0100

    Basic doc files.

---
+++ interp/README.md
@@ -0,0 +1,3 @@
+# Gonum interp [![GoDoc](https://godoc.org/gonum.org/v1/gonum/interp?status.svg)](https://godoc.org/gonum.org/v1/gonum/interp)
+
+Package interp is an interpolation package for the Go language.
---
+++ interp/doc.go
@@ -0,0 +1,6 @@
+// Copyright ©2015 The Gonum Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package interp implements algorithms for interpolating sampled values.
+package interp // import "gonum.org/v1/gonum/interp"

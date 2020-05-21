4d59b122c9a1238ef95a5510855dbbbd6ba2d2fc
Roman Werpachowski <61159095+romanwerpachowski@users.noreply.github.com>
2020-05-21 01:02:59 +0100

    Add list of provided interpolation algorithms.

--- interp/doc.go
+++ interp/doc.go
@@ -1,6 +1,10 @@
 // Copyright ©2015 The Gonum Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-
+//
 // Package interp implements algorithms for interpolating sampled values.
+//
+// Contains the following algorithms:
+// 1. constant 1D interpolator
+// 2. linear 1D interpolator
 package interp // import "gonum.org/v1/gonum/interp"

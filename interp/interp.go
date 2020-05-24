765afe34a0d9820ebe188ab27ad8edbb3078e62b
Roman Werpachowski <romanw@google.com>
2020-05-24 23:56:35 +0100

    Remove Interval() method and define extrapolation beyond data range.

--- interp/interp.go
+++ interp/interp.go
@@ -5,43 +5,29 @@
 package interp
 
 import (
-	"fmt"
-	"math"
 	"sort"
-
-	"gonum.org/v1/gonum/spatial/r1"
 )
 
-// Interpolator represents a 1-dimensional interpolator.
-// It interpolates a function over a defined closed interval range
-// (which can be infinite on both sides).
-type Interpolator interface {
-	// Interval returns the valid closed interval for interpolation.
-	Interval() r1.Interval
-
-	// ValueAt returns the interpolation value at x.
-	// It panics if x is not in the interpolation interval.
-	ValueAt(float64) float64
+// Predictor predicts the value of a function. It handles both
+// interpolation and extrapolation.
+type Predictor interface {
+	// Predict returns the predicted value at x.
+	Predict(float64) float64
 }
 
-// Constant is an interpolator that predicts the same constant value
-// over the range [-infinity, infinity].
+// Constant predicts a constant value.
 type Constant struct {
 	// Constant Y value.
 	Value float64
 }
 
-// Interval returns the valid closed interval for interpolation.
-func (ci Constant) Interval() r1.Interval {
-	return r1.Interval{Min: math.Inf(-1), Max: math.Inf(1)}
-}
-
-// ValueAt returns the interpolation value at x.
-func (ci Constant) ValueAt(x float64) float64 {
-	return ci.Value
+// Predict returns the predicted value at x.
+func (c Constant) Predict(x float64) float64 {
+	return c.Value
 }
 
 // PiecewiseLinear is a piecewise linear 1-dimensional interpolator.
+// It extrapolates flat forwards (backwards) the last (first) known Y value.
 type PiecewiseLinear struct {
 	// Interpolated X values.
 	xs []float64
@@ -77,22 +63,28 @@ func NewPiecewiseLinear(xs []float64, ys []float64) *PiecewiseLinear {
 	return &PiecewiseLinear{xs, ys, slopes}
 }
 
-// Interval returns the valid closed interval for interpolation.
-func (li PiecewiseLinear) Interval() r1.Interval {
-	return r1.Interval{Min: li.xs[0], Max: li.xs[len(li.xs)-1]}
-}
-
-// ValueAt returns the interpolation value at x.
-func (li PiecewiseLinear) ValueAt(x float64) float64 {
-	i, xI := findSegment(li.xs, x)
+// Predict returns the interpolation value at x.
+func (pl PiecewiseLinear) Predict(x float64) float64 {
+	i := findSegment(pl.xs, x)
+	if i < 0 {
+		return pl.ys[0]
+	}
+	// i < len(pci.xs)
+	xI := pl.xs[i]
 	if x == xI {
-		return li.ys[i]
+		return pl.ys[i]
+	}
+	n := len(pl.xs)
+	if i == n-1 {
+		// x > li.xs[i]
+		return pl.ys[n-1]
 	}
 	// i < len(i1d.xs) - 1
-	return li.ys[i] + li.slopes[i]*(x-xI)
+	return pl.ys[i] + pl.slopes[i]*(x-xI)
 }
 
 // PiecewiseConstant is a piecewise constant 1-dimensional interpolator.
+// It extrapolates flat forwards (backwards) the last (first) known Y value.
 type PiecewiseConstant struct {
 	// Interpolated X values.
 	xs []float64
@@ -126,41 +118,30 @@ func NewPiecewiseConstant(xs []float64, ys []float64, leftContinuous bool) *Piec
 	return &PiecewiseConstant{xs, ys, leftContinuous}
 }
 
-// Interval returns the valid closed interval for interpolation.
-func (pci PiecewiseConstant) Interval() r1.Interval {
-	return r1.Interval{Min: pci.xs[0], Max: pci.xs[len(pci.xs)-1]}
+// Predict returns the interpolation value at x.
+func (pc PiecewiseConstant) Predict(x float64) float64 {
+	i := findSegment(pc.xs, x)
+	if i < 0 {
+		return pc.ys[0]
+	}
+	// i < len(pci.xs)
+	if x == pc.xs[i] {
+		return pc.ys[i]
+	}
+	n := len(pc.xs)
+	if i == n-1 {
+		// x > pci.xs[i]
+		return pc.ys[n-1]
+	}
+	if pc.leftContinuous {
+		return pc.ys[i+1]
+	}
+	return pc.ys[i]
 }
 
-// ValueAt returns the interpolation value at x.
-func (pci PiecewiseConstant) ValueAt(x float64) float64 {
-	i, xI := findSegment(pci.xs, x)
-	if x == xI {
-		return pci.ys[i]
-	}
-	// i < len(i1d.xs) - 1
-	if pci.leftContinuous {
-		return pci.ys[i+1]
-	}
-	return pci.ys[i]
-}
-
-// findSegment returns a tuple of: (i such that xs[i] <= x < xs[i + 1], xs[i]),
-// or panics if such i is not found. Assumes that len(xs) >= 2 without checking.
-func findSegment(xs []float64, x float64) (int, float64) {
-	// Find minimum i s.t. xs[i] >= x, or len(xs) if not found.
-	n := len(xs)
-	i := sort.Search(n, func(i int) bool { return xs[i] > x })
-	if i < n {
-		if i == 0 {
-			// x < begin()
-			panic(fmt.Sprintf("interp: x value %g below lower bound %g", x, xs[0]))
-		} else {
-			return i - 1, xs[i-1]
-		}
-	} else {
-		if xs[n-1] == x {
-			return n - 1, x
-		}
-		panic(fmt.Sprintf("interp: x value %g above upper bound %g", x, xs[n-1]))
-	}
+// findSegment returns 0 <= i < len(xs) such that xs[i] <= x < xs[i + 1], or -1
+// if no such interval containing x is found. It assumes that len(xs) >= 2
+// without checking.
+func findSegment(xs []float64, x float64) int {
+	return sort.Search(len(xs), func(i int) bool { return xs[i] > x }) - 1
 }
--- interp/interp_test.go
+++ interp/interp_test.go
@@ -20,28 +20,15 @@ func panics(fn func()) (panicked bool, message string) {
 	return
 }
 
-func TestConstantInterval(t *testing.T) {
+func TestConstant(t *testing.T) {
 	t.Parallel()
 	const value = 42.0
-	ci := Constant{value}
-	interval := ci.Interval()
-	if !math.IsInf(interval.Min, -1) {
-		t.Errorf("unexpected begin() value: got: %g want: %g", interval.Min, math.Inf(-1))
-	}
-	if !math.IsInf(interval.Max, 1) {
-		t.Errorf("unexpected end() value: got: %g want: %g", interval.Max, math.Inf(1))
-	}
-}
-
-func TestConstantValueAt(t *testing.T) {
-	t.Parallel()
-	const value = 42.0
-	ci := Constant{value}
+	c := Constant{value}
 	xs := []float64{math.Inf(-1), -11, 0.4, 1e9, math.Inf(1)}
 	for _, x := range xs {
-		y := ci.ValueAt(x)
+		y := c.Predict(x)
 		if y != value {
-			t.Errorf("unexpected ValueAt(%g) value: got: %g want: %g", x, y, value)
+			t.Errorf("unexpected Predict(%g) value: got: %g want: %g", x, y, value)
 		}
 	}
 }
@@ -49,27 +36,12 @@ func TestConstantValueAt(t *testing.T) {
 func TestFindSegment(t *testing.T) {
 	t.Parallel()
 	xs := []float64{0, 1, 2}
-	type params struct {
-		x         float64
-		expectedI int
-		expectedX float64
-	}
-	paramSets := []params{{0, 0, 0}, {0.3, 0, 0}, {1, 1, 1}, {1.5, 1, 1}, {2, 2, 2}}
-	for _, param := range paramSets {
-		i, x := findSegment(xs, param.x)
-		if i != param.expectedI || x != param.expectedX {
-			t.Errorf("unexpected value of findSegment(xs, %g): got %d, %g want: %d, %g", param.x, i, x, param.expectedI, param.expectedX)
-		}
-	}
-	panicXs := []float64{-0.5, 2.1}
-	expectedMessages := []string{
-		"interp: x value -0.5 below lower bound 0",
-		"interp: x value 2.1 above upper bound 2",
-	}
-	for i, x := range panicXs {
-		panicked, message := panics(func() { findSegment(xs, x) })
-		if !panicked || message != expectedMessages[i] {
-			t.Errorf("expected panic with message '%s' for evaluating at invalid x: %g", expectedMessages[i], x)
+	testXs := []float64{-0.6, 0, 0.3, 1, 1.5, 2, 2.8}
+	expectedIs := []int{-1, 0, 0, 1, 1, 2, 2}
+	for k, x := range testXs {
+		i := findSegment(xs, x)
+		if i != expectedIs[k] {
+			t.Errorf("unexpected value of findSegment(xs, %g): got %d want: %d", x, i, expectedIs[k])
 		}
 	}
 }
@@ -79,9 +51,11 @@ func BenchmarkFindSegment(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		findSegment(xs, 0)
 		findSegment(xs, 16.5)
+		findSegment(xs, -1)
 		findSegment(xs, 8.25)
 		findSegment(xs, 4.125)
 		findSegment(xs, 13.6)
+		findSegment(xs, 23.6)
 		findSegment(xs, 13.5)
 		findSegment(xs, 6)
 		findSegment(xs, 4.5)
@@ -89,23 +63,14 @@ func BenchmarkFindSegment(b *testing.B) {
 }
 
 // testPiecewiseInterpolatorCreation tests common functionality in creating piecewise  interpolators.
-func testPiecewiseInterpolatorCreation(t *testing.T, create func(xs []float64, ys []float64) Interpolator) {
-	xs := []float64{0, 1, 2}
-	i1d := create(xs, []float64{-0.5, 1.5, 1})
-	interval := i1d.Interval()
-	if xs[0] != interval.Min {
-		t.Errorf("unexpected begin() value: got %g: want: %g", interval.Min, xs[0])
-	}
-	if xs[2] != interval.Max {
-		t.Errorf("unexpected end() value: got %g: want: %g", interval.Max, xs[2])
-	}
+func testPiecewiseInterpolatorCreation(t *testing.T, create func(xs []float64, ys []float64) Predictor) {
 	type panicParams struct {
 		xs              []float64
 		ys              []float64
 		expectedMessage string
 	}
 	panicParamSets := []panicParams{
-		{xs, []float64{-0.5, 1.5}, "xs and ys have different lengths"},
+		{[]float64{0, 1, 2}, []float64{-0.5, 1.5}, "xs and ys have different lengths"},
 		{[]float64{0.3}, []float64{0}, "too few points for interpolation"},
 		{[]float64{0.3, 0.3}, []float64{0, 0}, "xs values not strictly increasing"},
 		{[]float64{0.3, -0.3}, []float64{0, 0}, "xs values not strictly increasing"},
@@ -121,33 +86,32 @@ func testPiecewiseInterpolatorCreation(t *testing.T, create func(xs []float64, y
 
 func TestNewPiecewiseLinear(t *testing.T) {
 	t.Parallel()
-	testPiecewiseInterpolatorCreation(t, func(xs []float64, ys []float64) Interpolator { return NewPiecewiseLinear(xs, ys) })
+	testPiecewiseInterpolatorCreation(t, func(xs []float64, ys []float64) Predictor { return NewPiecewiseLinear(xs, ys) })
 }
 
-// testInterpolatorValueAt tests evaluation of a  interpolator.
-func testInterpolatorValueAt(t *testing.T, i1d Interpolator, xs []float64, expectedYs []float64, tol float64) {
+// testInterpolatorPredict tests evaluation of a  interpolator.
+func testInterpolatorPredict(t *testing.T, p Predictor, xs []float64, expectedYs []float64, tol float64) {
 	for i, x := range xs {
-		y := i1d.ValueAt(x)
+		y := p.Predict(x)
 		yErr := math.Abs(y - expectedYs[i])
 		if yErr > tol {
 			if tol == 0 {
-				t.Errorf("unexpected ValueAt(%g) value: got: %g want: %g", x, y, expectedYs[i])
+				t.Errorf("unexpected Predict(%g) value: got: %g want: %g", x, y, expectedYs[i])
 			} else {
-				t.Errorf("unexpected ValueAt(%g) value: got: %g want: %g with tolerance: %g", x, y, expectedYs[i], tol)
+				t.Errorf("unexpected Predict(%g) value: got: %g want: %g with tolerance: %g", x, y, expectedYs[i], tol)
 			}
 		}
 	}
 }
 
-func TestPiecewiseLinearValueAt(t *testing.T) {
+func TestPiecewiseLinearPredict(t *testing.T) {
 	t.Parallel()
 	xs := []float64{0, 1, 2}
 	ys := []float64{-0.5, 1.5, 1}
-	li := NewPiecewiseLinear(xs, ys)
-	testInterpolatorValueAt(t, li, xs, ys, 0)
-	testXs := []float64{0.1, 0.5, 0.8, 1.2}
-	expectedYs := []float64{-0.3, 0.5, 1.1, 1.4}
-	testInterpolatorValueAt(t, li, testXs, expectedYs, 1e-15)
+	pl := NewPiecewiseLinear(xs, ys)
+	testInterpolatorPredict(t, pl, xs, ys, 0)
+	testInterpolatorPredict(t, pl, []float64{-0.4, 2.6}, []float64{-0.5, 1}, 0)
+	testInterpolatorPredict(t, pl, []float64{0.1, 0.5, 0.8, 1.2}, []float64{-0.3, 0.5, 1.1, 1.4}, 1e-15)
 }
 
 func BenchmarkNewPiecewiseLinear(b *testing.B) {
@@ -158,64 +122,66 @@ func BenchmarkNewPiecewiseLinear(b *testing.B) {
 	}
 }
 
-func BenchmarkPiecewiseLinearValueAt(b *testing.B) {
+func BenchmarkPiecewiseLinearPredict(b *testing.B) {
 	xs := []float64{0, 1.5, 3, 4.5, 6, 7.5, 9, 12, 13.5, 16.5}
 	ys := []float64{0, 1, 2, 2.5, 2, 1.5, 4, 10, -2, 2}
-	li := NewPiecewiseLinear(xs, ys)
+	pl := NewPiecewiseLinear(xs, ys)
 	for i := 0; i < b.N; i++ {
-		li.ValueAt(0)
-		li.ValueAt(16.5)
-		li.ValueAt(4)
-		li.ValueAt(7.32)
-		li.ValueAt(9.0001)
-		li.ValueAt(1.4)
-		li.ValueAt(1.6)
-		li.ValueAt(13.5)
-		li.ValueAt(4.5)
+		pl.Predict(0)
+		pl.Predict(16.5)
+		pl.Predict(-2)
+		pl.Predict(4)
+		pl.Predict(7.32)
+		pl.Predict(9.0001)
+		pl.Predict(1.4)
+		pl.Predict(1.6)
+		pl.Predict(30)
+		pl.Predict(13.5)
+		pl.Predict(4.5)
 	}
 }
 
 func TestNewPiecewiseConstant(t *testing.T) {
-	testPiecewiseInterpolatorCreation(t, func(xs []float64, ys []float64) Interpolator { return NewPiecewiseConstant(xs, ys, true) })
-	testPiecewiseInterpolatorCreation(t, func(xs []float64, ys []float64) Interpolator { return NewPiecewiseConstant(xs, ys, false) })
+	testPiecewiseInterpolatorCreation(t, func(xs []float64, ys []float64) Predictor { return NewPiecewiseConstant(xs, ys, true) })
+	testPiecewiseInterpolatorCreation(t, func(xs []float64, ys []float64) Predictor { return NewPiecewiseConstant(xs, ys, false) })
 }
 
-func benchmarkPiecewiseConstantValueAt(b *testing.B, leftContinuous bool) {
+func benchmarkPiecewiseConstantPredict(b *testing.B, leftContinuous bool) {
 	xs := []float64{0, 1.5, 3, 4.5, 6, 7.5, 9, 12, 13.5, 16.5}
 	ys := []float64{0, 1, 2, 2.5, 2, 1.5, 4, 10, -2, 2}
-	pci := NewPiecewiseConstant(xs, ys, leftContinuous)
+	pc := NewPiecewiseConstant(xs, ys, leftContinuous)
 	for i := 0; i < b.N; i++ {
-		pci.ValueAt(0)
-		pci.ValueAt(16.5)
-		pci.ValueAt(4)
-		pci.ValueAt(7.32)
-		pci.ValueAt(9.0001)
-		pci.ValueAt(1.4)
-		pci.ValueAt(1.6)
-		pci.ValueAt(13.5)
-		pci.ValueAt(4.5)
+		pc.Predict(0)
+		pc.Predict(16.5)
+		pc.Predict(4)
+		pc.Predict(7.32)
+		pc.Predict(9.0001)
+		pc.Predict(1.4)
+		pc.Predict(1.6)
+		pc.Predict(13.5)
+		pc.Predict(4.5)
 	}
 }
 
-func BenchmarkPiecewiseConstantLeftContinuousValueAt(b *testing.B) {
-	benchmarkPiecewiseConstantValueAt(b, true)
+func BenchmarkPiecewiseConstantLeftContinuousPredict(b *testing.B) {
+	benchmarkPiecewiseConstantPredict(b, true)
 }
 
-func BenchmarkPiecewiseConstantRightContinuousValueAt(b *testing.B) {
-	benchmarkPiecewiseConstantValueAt(b, false)
+func BenchmarkPiecewiseConstantRightContinuousPredict(b *testing.B) {
+	benchmarkPiecewiseConstantPredict(b, false)
 }
 
-func TestPiecewiseConstantValueAt(t *testing.T) {
+func TestPiecewiseConstantPredict(t *testing.T) {
 	t.Parallel()
 	xs := []float64{0, 1, 2}
 	ys := []float64{-0.5, 1.5, 1}
-	pciLeft := NewPiecewiseConstant(xs, ys, true)
-	pciRight := NewPiecewiseConstant(xs, ys, false)
-	testInterpolatorValueAt(t, pciLeft, xs, ys, 0)
-	testInterpolatorValueAt(t, pciRight, xs, ys, 0)
-	testXs := []float64{0.1, 0.5, 0.8, 1.2}
-	leftYs := []float64{1.5, 1.5, 1.5, 1}
-	rightYs := []float64{-0.5, -0.5, -0.5, 1.5}
-	testInterpolatorValueAt(t, pciLeft, testXs, leftYs, 0)
-	testInterpolatorValueAt(t, pciRight, testXs, rightYs, 0)
+	pcLeft := NewPiecewiseConstant(xs, ys, true)
+	pcRight := NewPiecewiseConstant(xs, ys, false)
+	testInterpolatorPredict(t, pcLeft, xs, ys, 0)
+	testInterpolatorPredict(t, pcRight, xs, ys, 0)
+	testXs := []float64{-0.9, 0.1, 0.5, 0.8, 1.2, 3.1}
+	leftYs := []float64{-0.5, 1.5, 1.5, 1.5, 1, 1}
+	rightYs := []float64{-0.5, -0.5, -0.5, -0.5, 1.5, 1, 1}
+	testInterpolatorPredict(t, pcLeft, testXs, leftYs, 0)
+	testInterpolatorPredict(t, pcRight, testXs, rightYs, 0)
 }

1e1364ed79491a56d2063c0a2ac38452ed5f6205
Roman Werpachowski <romanw@google.com>
2020-05-31 22:41:27 +0100

    Add documentation to interfaces.

--- stat/distuv/interfaces.go
+++ stat/distuv/interfaces.go
@@ -4,19 +4,23 @@
 
 package distuv
 
+// LogProber calculates the log-probability for a given value of a random variable.
 type LogProber interface {
 	LogProb(float64) float64
 }
 
+// Rander samples a random variable.
 type Rander interface {
 	Rand() float64
 }
 
+// RandLogProber is a Rander and a LogProber.
 type RandLogProber interface {
 	Rander
 	LogProber
 }
 
+// Quantiler calculates the quantile of a distribution for given probability.
 type Quantiler interface {
 	Quantile(p float64) float64
 }

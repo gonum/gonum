b30b7bd59134080bbb09e66f4c8c597178a646a7
John Doe <fffelagund@gmail.com>
2020-06-30 13:18:19 +0300

    tests & simple implementation

---
+++ graph/flownet/max_flow_test.go
@@ -0,0 +1,138 @@
+package flownet
+
+import (
+	"testing"
+
+	"gonum.org/v1/gonum/graph/simple"
+)
+
+func makeGraph() *SimpleGraph {
+	g := simple.NewWeightedDirectedGraph(0, -1)
+	g.AddNode(simple.Node(0))
+	g.AddNode(simple.Node(1))
+	g.AddNode(simple.Node(2))
+	g.AddNode(simple.Node(3))
+	g.SetWeightedEdge(simple.WeightedEdge{
+		F: simple.Node(0), T: simple.Node(1),
+		W: 5.0,
+	})
+	g.SetWeightedEdge(simple.WeightedEdge{
+		F: simple.Node(0), T: simple.Node(2),
+		W: 15.0,
+	})
+	g.SetWeightedEdge(simple.WeightedEdge{
+		F: simple.Node(2), T: simple.Node(1),
+		W: 10.0,
+	})
+	g.SetWeightedEdge(simple.WeightedEdge{
+		F: simple.Node(3), T: simple.Node(2),
+		W: 5.0,
+	})
+	g.SetWeightedEdge(simple.WeightedEdge{
+		F: simple.Node(3), T: simple.Node(0),
+		W: 10.0,
+	})
+	return &SimpleGraph{*g}
+}
+
+func TestEdgesPresenceSimpleGraph(t *testing.T) {
+	var sg Graph = makeGraph()
+	for _, edge := range [][]int64{{0, 1}, {0, 2}, {2, 1}, {3, 2}, {3, 0}} {
+		e := sg.Edge(edge[0], edge[1])
+		if e == nil {
+			t.Errorf("Edge %d %d is missing.", edge[0], edge[1])
+		}
+	}
+}
+
+func TestEdgesPresenceResidualGraph(t *testing.T) {
+	var g ResidualGraph = makeGraph().ResidualGraph()
+	for _, edge := range [][]int64{{0, 1, 5}, {0, 2, 15}, {2, 1, 10}, {3, 2, 5}, {3, 0, 10}} {
+		e := g.ResidualEdge(edge[0], edge[1])
+		capacity := int32(edge[2])
+		if e == nil {
+			t.Errorf("Edge %d %d is missing.", edge[0], edge[1])
+		}
+		if e.MaxCapacity() != capacity {
+			t.Errorf("Edge %d %d capacity is %d. Must be %d", edge[0], edge[1], e.MaxCapacity(), capacity)
+		}
+
+		if e.CurrentFlow() != 0 {
+			t.Errorf("Edge %d %d flow is %d. Must be 0", edge[0], edge[1], e.CurrentFlow())
+		}
+	}
+}
+
+func TestEdgesFromResidualGraph(t *testing.T) {
+	var g ResidualGraph = makeGraph().ResidualGraph()
+
+	for frm, to := range map[int64]map[int64]int32{
+		0: {1: 5, 2: 15},
+		2: {1: 10},
+		3: {2: 5, 0: 10},
+	} {
+		fromNodes := g.From(frm)
+		for fromNodes.Next() {
+			toNode := fromNodes.Node().ID()
+
+			edge := g.ResidualEdge(frm, toNode)
+			expectedCapacity := to[toNode]
+			if edge.MaxCapacity() != expectedCapacity {
+				t.Errorf("Edge %d %d capacity is %d. Must be %d", frm, toNode, edge.MaxCapacity(), expectedCapacity)
+			}
+		}
+	}
+
+}
+
+func TestSetFlowResidualGraph(t *testing.T) {
+	var g ResidualGraph = makeGraph().ResidualGraph()
+
+	g.SetFlow(3, 0, 7)
+
+	if g.ResidualEdge(3, 0).CurrentFlow() != 7 {
+		t.Errorf("Edge %d %d flow is %d. Must be %d", 3, 0, g.ResidualEdge(3, 0).CurrentFlow(), 7)
+	}
+
+	if g.ResidualEdge(0, 3).CurrentFlow() != 3 {
+		t.Errorf("Edge %d %d flow is %d. Must be %d", 0, 3, g.ResidualEdge(0, 3).CurrentFlow(), 3)
+	}
+
+}
+
+func TestAugmentingPath(t *testing.T) {
+	var g ResidualGraph = makeGraph().ResidualGraph()
+	source := simple.Node(3)
+	sink := simple.Node(1)
+	path := AugmentingPath(g, source, sink)
+	flow := AvailableCapacity(path)
+	if flow <= 0 {
+		t.Errorf("Flow is expected to be > 0")
+	}
+	for _, edge := range path {
+		oldFlow := g.ResidualEdge(edge.From().ID(), edge.To().ID()).CurrentFlow()
+		g.SetFlow(edge.From().ID(), edge.To().ID(), flow)
+		if oldFlow == g.ResidualEdge(edge.From().ID(), edge.To().ID()).CurrentFlow() {
+			t.Errorf("Flow in the edge %d %d hasn't changed", edge.From().ID(), edge.From().ID())
+		}
+	}
+
+	path2 := AugmentingPath(g, source, sink)
+	for _, edge := range path2 {
+		if edge.CurrentFlow() == edge.MaxCapacity() {
+			t.Errorf("Flow in the edge %d %d is equal to its max capacity.", edge.From().ID(), edge.From().ID())
+		}
+	}
+}
+
+func TestMaxFlow(t *testing.T) {
+	flow := MaxFlow(
+		makeGraph(),
+		simple.Node(3),
+		simple.Node(1),
+	)
+	if flow != 15 {
+		t.Errorf("Max flow = %d. Must be 15", flow)
+	}
+
+}
---
+++ graph/flownet/simple.go
@@ -0,0 +1,150 @@
+package flownet
+
+import (
+	"gonum.org/v1/gonum/graph"
+	"gonum.org/v1/gonum/graph/iterator"
+	"gonum.org/v1/gonum/graph/simple"
+)
+
+type SimpleGraph struct {
+	simple.WeightedDirectedGraph
+}
+
+func (g *SimpleGraph) ResidualGraph() ResidualGraph {
+	nodes := make(map[int64]graph.Node)
+	from := make(map[int64]map[int64]*ResidualEdge)
+
+	srcNodes := g.Nodes()
+	for srcNodes.Next() {
+		node := srcNodes.Node()
+		nodes[node.ID()] = node
+	}
+	srcNodes.Reset()
+	for srcNodes.Next() {
+		u := srcNodes.Node()
+		uid := u.ID()
+		to := g.From(uid)
+		for to.Next() {
+			v := to.Node()
+			vid := v.ID()
+			var edge ResidualEdge = SimpleResidualEdge{
+				simple.Edge{F: u, T: v},
+				int32(g.WeightedEdge(uid, vid).Weight()),
+				0,
+				false,
+			}
+			if fm, ok := from[uid]; ok {
+				fm[vid] = &edge
+			} else {
+				from[uid] = map[int64]*ResidualEdge{vid: &edge}
+			}
+		}
+	}
+
+	keys := make([]int64, 0, len(from))
+	for k := range from {
+		keys = append(keys, k)
+	}
+
+	return &SimpleResidualGraph{
+		nodes: nodes,
+		from:  from,
+	}
+}
+
+type SimpleResidualGraph struct {
+	nodes map[int64]graph.Node
+	from  map[int64]map[int64]*ResidualEdge
+}
+
+func (g *SimpleResidualGraph) From(id int64) graph.Nodes {
+	if _, ok := g.from[id]; !ok {
+		return graph.Empty
+	}
+
+	from := make([]graph.Node, len(g.from[id]))
+	i := 0
+	for vid := range g.from[id] {
+		from[i] = g.nodes[vid]
+		i++
+	}
+	if len(from) == 0 {
+		return graph.Empty
+	}
+	return iterator.NewOrderedNodes(from)
+}
+
+func (g *SimpleResidualGraph) Edge(uid, vid int64) graph.Edge {
+	return g.ResidualEdge(uid, vid)
+}
+
+func (g *SimpleResidualGraph) SetFlow(uid, vid int64, flow int32) {
+	if uid == vid {
+		panic("max_flow: adding self edge")
+	}
+	edge := g.ResidualEdge(uid, vid)
+	newEdge := edge.WithFlow(flow)
+	if edge.IsReverseEdge() {
+		newEdge = newEdge.ReversedResidualEdge()
+	}
+	g.from[newEdge.From().ID()][newEdge.To().ID()] = &newEdge
+}
+
+func (g *SimpleResidualGraph) ResidualEdge(uid, vid int64) ResidualEdge {
+	keys := make([]int64, 0, len(g.from))
+	for k := range g.from {
+		keys = append(keys, k)
+	}
+
+	if _, ok := g.from[uid]; ok {
+		keys := make([]int64, 0, len(g.from[uid]))
+		for k := range g.from[uid] {
+			keys = append(keys, k)
+		}
+
+		if _, ok := g.from[uid][vid]; ok {
+			return *g.from[uid][vid]
+		}
+	}
+	if _, ok := g.from[vid]; ok {
+		if _, ok := g.from[vid][uid]; ok {
+			edge := g.from[vid][uid]
+			return (*edge).ReversedResidualEdge()
+		}
+	}
+	return nil
+}
+
+type SimpleResidualEdge struct {
+	simple.Edge
+	capacity, flow int32
+	reverse        bool
+}
+
+func (re SimpleResidualEdge) MaxCapacity() int32 {
+	return re.capacity
+}
+
+func (re SimpleResidualEdge) CurrentFlow() int32 {
+	return re.flow
+}
+
+func (re SimpleResidualEdge) IsReverseEdge() bool {
+	return re.reverse
+}
+
+func (re SimpleResidualEdge) ReversedResidualEdge() ResidualEdge {
+	return SimpleResidualEdge{
+		simple.Edge{F: re.T, T: re.F},
+		re.capacity, re.capacity - re.flow,
+		!re.reverse,
+	}
+}
+
+func (re SimpleResidualEdge) WithFlow(flow int32) ResidualEdge {
+	return SimpleResidualEdge{
+		re.Edge,
+		re.capacity, flow,
+		re.reverse,
+	}
+}

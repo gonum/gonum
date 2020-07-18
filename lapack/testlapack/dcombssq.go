c357261fd97454833cd79dfa6659e3990c5a68bf
Vladimir Chalupecky <vladimir@chalupecky.dev>
2020-07-18 21:16:43 +0200

    lapack/gonum: propagate NaN from Dcombssq

--- lapack/testlapack/dcombssq.go
+++ lapack/testlapack/dcombssq.go
@@ -19,13 +19,28 @@ func DcombssqTest(t *testing.T, impl Dcombssqer) {
 	const tol = 1e-15
 
 	rnd := rand.New(rand.NewSource(1))
-	for i := 0; i < 10; i++ {
-		// Generate random, non-negative input.
+	for i := 0; i < 100; i++ {
+		// Generate random, non-negative input, with an occasional NaN.
+		var hasNaN bool
 		scale1 := rnd.Float64()
-		ssq1 := rnd.Float64()
+		var ssq1 float64
+		if rnd.Float64() < 0.5 {
+			ssq1 = math.NaN()
+			hasNaN = true
+		} else {
+			ssq1 = rnd.Float64()
+		}
+
 		scale2 := rnd.Float64()
-		ssq2 := rnd.Float64()
-		switch i {
+		var ssq2 float64
+		if rnd.Float64() < 0.5 {
+			ssq2 = math.NaN()
+			hasNaN = true
+		} else {
+			ssq2 = rnd.Float64()
+		}
+
+		switch rnd.Intn(4) {
 		case 0:
 			scale1 = 0
 		case 1:
@@ -37,6 +52,13 @@ func DcombssqTest(t *testing.T, impl Dcombssqer) {
 		// Compute scale and ssq such that
 		//  scale^2 * ssq := scale1^2 * ssq1 + scale2^2 * ssq2
 		scale, ssq := impl.Dcombssq(scale1, ssq1, scale2, ssq2)
+
+		if hasNaN {
+			if !math.IsNaN(ssq) {
+				t.Errorf("Case %v: unexpected ssq; got %v, want NaN", i, ssq)
+			}
+			continue
+		}
 
 		// Compute the expected result in a non-sophisticated way and
 		// compare against the result we got.

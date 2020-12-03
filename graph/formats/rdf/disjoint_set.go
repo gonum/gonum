782095d131d30bae40095a840573e8b037c75175
Dan Kortschak <dan@kortschak.io>
2020-12-03 11:55:00 +1030

    graph/formats/rdf: implement canonicalisation hashing
    Test cases test001-in.nq to test062-in.nq are from [1] licensed under the
    W3C BSD 3-clause license. Test cases hogan*.nq are from the original paper
    used with permission.
    
    [1]https://json-ld.github.io/normalization/tests/index.html

---
+++ graph/formats/rdf/disjoint_set.go
@@ -0,0 +1,56 @@
+// Copyright ©2020 The Gonum Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package rdf
+
+// djSet implements a disjoint set finder using the union-find algorithm.
+type djSet map[string]*dsNode
+
+// add adds e to the collection of sets held by the disjoint set.
+func (s djSet) add(e string) {
+	if _, ok := s[e]; ok {
+		return
+	}
+	s[e] = &dsNode{}
+}
+
+// union joins two sets a and b within the collection of sets held by
+// the disjoint set.
+func (djSet) union(a, b *dsNode) {
+	ra := find(a)
+	rb := find(b)
+	if ra == rb {
+		return
+	}
+	if ra.rank < rb.rank {
+		ra.parent = rb
+		return
+	}
+	rb.parent = ra
+	if ra.rank == rb.rank {
+		ra.rank++
+	}
+}
+
+// find returns the root of the set containing e.
+func (s djSet) find(e string) *dsNode {
+	n, ok := s[e]
+	if !ok {
+		return nil
+	}
+	return find(n)
+}
+
+// find returns the root of the set containing the set node, n.
+func find(n *dsNode) *dsNode {
+	for ; n.parent != nil; n = n.parent {
+	}
+	return n
+}
+
+// dsNode is a disjoint set element.
+type dsNode struct {
+	parent *dsNode
+	rank   int
+}

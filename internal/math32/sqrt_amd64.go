ccc0da248a78266c0e8a38ac56e983d17c4ba15f
Dan Kortschak <dan@kortschak.io>
2021-04-19 13:54:12 +0930

    all: run make builds pass with go master
    Run gofmt go1.17 adds the new //go:build comments

--- internal/math32/sqrt_amd64.go
+++ internal/math32/sqrt_amd64.go
@@ -6,6 +6,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:build !noasm && !gccgo && !safe
 // +build !noasm,!gccgo,!safe
 
 package math32
--- mathext/internal/amos/amoslib/fortran.go
+++ mathext/internal/amos/amoslib/fortran.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:build fortran
 // +build fortran
 
 // Package amoslib is a wrapper around the Fortran amos library.

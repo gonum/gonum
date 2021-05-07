39cb21da6b4567f8a247c85b00757773eee52e6b
Vladimir Chalupecky <vladimir@chalupecky.dev>
2021-05-07 16:56:29 +0200

    mat: remove TestTridiagNorm
    Tridiag.Norm is now covered by TestNorm

--- mat/tridiag_test.go
+++ mat/tridiag_test.go
@@ -6,7 +6,6 @@ package mat
 
 import (
 	"fmt"
-	"math"
 	"reflect"
 	"testing"
 
@@ -320,20 +319,6 @@ func TestTridiagZero(t *testing.T) {
 				if a.At(i, j) != 0 {
 					t.Errorf("Case n=%d: unexpected non-zero at (%d,%d): got %f", n, i, j, a.At(i, j))
 				}
-			}
-		}
-	}
-}
-
-func TestTridiagNorm(t *testing.T) {
-	t.Parallel()
-	for _, n := range []int{1, 2, 3, 4, 7, 10} {
-		for _, norm := range []float64{1, 2, math.Inf(1)} {
-			a, ref := newTestTridiag(n)
-			want := Norm(ref, norm)
-			got := a.Norm(norm)
-			if got != want {
-				t.Errorf("Case n=%d,norm=%.0f: unexpected norm: got %f, want %f", n, norm, got, want)
 			}
 		}
 	}

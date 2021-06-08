9c4f829931ef8e3ce14a6e12e83ca4f01c0d35da
soypat <graded.sp@gmail.com>
2021-06-08 09:04:53 -0300

    add reconstruction

--- lapack/testlapack/dgetc2.go
+++ lapack/testlapack/dgetc2.go
@@ -6,10 +6,10 @@ package testlapack
 
 import (
 	"fmt"
-	"sort"
 	"testing"
 
 	"golang.org/x/exp/rand"
+	"gonum.org/v1/gonum/blas/blas64"
 )
 
 type Dgetc2er interface {
@@ -18,7 +18,7 @@ type Dgetc2er interface {
 
 func Dgetc2Test(t *testing.T, impl Dgetc2er) {
 	rnd := rand.New(rand.NewSource(1))
-	for _, n := range []int{0, 1, 2, 3, 4, 5, 10, 20} {
+	for _, n := range []int{2} { // []int{0, 1, 2, 3, 4, 5, 10, 20}
 		for _, lda := range []int{n} {
 			dgetc2Test(t, impl, rnd, n, lda)
 		}
@@ -47,22 +47,32 @@ func dgetc2Test(t *testing.T, impl Dgetc2er, rnd *rand.Rand, n, lda int) {
 		t.Fatalf("%v: matrix was perturbed at %d", name, k)
 	}
 
-	// sort pivot indices and verify all indices up to n-1 are present
-	sort.Ints(ipiv)
-	sort.Ints(jpiv)
-	errcount := 0
+	// Verify all indices are set.
 	for i := 0; i < n; i++ {
-		if ipiv[i] != i {
-			t.Errorf("%v: ipiv[%d] does not correspond", name, i)
-			errcount++
+		if ipiv[i] < 0 {
+			t.Errorf("%v: ipiv[%d] is negative", name, i)
 		}
-		if jpiv[i] != i {
-			t.Errorf("%v: jpiv[%d] does not correspond", name, i)
-			errcount++
+		if jpiv[i] < 0 {
+			t.Errorf("%v: jpiv[%d] is negative", name, i)
 		}
 	}
-	if errcount > 0 {
-		t.Errorf("ipiv:%d", ipiv)
-		t.Errorf("jpiv:%d", jpiv)
+
+	// Try to reconstruct a from ipiv and jpiv.
+	bi := blas64.Implementation()
+	for i := n - 1; i >= 0; i-- {
+		ipv, jpv := ipiv[i], jpiv[i]
+
+		if jpv != i {
+			bi.Dswap(n, aout[i:], lda, aout[jpv:], lda)
+		}
+		if ipv != i {
+			bi.Dswap(n, aout[i*lda:], 1, aout[ipv*lda:], 1)
+		}
+	}
+
+	for i := range aout {
+		if aout[i] != ap.Data[i] {
+			t.Errorf("%v: matrix %d idx not equal after reconstruction", name, i)
+		}
 	}
 }

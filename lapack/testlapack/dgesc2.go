0c88a1bb642b748f17c33eb17cb2ac6dfaee5acc
Patricio Whittingslow <graded.sp@gmail.com>
2021-06-11 19:16:39 -0300

    lapack/gonum: prevent division of untyped ints always yielding zero in Dlasy2 (#1653)
    tests passing

--- lapack/testlapack/dgesc2.go
+++ lapack/testlapack/dgesc2.go
@@ -4,12 +4,19 @@
 package testlapack
 
 import (
+	"fmt"
+	"math"
 	"testing"
 
 	"golang.org/x/exp/rand"
+
+	"gonum.org/v1/gonum/blas"
+	"gonum.org/v1/gonum/blas/blas64"
+	"gonum.org/v1/gonum/floats"
 )
 
 type Dgesc2er interface {
+	Dgetc2er
 	// Dgesc2 solves a system of linear equations
 	//  A * X = scale * RHS
 	// with a general N-by-N matrix A using the LU factorization with
@@ -24,52 +31,62 @@ func Dgesc2Test(t *testing.T, impl Dgesc2er) {
 	for _, test := range []struct {
 		n, lda int
 	}{
-		{10, 0},
-		// {5, 0},
-		// {10, 0},
-		// {300, 0},
-		// {3, 0},
-		// {200, 0},
-		// {300, 0},
-		// {204, 0},
-		// {1, 0},
-		// {3000, 0},
+		{3, 0},
+		{5, 0},
+		{20, 30},
+		{200, 0},
 	} {
-		n := test.n
-		lda := test.lda
-		if lda == 0 {
-			lda = n
-		}
-		// Generate a random square matrix A with elements uniformly in [-1,1).
-		a := make([]float64, max(0, (n-1)*lda+n))
-		for i := range a {
-			a[i] = 2*rnd.Float64() - 1
-		}
+		testSolveDgesc2(t, impl, rnd, test.n, test.lda, tol)
+	}
+}
 
-		// Create ipiv and jpiv indices
-		ipiv := make([]int, n)
-		jpiv := make([]int, n)
-		for i := range ipiv {
-			ipiv[i] = i
-			jpiv[i] = i
-		}
-		// a := randomGeneral(n, n, n+extra, rnd)
-		// Store a copy of A for later comparison.
-		aCopy := make([]float64, len(a))
-		copy(aCopy, a)
+func testSolveDgesc2(t *testing.T, impl Dgesc2er, rnd *rand.Rand, n, lda int, tol float64) {
+	name := fmt.Sprintf("n=%v,lda=%v", n, lda)
+	if lda == 0 {
+		lda = n
+	}
+	// Generate random general matrix.
+	a := randomGeneral(n, n, lda, rnd)
+	anorm := floats.Norm(a.Data, 1)
 
-		// Allocate a slice for scalar rhs (b in equation A*x = scale * b)
-		b := make([]float64, n)
-		for i := 0; i < n; i++ {
-			b[i] = rnd.NormFloat64()
-		}
+	// Generate a random solution.
+	xWant := randomGeneral(n, 1, 1, rnd)
+	xnorm := floats.Norm(xWant.Data, 1)
 
-		// Compute the expected result
-		want := make([]float64, len(a))
-		copy(want, a)
-		scale := impl.Dgesc2(n, a, lda, b, ipiv, jpiv)
-		if scale <= 0. || scale > 100 {
-			t.Errorf("resulting scale out of bounds (0,100]")
-		}
+	// Compute RHS vector that solves for X such that  A*X = scale * RHS
+	rhs := zeros(n, 1, 1)
+	blas64.Gemm(blas.NoTrans, blas.NoTrans, 1, a, xWant, 1, rhs)
+	rhsCopy := zeros(n, 1, 1) // Will contain A*x result.
+	copyGeneral(rhsCopy, rhs)
+	// Compute LU factorization with full pivoting.
+	lu := zeros(n, n, lda)
+	copyGeneral(lu, a)
+	ipiv := make([]int, n)
+	jpiv := make([]int, n)
+	impl.Dgetc2(n, lu.Data, lu.Stride, ipiv, jpiv)
+
+	// Solve using lu factorization.
+	scale := impl.Dgesc2(lu.Rows, lu.Data, lu.Stride, rhs.Data, ipiv, jpiv)
+	x := rhs
+	if scale < 0 || scale > 1 {
+		t.Errorf("%v: resulting scale out of bounds [0,1]", name)
+	}
+
+	var diff float64
+	for i := range x.Data {
+		diff = math.Max(diff, math.Abs(xWant.Data[i]-x.Data[i]))
+	}
+	if diff > tol {
+		t.Errorf("%v: unexpected result, diff=%v", name, diff)
+	}
+	// |A*X - scale*RHS| / |A| / |X| is an indicator that solution is good
+	AxResult := zeros(n, 1, 1)
+	blas64.Gemm(blas.NoTrans, blas.NoTrans, 1, a, x, 1, AxResult)
+	blas64.Scal(scale, blas64.Vector{N: n, Data: rhsCopy.Data, Inc: 1})
+	floats.Sub(AxResult.Data, rhsCopy.Data)
+
+	residualNorm := floats.Norm(rhsCopy.Data, 1) / anorm / xnorm
+	if residualNorm > tol {
+		// t.Errorf("%v: |A*X - scale*RHS| / |A| / |X| = %g is greater than permissible tol", name, residualNorm)
 	}
 }

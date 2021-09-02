06c2c527382aa521a169abf8586cf9860c12c71c
Vladimir Chalupecky <vladimir@chalupecky.dev>
2021-09-02 16:22:51 +0200

    lapack/{gonum,testlapack}: clean up Dorgr2 and its test

--- lapack/gonum/dorgr2.go
+++ lapack/gonum/dorgr2.go
@@ -9,39 +9,34 @@ import (
 	"gonum.org/v1/gonum/blas/blas64"
 )
 
-// Dorgr2 generates an m×n real matrix Q with orthonormal rows,
-// which is defined as the last m rows of a product of k elementary
-// reflectors of order n
+// Dorgr2 generates an m×n real matrix Q with orthonormal rows, which is defined
+// as the last m rows of a product of k elementary reflectors of order n
 //  Q = H_0 * H_1 * ... * H_{k-1}
 // as returned by Dgerqf.
 //
-// Each entry of tau contains the scalar factor of the elementary reflector H_i
-// and the length of tau must be at least k. The length of work must be at least m.
-// a is a matrix of dimensions (n,lda). On entry the [m-k+i-1]-th row must contain (counting from zero)
-// the vector which defines the elementary reflector H_i, for i = 0,1,2,...,k-1, as
-// returned by Dgerqf in the last k rows of its array argument A.
-// On exit, the m×n matrix Q.
-//  n >= m >= k >= 0
+// On entry, the (m-k+i)-th row of A must contain the vector which defines the
+// elementary reflector H_i, for i = 0,1,...,k, as returned by Dgerqf. On
+// return, A will contain the m×n matrix Q.
 //
-// Dorgr2 will panic if the conditions on input values are not met.
+// The i-th element of tau must contain the scalar factor of the elementary
+// reflector H_i, as returned by Dgerqf.
+//
+// It must hold that
+//  n >= m >= k >= 0,
+// the length of tau must be k and the length of work must be m, otherwise
+// Dorgr2 will panic.
 //
 // Dorgr2 is an internal routine. It is exported for testing purposes.
 func (impl Implementation) Dorgr2(m, n, k int, a []float64, lda int, tau, work []float64) {
 	switch {
-	case m < 0:
-		panic(mLT0)
-	case n < 0:
-		panic(nLT0)
-	case m > n:
-		panic(mGTN)
 	case k < 0:
 		panic(kLT0)
-	case k > m:
+	case m < k:
 		panic(kGTM)
+	case n < m:
+		panic(mGTN)
 	case lda < max(1, n):
 		panic(badLdA)
-	case len(work) < m:
-		panic(shortWork)
 	}
 
 	// Quick return if possible.
@@ -54,16 +49,17 @@ func (impl Implementation) Dorgr2(m, n, k int, a []float64, lda int, tau, work [
 		panic(badLenTau)
 	case len(a) < (m-1)*lda+n:
 		panic(shortA)
+	case len(work) < m:
+		panic(shortWork)
 	}
 
-	if k < m {
-		// Initialise rows 0:m-k to rows of the unit matrix.
-		for l := 0; l < m-k; l++ {
-			for j := 0; j < n; j++ {
-				a[l*lda+j] = 0
-			}
-			a[l*lda+n-m+l] = 1
+	// Initialise rows 0:m-k to rows of the unit matrix.
+	for l := 0; l < m-k; l++ {
+		row := a[l*lda : l*lda+n]
+		for j := range row {
+			row[j] = 0
 		}
+		a[l*lda+n-m+l] = 1
 	}
 	bi := blas64.Implementation()
 	for i := 0; i < k; i++ {

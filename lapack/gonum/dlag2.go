b2a0043a313e1b9132ef535d6855bde090585e71
Vladimir Chalupecky <vladimir@chalupecky.dev>
2021-10-08 17:13:24 +0200

    lapack/gonum: clean up Dlag2 and its test

--- lapack/gonum/dlag2.go
+++ lapack/gonum/dlag2.go
@@ -6,61 +6,39 @@ package gonum
 
 import "math"
 
-// Dlag2 computes the eigenvalues of a 2×2 generalized eigenvalue
-// problem
-//  A - w*B,
-// with scaling as necessary to avoid over-/underflow.
-// The scaling factor "s" results in a modified eigenvalue equation
+// Dlag2 computes the eigenvalues of a 2×2 generalized eigenvalue problem
+//  A - w*B
+// where B is an upper triangular matrix.
+//
+// Dlag2 uses scaling as necessary to avoid over-/underflow. Scaling results in
+// a modified eigenvalue problem
 //  s*A - w*B
-// where  s  is a non-negative scaling factor chosen so that  w,  w*B,
-// and  s*A  do not overflow and, if possible, do not underflow, either.
+// where s is a non-negative scaling factor chosen so that w, w*B, and s*A do
+// not overflow and, if possible, do not underflow, either.
 //
-// B is an upper triangular ldb×2 matrix
-// On entry, the 2×2 upper triangular matrix B.  It is
-// assumed that the one-norm of B is less than 1/dlamchS.  The
-// diagonals should be at least sqrt(dlamchS) times the largest
-// element of B (in absolute value); if a diagonal is smaller
-// than that, then  +/- sqrt(dlamchS) will be used instead of
-// that diagonal.
+// scale1 and scale2 are used to avoid over-/underflow in the eigenvalue
+// equation which defines the first and second eigenvalue respectively. Note
+// that scale1 and scale2 may be zero or less than the underflow threshold if
+// the corresponding exact eigenvalue is sufficiently large.
 //
-// It is assumed that A's 1-norm is less than 1/safmin.  Entries less than
-// sqrt(safmin)*norm(A) are subject to being treated as zero.
+// If the eigenvalues are real, then:
+//  - wi is zero,
+//  - the eigenvalues are wr1/scale1 and wr2/scale2.
+//
+// If the eigenvalues are complex, then:
+//  - wi is non-negative,
+//  - the eigenvalues are (wr1 ± wi*i)/scale1,
+//  - wr1 = wr2,
+//  - scale1 = scale2.
+//
+// Dlag2 assumes that the one-norm of A and B is less than 1/dlamchS. Entries of
+// A less than sqrt(dlamchS)*norm(A) are subject to being treated as zero. The
+// diagonals of B should be at least sqrt(dlamchS) times the largest element of
+// B (in absolute value); if a diagonal is smaller than that, then
+// ±sqrt(dlamchS) will be used instead of that diagonal.
 //
 // Dlag2 is an internal routine. It is exported for testing purposes.
-//
-// scale1 is used to avoid over-/underflow in the
-// eigenvalue equation which defines the first eigenvalue.  If
-// the eigenvalues are complex, then the eigenvalues are
-// ( wr1  +/-  wi*i ) / scale1  (which may lie outside the
-// exponent range of the machine), scale1=scale2, and scale1
-// will always be positive.  If the eigenvalues are real, then
-// the first (real) eigenvalue is  wr1 / scale1 , but this may
-// overflow or underflow, and in fact, scale1 may be zero or
-// less than the underflow threshold if the exact eigenvalue
-// is sufficiently large.
-//
-// scale2 is used to avoid over-/underflow in the
-// eigenvalue equation which defines the second eigenvalue.  If
-// the eigenvalues are complex, then scale2=scale1.  If the
-// eigenvalues are real, then the second (real) eigenvalue is
-// wr2 / scale2 , but this may overflow or underflow, and in
-// fact, scale2 may be zero or less than the underflow
-// threshold if the exact eigenvalue is sufficiently large.
-//
-// If the eigenvalue is real, then wr1 is scale1 times the
-// eigenvalue closest to the (1,1) element of A*B^{-1}.  If the
-// eigenvalue is complex, then wr1=wr2 is scale1 times the real
-// part of the eigenvalues.
-//
-// If the eigenvalue is real, then wr2 is scale2 times the
-// other eigenvalue.  If the eigenvalue is complex, then
-// wr1=wr2 is scale1 times the real part of the eigenvalues.
-//
-// If the eigenvalue is real, then wi is zero.  If the
-// eigenvalue is complex, then wi is scale1 times the imaginary
-// part of the eigenvalues.  wi will always be non-negative.
 func (Implementation) Dlag2(a []float64, lda int, b []float64, ldb int) (scale1, scale2, wr1, wr2, wi float64) {
-
 	switch {
 	case lda < 2:
 		panic(badLdA)
@@ -74,13 +52,13 @@ func (Implementation) Dlag2(a []float64, lda int, b []float64, ldb int) (scale1,
 
 	const (
 		safmin = dlamchS
+		safmax = 1 / safmin
 		fuzzy1 = 1 + 1e-5
 	)
 	rtmin := math.Sqrt(safmin)
 	rtmax := 1 / rtmin
-	safmax := 1 / safmin
 
-	// Scale a.
+	// Scale A.
 	anorm := math.Max(math.Abs(a[0])+math.Abs(a[lda]),
 		math.Abs(a[1])+math.Abs(a[lda+1]))
 	anorm = math.Max(anorm, safmin)
@@ -90,7 +68,7 @@ func (Implementation) Dlag2(a []float64, lda int, b []float64, ldb int) (scale1,
 	a12 := ascale * a[1]
 	a22 := ascale * a[lda+1]
 
-	// Perturb b if necessary to insure non-singularity.
+	// Perturb B if necessary to insure non-singularity.
 	b11 := b[0]
 	b12 := b[1]
 	b22 := b[ldb+1]
@@ -112,35 +90,42 @@ func (Implementation) Dlag2(a []float64, lda int, b []float64, ldb int) (scale1,
 	b22 *= bscale
 
 	// Compute larger eigenvalue by method described by C. van Loan.
+	var (
+		as12, abi22   float64
+		pp, qq, shift float64
+	)
 	binv11 := 1 / b11
 	binv22 := 1 / b22
 	s1 := a11 * binv11
 	s2 := a22 * binv22
-	var as11, as12, as22, abi22, shift float64
-	var qq, ss, pp, discr, r float64
+	// AS is A shifted by -shift*B.
 	if math.Abs(s1) <= math.Abs(s2) {
-		as12 = a12 - s1*b12
-		as22 = a22 - s1*b22
-		ss = a21 * (binv11 * binv22)
+		shift = s1
+		as12 = a12 - shift*b12
+		as22 := a22 - shift*b22
+		ss := a21 * (binv11 * binv22)
 		abi22 = as22*binv22 - ss*b12
 		pp = 0.5 * abi22
-		shift = s1
+		qq = ss * as12
 	} else {
-		as12 = a12 - s2*b12
-		as11 = a11 - s2*b11
-		ss = a21 * (binv11 * binv22)
+		shift = s2
+		as12 = a12 - shift*b12
+		as11 := a11 - shift*b11
+		ss := a21 * (binv11 * binv22)
 		abi22 = -ss * b12
 		pp = 0.5 * (as11*binv11 + abi22)
-		shift = s2
+		qq = ss * as12
 	}
-	qq = ss * as12
-	pp2 := pp * pp
+	var discr, r float64
 	if math.Abs(pp*rtmin) >= 1 {
-		discr = rtmin*rtmin*pp2 + qq*safmin
+		tmp := rtmin * pp
+		discr = tmp*tmp + qq*safmin
 		r = math.Sqrt(math.Abs(discr)) * rtmax
 	} else {
+		pp2 := pp * pp
 		if pp2+math.Abs(qq) <= safmin {
-			discr = rtmax*rtmax*pp2 + qq*safmax
+			tmp := rtmax * pp
+			discr = tmp*tmp + qq*safmax
 			r = math.Sqrt(math.Abs(discr)) * rtmin
 		} else {
 			discr = pp2 + qq
@@ -148,11 +133,13 @@ func (Implementation) Dlag2(a []float64, lda int, b []float64, ldb int) (scale1,
 		}
 	}
 
-	// Note: the test of R in the following `if` is to cover the case when
-	// discr is small and negative and is flushed to zero during
-	// the calculation of R.  On machines which have a consistent
-	// flush-to-zero threshold and handle numbers above that
-	// threshold correctly, it would not be necessary.
+	// TODO(vladimir-ch): Is the following comment from the reference needed in
+	// a Go implementation?
+	//
+	// Note: the test of r in the following `if` is to cover the case when discr
+	// is small and negative and is flushed to zero during the calculation of r.
+	// On machines which have a consistent flush-to-zero threshold and handle
+	// numbers above that threshold correctly, it would not be necessary.
 	if discr >= 0 || r == 0 {
 		sum := pp + math.Copysign(r, pp)
 		diff := pp - math.Copysign(r, pp)
@@ -181,19 +168,19 @@ func (Implementation) Dlag2(a []float64, lda int, b []float64, ldb int) (scale1,
 
 	// Further scaling to avoid underflow and overflow in computing
 	// scale1 and overflow in computing w*B.
+	//
 	// This scale factor (wscale) is bounded from above using c1 and c2,
-	// and from below using c3 and c4.
-	// c1 implements the condition s*A must never overflow.
-	// c2 implements the condition w*B must never overflow.
-	// c3, with c2,
-	// implement the condition that s*A - w*B must never overflow.
-	// c4 implements the condition s should not underflow.
-	// c5 implements the condition max(s,|w|) should be at least 2.
+	// and from below using c3 and c4:
+	//  - c1 implements the condition s*A must never overflow.
+	//  - c2 implements the condition w*B must never overflow.
+	//  - c3, with c2, implement the condition that s*A - w*B must never overflow.
+	//  - c4 implements the condition s should not underflow.
+	//  - c5 implements the condition max(s,|w|) should be at least 2.
 	c1 := bsize * (safmin * math.Max(1, ascale))
 	c2 := safmin * math.Max(1, bnorm)
 	c3 := bsize * safmin
-	c4 := 1.
-	c5 := 1.
+	c4 := 1.0
+	c5 := 1.0
 	if ascale <= 1 || bsize <= 1 {
 		c5 = math.Min(1, ascale*bsize)
 		if ascale <= 1 && bsize <= 1 {
@@ -241,5 +228,6 @@ func (Implementation) Dlag2(a []float64, lda int, b []float64, ldb int) (scale1,
 			scale2 = ascale * bsize
 		}
 	}
+
 	return scale1, scale2, wr1, wr2, wi
 }
--- lapack/testlapack/dlag2.go
+++ lapack/testlapack/dlag2.go
@@ -10,7 +10,9 @@ import (
 	"testing"
 
 	"golang.org/x/exp/rand"
+
 	"gonum.org/v1/gonum/blas/blas64"
+	"gonum.org/v1/gonum/floats"
 )
 
 type Dlag2er interface {
@@ -21,142 +23,197 @@ func Dlag2Test(t *testing.T, impl Dlag2er) {
 	rnd := rand.New(rand.NewSource(1))
 	for _, lda := range []int{2, 5} {
 		for _, ldb := range []int{2, 5} {
-			for i := 0; i <= 80; i++ { // 80 iterations for variability.
-				dlag2Test(t, impl, rnd, lda, ldb)
+			for aKind := 0; aKind <= 20; aKind++ {
+				for bKind := 0; bKind <= 20; bKind++ {
+					dlag2Test(t, impl, rnd, lda, ldb, aKind, bKind)
+				}
 			}
 		}
 	}
 }
 
-func dlag2Test(t *testing.T, impl Dlag2er, rnd *rand.Rand, lda, ldb int) {
+func dlag2Test(t *testing.T, impl Dlag2er, rnd *rand.Rand, lda, ldb int, aKind, bKind int) {
 	const tol = 1e-14
-	a := randomGeneral(2, 2, lda, rnd)
-	b := randomGeneral(2, 2, ldb, rnd)
-	a11 := a.Data[0]
-	a12 := a.Data[1]
-	a21 := a.Data[lda]
-	a22 := a.Data[lda+1]
-	b11 := b.Data[0]
-	b12 := b.Data[1]
-	b22 := b.Data[ldb+1]
-	name := fmt.Sprintf("lda=%d, ldb=%d", lda, ldb)
+
+	a := makeDlag2TestMatrix(rnd, lda, aKind)
+	b := makeDlag2TestMatrix(rnd, ldb, bKind)
+
+	aCopy := cloneGeneral(a)
+	bCopy := cloneGeneral(b)
+
 	scale1, scale2, wr1, wr2, wi := impl.Dlag2(a.Data, a.Stride, b.Data, b.Stride)
-	if wi < 0 {
-		t.Errorf("%v: wi can not be negative wi=%v", name, wi)
+
+	name := fmt.Sprintf("lda=%d,ldb=%d,aKind=%d,bKind=%d", lda, ldb, aKind, bKind)
+	aStr := fmt.Sprintf("A = [%g,%g]\n    [%g,%g]", a.Data[0], a.Data[1], a.Data[a.Stride], a.Data[a.Stride+1])
+	bStr := fmt.Sprintf("B = [%g,%g]\n    [%g,%g]", b.Data[0], b.Data[1], 0.0, b.Data[b.Stride+1])
+
+	if !floats.Same(a.Data, aCopy.Data) {
+		t.Errorf("%s: unexpected modification of a", name)
+	}
+	if !floats.Same(b.Data, bCopy.Data) {
+		t.Errorf("%s: unexpected modification of b", name)
 	}
 
-	dget53Test(t, a, b, scale1, wr1, wi)
-	dget53Test(t, a, b, scale2, wr2, wi)
+	if wi < 0 {
+		t.Fatalf("%s: wi is negative; wi=%g,\n%s\n%s", name, wi, aStr, bStr)
+		return
+	}
 
-	// Complex eigenvalue solution.
 	if wi > 0 {
 		if wr1 != wr2 {
-			t.Errorf("%v: wr1 should equal wr2 for complex eigenvalues. got %v!=%v", name, wr1, wr2)
+			t.Fatalf("%s: complex eigenvalue but wr1 != wr2; wr1=%g, wr2=%g,\n%s\n%s", name, wr1, wr2, aStr, bStr)
+			return
 		}
-		idet1 := cmplxdet2x2(complex(scale1*a11-wr1*b11, -wi*b11), complex(scale1*a12-wr1*b12, -wi*b12),
-			complex(scale1*a21, 0), complex(scale1*a22-wr1*b22, -wi*b22))
-		idet2 := cmplxdet2x2(complex(scale1*a11-wr1*b11, wi*b11), complex(scale1*a12-wr1*b12, wi*b12),
-			complex(scale1*a21, 0), complex(scale1*a22-wr1*b22, wi*b22))
-
-		if math.Abs(real(idet1))+math.Abs(imag(idet1)) > tol || math.Abs(real(idet2))+math.Abs(imag(idet2)) > tol {
-			t.Errorf("%v: (%.4v±%.4vi)/%.4v did not solve eigenvalue problem", name, wr1, wi, scale1)
+		if scale1 != scale2 {
+			t.Fatalf("%s: complex eigenvalue but scale1 != scale2; scale1=%g, scale2=%g,\n%s\n%s", name, scale1, scale2, aStr, bStr)
+			return
 		}
-		return // Complex solution verified
 	}
 
-	// Real eigenvalue solution.
-	// |s*A - w*B| = 0   is solution for eigenvalue problem. Calculate distance from solution.
-	res1 := det2x2(scale1*a11-wr1*b11, scale1*a12-wr1*b12,
-		scale1*a21, scale1*a22-wr1*b22)
-	if math.Abs(res1) > tol {
-		t.Errorf("%v: got a residual from |%0.4v*A - w1*B| > tol: %v", name, scale1, res1)
+	resid, err := residualDlag2(a, b, scale1, complex(wr1, wi))
+	if err != nil {
+		t.Logf("%s: invalid input data: %v\n%s\n%s", name, err, aStr, bStr)
+		return
 	}
-	res2 := det2x2(scale2*a11-wr2*b11, scale2*a12-wr2*b12,
-		scale2*a21, scale2*a22-wr2*b22)
-	if math.Abs(res2) > tol {
-		t.Errorf("%v: got a residual from |s*A - w2*B| > tol: %v", name, res1)
+	if resid > tol || math.IsNaN(resid) {
+		t.Errorf("%s: unexpected first eigenvalue %g with s=%g; resid=%g, want<=%g\n%s\n%s", name, complex(wr1, wi), scale1, resid, tol, aStr, bStr)
+	}
+
+	resid, err = residualDlag2(a, b, scale2, complex(wr2, -wi))
+	if err != nil {
+		t.Logf("%s: invalid input data: %s\n%s\n%s", name, err, aStr, bStr)
+		return
+	}
+	if resid > tol || math.IsNaN(resid) {
+		t.Errorf("%s: unexpected second eigenvalue %g with s=%g; resid=%g, want<=%g\n%s\n%s", name, complex(wr2, -wi), scale2, resid, tol, aStr, bStr)
 	}
 }
 
-// dget53Test checks the generalized eigenvalues computed by dlag2.
-// The basic test for an eigenvalue is:
-//                           | det( s*A - w*B ) |
-//  result =  ---------------------------------------------------
-//            ulp max( s*norm(A), |w|*norm(B) )*norm( s*A - w*B )
-//
-// If s and w cant be scaled result will be 1/ulp.
-func dget53Test(t *testing.T, a, b blas64.General, scale, wr, wi float64) (result float64) {
-	lda := a.Stride
-	ldb := b.Stride
-	// Machine constants and norms.
-	safmin := dlamchS
-	ulp := dlamchE * dlamchB
-	absw := math.Abs(wr) + math.Abs(wi)
-	anorm := math.Max(math.Abs(a.Data[0])+math.Abs(a.Data[1]), math.Max(math.Abs(a.Data[lda])+math.Abs(a.Data[lda+1]), safmin))
-	bnorm := math.Max(math.Abs(b.Data[0]), math.Max(math.Abs(b.Data[ldb])+math.Abs(b.Data[ldb+1]), safmin))
+func makeDlag2TestMatrix(rnd *rand.Rand, ld, kind int) blas64.General {
+	a := zeros(2, 2, ld)
+	switch kind {
+	case 0:
+		// Zero matrix.
+	case 1:
+		// Identity.
+		a.Data[0] = 1
+		a.Data[a.Stride+1] = 1
+	case 2:
+		// Large diagonal.
+		a.Data[0] = 2 * safmax
+		a.Data[a.Stride+1] = 2 * safmax
+	case 3:
+		// Tiny diagonal.
+		a.Data[0] = safmin
+		a.Data[a.Stride+1] = safmin
+	case 4:
+		// Tiny and large diagonal.
+		a.Data[0] = safmin
+		a.Data[a.Stride+1] = safmax
+	case 5:
+		// Large and tiny diagonal.
+		a.Data[0] = safmax
+		a.Data[a.Stride+1] = safmin
+	case 6:
+		// Large complex eigenvalue.
+		a.Data[0] = safmax
+		a.Data[1] = safmax
+		a.Data[a.Stride] = -safmax
+		a.Data[a.Stride+1] = safmax
+	case 7:
+		// Tiny complex eigenvalue.
+		a.Data[0] = safmin
+		a.Data[1] = safmin
+		a.Data[a.Stride] = -safmin
+		a.Data[a.Stride+1] = safmin
+	case 8:
+		// Random matrix with large elements.
+		a.Data[0] = safmax * (2*rnd.Float64() - 1)
+		a.Data[1] = safmax * (2*rnd.Float64() - 1)
+		a.Data[a.Stride] = safmax * (2*rnd.Float64() - 1)
+		a.Data[a.Stride+1] = safmax * (2*rnd.Float64() - 1)
+	case 9:
+		// Random matrix with tiny elements.
+		a.Data[0] = safmin * (2*rnd.Float64() - 1)
+		a.Data[1] = safmin * (2*rnd.Float64() - 1)
+		a.Data[a.Stride] = safmin * (2*rnd.Float64() - 1)
+		a.Data[a.Stride+1] = safmin * (2*rnd.Float64() - 1)
+	default:
+		// Random matrix.
+		a = randomGeneral(2, 2, ld, rnd)
+	}
+	return a
+}
+
+// residualDlag2 returns the value of
+//             | det( s*A - w*B ) |
+//  -------------------------------------------
+//  max(s*norm(A), |w|*norm(B))*norm(s*A - w*B)
+// that can be used to check the generalized eigenvalues computed by Dlag2 and
+// an error that indicates invalid input data.
+func residualDlag2(a, b blas64.General, s float64, w complex128) (float64, error) {
+	const ulp = dlamchP
+
+	a11, a12 := a.Data[0], a.Data[1]
+	a21, a22 := a.Data[a.Stride], a.Data[a.Stride+1]
+
+	b11, b12 := b.Data[0], b.Data[1]
+	b22 := b.Data[b.Stride+1]
+
+	// Compute norms.
+	absw := zabs(w)
+	anorm := math.Max(math.Abs(a11)+math.Abs(a21), math.Abs(a12)+math.Abs(a22))
+	anorm = math.Max(anorm, safmin)
+	bnorm := math.Max(math.Abs(b11), math.Abs(b12)+math.Abs(b22))
+	bnorm = math.Max(bnorm, safmin)
 
 	// Check for possible overflow.
-	temp := (safmin*bnorm)*absw + (safmin*anorm)*scale
-
-	scales := scale
-	wrs := wr
-	wis := wi
+	temp := (safmin*anorm)*s + (safmin*bnorm)*absw
 	if temp >= 1 {
-		t.Error("dget53: s*norm(A) + |w|*norm(B) > 1/safe_minimum")
-		temp = 1 / temp
-		scales *= temp
-		wrs *= temp
-		wis *= temp
-		absw = math.Abs(wrs) + math.Abs(wis)
+		// Scale down to avoid overflow.
+		s /= temp
+		w = scale(1/temp, w)
+		absw = zabs(w)
 	}
-	s1 := math.Max(ulp*math.Max(scales*anorm, absw*bnorm), safmin*math.Max(scales, absw))
 
-	// Check for W and SCALE essentially zero.
+	// Check for w and s essentially zero.
+	s1 := math.Max(ulp*math.Max(s*anorm, absw*bnorm), safmin*math.Max(s, absw))
 	if s1 < safmin {
-		t.Error("dget53: ulp*max( s*norm(A), |w|*norm(B) ) < safe_minimum")
-		if scales < safmin && absw < safmin {
-			t.Error("dget53: s and w could not be scaled so as to compute test")
-			return 1 / ulp
+		if s < safmin && absw < safmin {
+			return 1 / ulp, fmt.Errorf("ulp*max(s*|A|,|w|*|B|) < safmin and s and w could not be scaled; s=%g, |w|=%g", s, absw)
 		}
 		// Scale up to avoid underflow.
-		temp = 1 / math.Max(scales*anorm+absw*bnorm, safmin)
-		scales *= temp
-		wrs *= temp
-		wis *= temp
-		absw = math.Abs(wrs) + math.Abs(wis)
-		s1 = math.Max(ulp*math.Max(scales*anorm, absw*bnorm),
-			safmin*math.Max(scales, absw))
+		temp = 1 / math.Max(s*anorm+absw*bnorm, safmin)
+		s *= temp
+		w = scale(temp, w)
+		absw = zabs(w)
+		s1 = math.Max(ulp*math.Max(s*anorm, absw*bnorm), safmin*math.Max(s, absw))
 		if s1 < safmin {
-			t.Error("dget53: s and w could not be scaled so as to compute test")
-			return 1 / ulp
+			return 1 / ulp, fmt.Errorf("ulp*max(s*|A|,|w|*|B|) < safmin and s and w could not be scaled; s=%g, |w|=%g", s, absw)
 		}
 	}
 
 	// Compute C = s*A - w*B.
-	cr11 := scales*a.Data[0] - wrs*b.Data[0]
-	ci11 := -wis * b.Data[0]
-	cr21 := scales * a.Data[1]
-	cr12 := scales*a.Data[lda] - wrs*b.Data[ldb]
-	ci12 := -wis * b.Data[ldb]
-	cr22 := scales*a.Data[lda+1] - wrs*b.Data[ldb+1]
-	ci22 := -wis * b.Data[ldb+1]
+	c11 := complex(s*a11, 0) - w*complex(b11, 0)
+	c12 := complex(s*a12, 0) - w*complex(b12, 0)
+	c21 := complex(s*a21, 0)
+	c22 := complex(s*a22, 0) - w*complex(b22, 0)
+	// Compute norm(s*A - w*B).
+	cnorm := math.Max(zabs(c11)+zabs(c21), zabs(c12)+zabs(c22))
+	// Compute det(s*A - w*B)/norm(s*A - w*B).
+	cs := 1 / math.Sqrt(math.Max(cnorm, safmin))
+	det := cmplxdet2x2(scale(cs, c11), scale(cs, c12), scale(cs, c21), scale(cs, c22))
+	// Compute |det(s*A - w*B)|/(norm(s*A - w*B)*max(s*norm(A), |w|*norm(B))).
+	return zabs(det) / s1 * ulp, nil
+}
 
-	// Compute the smallest singular value of s*A - w*B:
-	//                 |det( s*A - w*B )|
-	//     sigma_min = ------------------
-	//                 norm( s*A - w*B )
-	cnorm := math.Max(math.Abs(cr11)+math.Abs(ci11)+math.Abs(cr21),
-		math.Max(math.Abs(cr12)+math.Abs(ci12)+math.Abs(cr22)+math.Abs(ci22), safmin))
-	cscale := 1 / math.Sqrt(cnorm)
-	detr := (cscale*cr11)*(cscale*cr22) -
-		(cscale*ci11)*(cscale*ci22) -
-		(cscale*cr12)*(cscale*cr21)
-	deti := (cscale*cr11)*(cscale*ci22) +
-		(cscale*ci11)*(cscale*cr22) -
-		(cscale*ci12)*(cscale*cr21)
-	sigmin := math.Abs(detr) + math.Abs(deti)
-	result = sigmin / s1
-	return result
+func zabs(z complex128) float64 {
+	return math.Abs(real(z)) + math.Abs(imag(z))
+}
+
+// scale scales the complex number c by f.
+func scale(f float64, c complex128) complex128 {
+	return complex(f*real(c), f*imag(c))
 }
 
 // cmplxdet2x2 returns the determinant of

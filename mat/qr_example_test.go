b9390655b73840721e16891f0a8cfbad942b4c58
Sebastien Binet <binet@cern.ch>
2022-01-11 17:40:02 +0100

    PS1

--- mat/qr_example_test.go
+++ mat/qr_example_test.go
@@ -12,39 +12,30 @@ import (
 )
 
 func ExampleQR_solveTo() {
+	// QR factorization can be used for solving linear inverse problems,
+	// as this is a more numerically stable technique than direct
+	// matrix inversion.
+	//
+	// Here, we want to solve:
+	//   Ax = b
+
 	var (
-		x = []float64{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-		y = []float64{1, 6, 17, 34, 57, 86, 121, 162, 209, 262, 321}
-
-		degree = 2
-
-		a = Vandermonde(x, degree+1)
-		b = mat.NewDense(len(y), 1, y)
-		c = mat.NewDense(degree+1, 1, nil)
+		a = mat.NewDense(4, 2, []float64{0, 1, 1, 1, 1, 1, 2, 1})
+		b = mat.NewDense(4, 1, []float64{1, 0, 2, 1})
+		x = mat.NewDense(2, 1, nil)
 	)
 
 	var qr mat.QR
 	qr.Factorize(a)
 
 	const trans = false
-	err := qr.SolveTo(c, trans, b)
+	err := qr.SolveTo(x, trans, b)
 	if err != nil {
 		log.Fatalf("could not solve QR: %+v", err)
 	}
-	fmt.Printf("%.3f\n", mat.Formatted(c))
+	fmt.Printf("%.3f\n", mat.Formatted(x))
 
 	// Output:
-	// ⎡1.000⎤
-	// ⎢2.000⎥
-	// ⎣3.000⎦
-}
-
-func Vandermonde(a []float64, d int) *mat.Dense {
-	x := mat.NewDense(len(a), d, nil)
-	for i := range a {
-		for j, p := 0, 1.0; j < d; j, p = j+1, p*a[i] {
-			x.Set(i, j, p)
-		}
-	}
-	return x
+	// ⎡0.000⎤
+	// ⎣1.000⎦
 }

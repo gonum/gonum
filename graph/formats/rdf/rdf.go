3593b1ee884018404801dfc74ce05b6af6f74c07
Dan Kortschak <dan@kortschak.io>
2022-01-29 13:50:32 +1030

    graph/formats/rdf: fix typo

--- graph/formats/rdf/rdf.go
+++ graph/formats/rdf/rdf.go
@@ -138,7 +138,7 @@ func isLiteral(s string) bool {
 	return strings.HasPrefix(s, `"`) && strings.HasSuffix(s, `"`)
 }
 
-// Parts returns the pars of the term and the kind of the term.
+// Parts returns the parts of the term and the kind of the term.
 // IRI node text is returned as a valid IRI with the quoting angle
 // brackets removed and escape sequences interpreted, and blank
 // nodes are stripped of the "_:" prefix.

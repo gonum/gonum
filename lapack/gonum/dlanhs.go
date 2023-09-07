7bed099d446cc34edea6402f7d9cd32ef11e1310
Vladimir Chalupecky <vladimir@chalupecky.dev>
2023-09-07 11:01:44 +0200

    lapack/gonum: clean up Dlanhs and its test

--- lapack/gonum/dlanhs.go
+++ lapack/gonum/dlanhs.go
@@ -11,26 +11,30 @@ import (
 	"gonum.org/v1/gonum/lapack"
 )
 
-// Dlanhs returns the value of the one norm, or the Frobenius norm, or
-// the infinity norm, or the element of largest absolute value of a
-// Hessenberg matrix A.
+// Dlanhs returns the value of the one norm, or the Frobenius norm, or the
+// infinity norm, or the element of largest absolute value of a Hessenberg
+// matrix A.
 //
-// On using norm=lapack.MaxColumnSum, the vector work must have length n.
+// If norm is lapack.MaxColumnSum, work must have length at least n.
 func (impl Implementation) Dlanhs(norm lapack.MatrixNorm, n int, a []float64, lda int, work []float64) float64 {
 	switch {
+	case norm != lapack.MaxRowSum && norm != lapack.MaxAbs && norm != lapack.MaxColumnSum && norm != lapack.Frobenius:
+		panic(badNorm)
 	case n < 0:
 		panic(nLT0)
 	case lda < max(1, n):
 		panic(badLdA)
+	}
+
+	if n == 0 {
+		return 0
+	}
+
+	switch {
 	case len(a) < (n-1)*lda+n:
 		panic(shortA)
 	case norm == lapack.MaxColumnSum && len(work) < n:
-		panic(badLWork)
-	case norm != lapack.MaxRowSum && norm != lapack.MaxAbs && norm != lapack.MaxColumnSum && norm != lapack.Frobenius:
-		panic(badNorm)
-	}
-	if n == 0 {
-		return 0 // Early return.
+		panic(shortWork)
 	}
 
 	bi := blas64.Implementation()
@@ -64,8 +68,9 @@ func (impl Implementation) Dlanhs(norm lapack.MatrixNorm, n int, a []float64, ld
 	case lapack.Frobenius:
 		scale := 0.0
 		sum := 1.0
-		for j := 0; j < n; j++ {
-			scale, sum = impl.Dlassq(min(n, j+2), a[j:], lda, scale, sum)
+		for i := 0; i < n; i++ {
+			minj := max(0, i-1)
+			scale, sum = impl.Dlassq(n-minj, a[i*lda+minj:], 1, scale, sum)
 		}
 		value = scale * math.Sqrt(sum)
 	}
--- lapack/testlapack/dlanhs.go
+++ lapack/testlapack/dlanhs.go
@@ -19,22 +19,27 @@ type Dlanhser interface {
 
 func DlanhsTest(t *testing.T, impl Dlanhser) {
 	const tol = 1e-15
-	allwork := make([]float64, 9)
 	rnd := rand.New(rand.NewSource(1))
-	for _, n := range []int{1, 2, 4, 9} {
-		for _, lda := range []int{n, n + 5} {
-			a := randomHessenberg(n, lda, rnd)
+	for _, n := range []int{0, 1, 2, 4, 9} {
+		for _, lda := range []int{max(1, n), n + 5} {
+			a := randomGeneral(n, n, lda, rnd)
 			for _, norm := range []lapack.MatrixNorm{lapack.MaxAbs, lapack.MaxRowSum, lapack.MaxColumnSum, lapack.Frobenius} {
 				var work []float64
 				if norm == lapack.MaxColumnSum {
-					work = allwork[:n]
+					work = nanSlice(n)
 				}
-				for i := range work {
-					work[i] = math.NaN()
+
+				got := impl.Dlanhs(norm, a.Rows, a.Data, lda, work)
+
+				// Zero out A below the first subdiagonal.
+				for i := 2; i < n; i++ {
+					for j := 0; j < max(0, i-1); j++ {
+						a.Data[i*a.Stride+j] = 0
+					}
 				}
 				want := dlange(norm, a.Rows, a.Cols, a.Data, a.Stride)
-				got := impl.Dlanhs(norm, a.Rows, a.Data, lda, work)
-				if math.Abs(want-got)/want > tol {
+
+				if math.Abs(want-got) > tol*want {
 					t.Errorf("Case n=%v,lda=%v,norm=%v: unexpected result. Want %v, got %v.", n, lda, normToString(norm), want, got)
 				}
 			}

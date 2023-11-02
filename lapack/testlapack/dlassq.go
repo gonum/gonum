5e05b179125de8d008c4eacd1a5a540d5ccba0ad
Vladimir Chalupecky <vladimir@chalupecky.dev>
2023-11-02 20:28:22 +0100

    lapack/gonum: fix accumulation in Dlassq
    See https://github.com/Reference-LAPACK/lapack/pull/909

--- lapack/testlapack/dlassq.go
+++ lapack/testlapack/dlassq.go
@@ -21,7 +21,7 @@ type Dlassqer interface {
 func DlassqTest(t *testing.T, impl Dlassqer) {
 	values := []float64{
 		0,
-		2 * safmin,
+		0.5 * safmin,
 		smlnum,
 		ulp,
 		1,
@@ -55,7 +55,7 @@ func DlassqTest(t *testing.T, impl Dlassqer) {
 func dlassqTest(t *testing.T, impl Dlassqer, rnd *rand.Rand, n, incx, cas int, v0, v1 float64) {
 	const (
 		rogue = 1234.5678
-		tol   = 1e-14
+		tol   = 1e-15
 	)
 
 	name := fmt.Sprintf("n=%v,incx=%v,cas=%v,v0=%v,v1=%v", n, incx, cas, v0, v1)
@@ -72,15 +72,7 @@ func dlassqTest(t *testing.T, impl Dlassqer, rnd *rand.Rand, n, incx, cas int, v
 		workssq += wi * wi
 	}
 
-	// Construct a test (n+1)-vector z as
-	//  z[0] = known value
-	//  z[1:n+1] = scaling factor * work[:]
-	z := make([]float64, n+1)
-	z[0] = math.Sqrt(v0)
-	for i, wi := range work {
-		z[i+1] = v1 * wi
-	}
-	// Set initial scale and ssq corresponding to z[0].
+	// Set initial scale and ssq corresponding to sqrt(v0).
 	var scale, ssq float64
 	switch cas {
 	case 0:
@@ -101,23 +93,27 @@ func dlassqTest(t *testing.T, impl Dlassqer, rnd *rand.Rand, n, incx, cas int, v
 		panic("bad cas")
 	}
 
-	const (
-		dtsml = 0x1p-511
-		dtbig = 0x1p486
-		dssml = 0x1p537
-		dsbig = 0x1p-538
-	)
-	if scale*math.Sqrt(ssq) > dtbig && scale < math.Sqrt(math.SmallestNonzeroFloat64/ulp)/dsbig {
-		// The scaled sum is big but the scale itself is small.
-		return
+	// Allocate input slice for Dlassq and fill it with
+	//  x[:] = scaling factor * work[:].
+	x := make([]float64, max(0, 1+(n-1)*incx))
+	for i := range x {
+		x[i] = rogue
 	}
-	if scale*math.Sqrt(ssq) < dtsml && scale > math.Sqrt(math.MaxFloat64)/dssml {
-		// The scaled sum is small but the scale itself is big.
-		return
+	for i, wi := range work {
+		x[i*incx] = v1 * wi
+	}
+	xCopy := make([]float64, len(x))
+	copy(xCopy, x)
+
+	scaleGot, ssqGot := impl.Dlassq(n, x, incx, scale, ssq)
+	nrmGot := scaleGot * math.Sqrt(ssqGot)
+
+	if !floats.Same(x, xCopy) {
+		t.Fatalf("%v: unexpected modification of x", name)
 	}
 
-	// Compute the expected value of the sum of squares using the (n+1)-vector z.
-	z0 := z[0]
+	// Compute the expected value of the sum of squares.
+	z0 := math.Sqrt(v0)
 	var z1n float64
 	if n >= 1 {
 		z1n = v1 * math.Sqrt(workssq)
@@ -134,24 +130,6 @@ func dlassqTest(t *testing.T, impl Dlassqer, rnd *rand.Rand, n, incx, cas int, v
 		nrmWant = 0
 	default:
 		nrmWant = zmax * math.Sqrt(1+(zmin/zmax)*(zmin/zmax))
-	}
-
-	// Allocate input slice for Dlassq and fill it with z[1:].
-	x := make([]float64, max(0, 1+(n-1)*incx))
-	for i := range x {
-		x[i] = rogue
-	}
-	for i, zi := range z[1:] {
-		x[i*incx] = zi
-	}
-	xCopy := make([]float64, len(x))
-	copy(xCopy, x)
-
-	scaleGot, ssqGot := impl.Dlassq(n, x, incx, scale, ssq)
-	nrmGot := scaleGot * math.Sqrt(ssqGot)
-
-	if !floats.Same(x, xCopy) {
-		t.Fatalf("%v: unexpected modification of x", name)
 	}
 
 	// Check the result.

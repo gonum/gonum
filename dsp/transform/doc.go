47dde21ae3612907cc203f091f87ed263110ba92
Robert Kleffner <gamecodeshark@gmail.com>
2024-11-17 09:18:35 -0700

    Update dsp/transform/doc.go
    Co-authored-by: Dan Kortschak <dan@kortschak.io>

--- dsp/transform/doc.go
+++ dsp/transform/doc.go
@@ -2,5 +2,5 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package transform provides important transforms on signals used in digital signal processing
+// Package transform provides important transforms on signals used in digital signal processing.
 package transform // import "gonum.org/v1/gonum/dsp/transform"

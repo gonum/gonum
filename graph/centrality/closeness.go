060cfb356c1967d0264b9ce357c9097ff01e1a10
Andreas Scharf <andreas.b.scharf@gmail.com>
2025-02-15 18:47:30 +0100

    Add closeness algorithm for unweighted and weighted graph.

---
+++ graph/centrality/closeness.go
@@ -0,0 +1,85 @@
+package centrality
+
+import (
+	"gonum.org/v1/gonum/graph"
+	"gonum.org/v1/gonum/graph/path"
+	"gonum.org/v1/gonum/graph/traverse"
+	"math"
+)
+
+// ClosenessCentrality computes closeness centrality for all nodes in an unweighted graph.
+func ClosenessCentrality(graph graph.Graph) map[int64]float64 {
+	nodes := graph.Nodes()
+	numberOfNodes := nodes.Len()
+	if numberOfNodes <= 1 {
+		return nil
+	}
+
+	centrality := make(map[int64]float64)
+
+	for nodes.Next() {
+		currentNode := nodes.Node()
+		shortestPath := shortestPathsBFS(graph, currentNode)
+
+		totalDistance := 0.0
+		for _, distance := range shortestPath {
+			totalDistance += float64(distance)
+		}
+
+		// Compute closeness centrality
+		if totalDistance > 0 {
+			centrality[currentNode.ID()] = float64(numberOfNodes-1) / totalDistance
+		} else {
+			centrality[currentNode.ID()] = 0
+		}
+	}
+
+	return centrality
+}
+
+func shortestPathsBFS(g graph.Graph, startNode graph.Node) map[int64]int {
+	distance := make(map[int64]int)
+	var t traverse.BreadthFirst
+	t.Walk(g, startNode, func(n graph.Node, depth int) bool {
+		distance[n.ID()] = depth
+		return false
+	})
+	return distance
+}
+
+func ClosenessCentralityWeighted(g graph.Weighted) map[int64]float64 {
+	nodes := g.Nodes()
+	numberOfNodes := nodes.Len()
+
+	if numberOfNodes <= 1 {
+		return nil
+	}
+	centrality := make(map[int64]float64)
+
+	for nodes.Next() {
+		currentNode := nodes.Node()
+		shortestPaths := path.DijkstraFrom(currentNode, g)
+
+		// Sum shortest path distances
+		totalDistance := 0.0
+		for nodes.Reset(); nodes.Next(); {
+			target := nodes.Node()
+			if target.ID() == currentNode.ID() {
+				continue
+			}
+			distance := shortestPaths.WeightTo(target.ID())
+			if distance != math.Inf(1) {
+				totalDistance += distance
+			}
+		}
+
+		// Compute closeness centrality
+		if totalDistance > 0 {
+			centrality[currentNode.ID()] = float64(numberOfNodes-1) / totalDistance
+		} else {
+			centrality[currentNode.ID()] = 0
+		}
+	}
+
+	return centrality
+}

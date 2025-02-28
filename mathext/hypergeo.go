4ed455260e99c990a10f7be53e194555691002a4
shaoyu <awawfumin@gmail.com>
2025-02-28 14:16:24 +0800

    add more tests for Hypergeo
    * tests from Table 26, John Pearson, Computation of Hypergeometric Functions, Master thesis for Worcester College, Oxford University
    * custom tests designed to pinpoint the exact criteria for handling
      large |a| and |c|

--- mathext/hypergeo.go
+++ mathext/hypergeo.go
@@ -7,6 +7,7 @@ package mathext
 import (
 	"math"
 
+	"gonum.org/v1/gonum/floats/scalar"
 	"gonum.org/v1/gonum/mathext/internal/cephes"
 )
 
@@ -16,6 +17,20 @@ import (
 //
 // See https://en.wikipedia.org/wiki/Hypergeometric_function for more details.
 func Hypergeo(a, b, c, z float64) float64 {
+	// Simplify discussion by ensuring |a| > |b|.
+	if math.Abs(b) > math.Abs(a) {
+		return Hypergeo(b, a, c, z)
+	}
+	// Fix numerical issues for large |a| and |c| using equations 15.2.10 and 15.2.12.
+	// M. Abramowitz and I. A. Stegun 1965. Handbook of Mathematical Functions, New York: Dover.
+	if a < -50 && isInt(c) {
+		return eqn15_2_10(a, b, c, z)
+	}
+	if c < -50 && isPosInt(a) {
+		return eqn15_2_12(a, b, c, z)
+	}
+
+	// Canonical case |z| < 1 supported by Cephes.
 	if math.Abs(z) < 1 {
 		return cephes.Hyp2f1(a, b, c, z)
 	}
@@ -25,8 +40,12 @@ func Hypergeo(a, b, c, z float64) float64 {
 		return math.NaN()
 	}
 
-	// When a or b is a negative integer, Hypergeo reduces to the polynomial described in equation 15.4.1 in Abramowitz.
-	// M. Abramowitz and I. A. Stegun 1965. Handbook of Mathematical Functions, New York: Dover.
+	// Special case for z=-1, use equation 15.3.4 in Abramowitz.
+	if z == -1 {
+		return eqn15_3_4(a, b, c, z)
+	}
+
+	// When a or b is a non-positive integer, Hypergeo reduces to the finite polynomial described in equation 15.4.1.
 	if isNonPosInt(a) {
 		return eqn15_4_1(int(-a), b, c, z)
 	}
@@ -35,14 +54,12 @@ func Hypergeo(a, b, c, z float64) float64 {
 	}
 
 	// Analytic continuation formula contains NaNs from Gamma(a-b) when a-b is an integer.
-	// Fix this by making a and b different using equation 15.3.4 from Abramowitz.
-	if isNonPosInt(a - b) {
-		y := Hypergeo(a, c-b, c, z/(z-1))
-		return math.Pow(1-z, -a) * y
-	}
+	// Fix this by making a and b different via equations 15.3.3 and 15.3.4.
 	if isNonPosInt(c-a) || isNonPosInt(c-b) {
-		y := Hypergeo(c-a, c-b, c, z)
-		return math.Pow(1-z, c-a-b) * y
+		return eqn15_3_3(a, b, c, z)
+	}
+	if isNonPosInt(a - b) {
+		return eqn15_3_4(a, b, c, z)
 	}
 
 	// Analytic continuation based on https://www.johndcook.com/blog/2021/11/03/escaping-the-unit-disk/
@@ -53,26 +70,135 @@ func Hypergeo(a, b, c, z float64) float64 {
 	return y1 + y2
 }
 
+func eqn15_2_10(a, b, c, x float64) float64 {
+	t := a - math.Round(a)
+	var f2 float64
+	f1 := Hypergeo(t, b, c, x)
+	var f0 float64
+	if a < 0 {
+		f0 = Hypergeo(t-1, b, c, x)
+		t--
+		for n := 1; n < int(math.Round(-a)); n++ {
+			f2 = f1
+			f1 = f0
+			f0 = -(2*t-c-t*x+b*x)/(c-t)*f1 - t*(x-1)/(c-t)*f2
+			t--
+		}
+	} else {
+		f0 = Hypergeo(t+1, b, c, x)
+		t++
+		for n := 1; n < int(math.Round(a)); n++ {
+			f2 = f1
+			f1 = f0
+			f0 = -((2*t-c-t*x+b*x)*f1 + (c-t)*f2) / (t * (x - 1))
+			t++
+		}
+	}
+	return f0
+}
+
+func eqn15_2_12(a, b, c, x float64) float64 {
+	t := c - math.Round(c)
+	var f2 float64
+	f1 := Hypergeo(a, b, t, x)
+	var f0 float64
+	if c < 0 {
+		f0 = Hypergeo(a, b, t-1, x)
+		t--
+		for n := 1; n < int(math.Round(-c)); n++ {
+			f2 = f1
+			f1 = f0
+			f0 = -t*(t-1-(2*t-a-b-1)*x)/t/(t-1)/(x-1)*f1 - (t-a)*(t-b)*x/t/(t-1)/(x-1)*f2
+			t--
+		}
+	} else {
+		f0 = Hypergeo(a, b, t+1, x)
+		t++
+		for n := 1; n < int(math.Round(a)); n++ {
+			f2 = f1
+			f1 = f0
+			f0 = -(t*(t-1-(2*t-a-b-1)*x)*f1 + t*(t-1)*(x-1)*f2) / ((t - a) * (t - b) * x)
+			t++
+		}
+	}
+	return f0
+}
+
+func eqn15_3_3(a, b, c, z float64) float64 {
+	y := Hypergeo(c-a, c-b, c, z)
+	return math.Pow(1-z, c-a-b) * y
+}
+
+func eqn15_3_4(a, b, c, z float64) float64 {
+	y := Hypergeo(a, c-b, c, z/(z-1))
+	return math.Pow(1-z, -a) * y
+}
+
 func eqn15_4_1(m int, b, c, z float64) float64 {
-	// zn is (z^n / n!).
-	var zn float64 = 1
+	// lzn is log(z^n / n!).
+	var lzn float64
+	var lznSign int = 1
 	// sum is the sum of the evaluated polynomial.
 	var sum float64 = 1
 	for n := 1; n <= m; n++ {
-		zn *= z / float64(n)
-		sum += pochhammer(float64(-m), n) * pochhammer(b, n) / pochhammer(c, n) * zn
+		lzn += math.Log(math.Abs(z)) - math.Log(float64(n))
+		if z < 0 {
+			lznSign *= -1
+		}
+
+		var w float64
+		var sign int = 1
+		lphC, sn := lpochhammer(c, n)
+		if sn == 0 { // divide by zero
+			return math.NaN()
+		}
+		w -= lphC
+		sign *= sn
+
+		lphM, sn := lpochhammer(float64(-m), n)
+		if sn == 0 {
+			continue
+		}
+		w += lphM
+		sign *= sn
+
+		lphB, sn := lpochhammer(b, n)
+		if sn == 0 {
+			continue
+		}
+		w += lphB
+		sign *= sn
+
+		sum += float64(sign) * float64(lznSign) * math.Exp(w+lzn)
 	}
 	return sum
 }
 
-func pochhammer(x float64, n int) float64 {
-	var y float64 = 1
+func lpochhammer(x float64, n int) (float64, int) {
+	var y float64
+	var sign int = 1
 	for k := range n {
-		y *= x - float64(k)
+		xk := x - float64(k)
+		if xk == 0 {
+			return math.NaN(), 0
+		}
+
+		y += math.Log(math.Abs(xk))
+		if xk < 0 {
+			sign *= -1
+		}
 	}
-	return y
+	return y, sign
+}
+
+func isInt(x float64) bool {
+	return scalar.EqualWithinAbs(math.Round(x), x, 1e-6)
+}
+
+func isPosInt(x float64) bool {
+	return (x > 0) && isInt(x)
 }
 
 func isNonPosInt(x float64) bool {
-	return (x <= 0) && (math.Round(x) == x)
+	return (x <= 0) && isInt(x)
 }
--- mathext/hypergeo_test.go
+++ mathext/hypergeo_test.go
@@ -12,7 +12,68 @@ import (
 	"gonum.org/v1/gonum/floats/scalar"
 )
 
-func TestHyp2f1(t *testing.T) {
+func TestHypergeo(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		a    float64
+		b    float64
+		c    float64
+		z    float64
+		want float64
+	}{
+		// Constants based on https://github.com/scipy/scipy/blob/main/scipy/special/tests/test_hyp2f1.py
+		{a: 1.3, b: -0.2, c: 0.3, z: -2.1, want: 1.8202169687521206},
+
+		// Constants based on https://github.com/scipy/scipy/blob/main/scipy/special/tests/test_mpmath.py
+		{a: 1. / 3, b: 2. / 3, c: 5. / 6, z: 27. / 32, want: 1.6},
+		{a: 1. / 4, b: 1. / 2, c: 3. / 4, z: 80. / 81, want: 1.8},
+		{a: 0.7235, b: -1, c: -5, z: 0.3, want: 1.04341},
+		{a: 0.25, b: 1. / 3, c: 2, z: 0.999, want: 1.0682644949603062},
+		{a: 0.25, b: 1. / 3, c: 2, z: -1, want: 0.9665658449252437},
+		{a: 2, b: 3, c: 5, z: 0.99, want: 27.699347904322664},
+		{a: 3. / 2, b: -0.5, c: 3, z: 0.99, want: 0.6840303684391167},
+		{a: 2, b: 2.5, c: -3.25, z: 0.999, want: 2.183739328012162e+26},
+		{a: -8, b: 18.016500331508873, c: 10.805295997850628, z: 0.90875647507000001, want: -3.566216341442061e-09},
+		{a: -10, b: 900, c: -10.5, z: 0.99, want: 2.5101757354622962e+22},
+		{a: -10, b: 900, c: 10.5, z: 0.99, want: 5.5748237303615776e+17},
+		{a: -1, b: 2, c: 1, z: -1, want: 3},
+		{a: 0.5, b: 1 - 270.5, c: 1.5, z: 0.999 * 0.999, want: 0.053963052503373715},
+
+		// Constants based on https://github.com/scipy/scipy/issues/1561
+		{a: 10, b: 5, c: -300.5, z: 0.5, want: -3.85202708152391e32},
+		{a: -290, b: 5, c: -300.5, z: 0.5, want: 26.9076853843542},
+		{a: 10, b: 5, c: -300.5, z: -0.5, want: 1.08800279612752},
+		{a: 10, b: 5, c: 300.5, z: 0.5, want: 1.08796774775660468},
+		{a: 100, b: 5, c: -300.5, z: 0.5, want: -1.07414870340863581e139},
+
+		// Constants based on Table 26, John Pearson, Computation of Hypergeometric Functions, Master thesis for Worcester College, Oxford University.
+		{a: 0.1, b: 0.2, c: 0.3, z: 0.5, want: 1.046432811217352},
+		{a: -0.1, b: 0.2, c: 0.3, z: 0.5, want: 0.956434210968214},
+		{a: 1e-8, b: 1e-8, c: 1e-8, z: 1e-6, want: 1},
+		{a: 2 + 1e-9, b: 3, c: 5, z: -0.75, want: 0.492238858852651},
+		{a: -2, b: -3, c: -5 + 1e-9, z: 0.5, want: 0.474999999913750},
+		{a: -1, b: -1.5, c: -2 - 1e-15, z: 0.5, want: 0.625},
+		{a: 500, b: -500, c: 500, z: 0.75, want: 9.332636185032189e-302},
+		{a: 500, b: 500, c: 500, z: -0.6, want: 8.709809816217217e-103},
+		{a: -1000, b: -2000, c: -4000.1, z: -0.5, want: 5.233580403196932e94},
+		{a: -100, b: -200, c: -300 + 1e-9, z: 0.5 * math.Sqrt(2), want: 2.653635302903707e-31},
+		{a: -100, b: -200, c: 300 + 1e-9, z: -0.5 * math.Sqrt(2), want: -1.073671875630690e-30},
+		{a: 300, b: 10, c: 5, z: 0.5, want: 3.912238919961547e98},
+		{a: 5, b: -300, c: 10, z: 0.5, want: 1.661006238211309e-7},
+		{a: 2.25, b: 3.75, c: -0.5, z: -1, want: -0.631220676949703},
+	}
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			y := Hypergeo(test.a, test.b, test.c, test.z)
+			if !scalar.EqualWithinRel(y, test.want, 1e-10) {
+				t.Errorf("expected result from Hypergeo(%f, %f, %f, %f): got %f want %f", test.a, test.b, test.c, test.z, y, test.want)
+			}
+		})
+	}
+}
+
+func TestAAA(t *testing.T) {
 	t.Parallel()
 
 	// Constants taken from https://github.com/RobinHankin/hypergeo/blob/master/tests/testthat/test_aaa.R
@@ -37,7 +98,7 @@ func TestHyp2f1(t *testing.T) {
 	}
 }
 
-func TestHyp2f1_15_1_15(t *testing.T) {
+func Test_15_1_15(t *testing.T) {
 	t.Parallel()
 
 	// eqn15_1_15_lhs is the left hand side of equation 15.1.15 of
@@ -70,7 +131,7 @@ func TestHyp2f1_15_1_15(t *testing.T) {
 	}
 }
 
-func TestHyp2f1_15_2_10(t *testing.T) {
+func Test_15_2_10(t *testing.T) {
 	t.Parallel()
 
 	eqn15_2_10 := func(a, b, c, z float64) float64 {
@@ -97,7 +158,7 @@ func TestHyp2f1_15_2_10(t *testing.T) {
 	}
 }
 
-func TestHyp2f1_15_1(t *testing.T) {
+func Test_15_1(t *testing.T) {
 	t.Parallel()
 
 	type equation struct {
@@ -162,7 +223,7 @@ func TestHyp2f1_15_1(t *testing.T) {
 	}
 }
 
-func TestHyp2f1_15_1_zz(t *testing.T) {
+func Test_15_1_zz(t *testing.T) {
 	t.Parallel()
 
 	type equation struct {
@@ -217,7 +278,7 @@ func TestHyp2f1_15_1_zz(t *testing.T) {
 	}
 }
 
-func TestHyp2f1_Igor_Kojanov(t *testing.T) {
+func Test_Igor_Kojanov(t *testing.T) {
 	t.Parallel()
 
 	var y float64
@@ -233,50 +294,12 @@ func TestHyp2f1_Igor_Kojanov(t *testing.T) {
 	}
 }
 
-func TestHyp2f1_John_Ormerod(t *testing.T) {
+func Test_John_Ormerod(t *testing.T) {
 	t.Parallel()
 
 	y := Hypergeo(5.25, 1, 6.5, 0.501)
 	want := 1.70239432012007391092082702795
 	if !scalar.EqualWithinAbs(y, want, 1e-10) {
 		t.Errorf("unexpected result from Hypergeo(5.25, 1, 6.5, 0.501): got %f want %f", y, want)
-	}
-}
-
-func TestHyp2f1Scipy(t *testing.T) {
-	t.Parallel()
-
-	tests := []struct {
-		a    float64
-		b    float64
-		c    float64
-		z    float64
-		want float64
-	}{
-		// Constants taken from https://github.com/scipy/scipy/blob/main/scipy/special/tests/test_hyp2f1.py
-		{a: 1.3, b: -0.2, c: 0.3, z: -2.1, want: 1.8202169687521206},
-
-		// Constants taken from https://github.com/scipy/scipy/blob/main/scipy/special/tests/test_mpmath.py
-		{a: 1. / 3, b: 2. / 3, c: 5. / 6, z: 27. / 32, want: 1.6},
-		{a: 1. / 4, b: 1. / 2, c: 3. / 4, z: 80. / 81, want: 1.8},
-		{a: 0.7235, b: -1, c: -5, z: 0.3, want: 1.04341},
-		{a: 0.25, b: 1. / 3, c: 2, z: 0.999, want: 1.0682644949603062},
-		{a: 0.25, b: 1. / 3, c: 2, z: -1, want: 0.9665658449252437},
-		{a: 2, b: 3, c: 5, z: 0.99, want: 27.699347904322664},
-		{a: 3. / 2, b: -0.5, c: 3, z: 0.99, want: 0.6840303684391167},
-		{a: 2, b: 2.5, c: -3.25, z: 0.999, want: 2.183739328012162e+26},
-		{a: -8, b: 18.016500331508873, c: 10.805295997850628, z: 0.90875647507000001, want: -3.566216341442061e-09},
-		{a: -10, b: 900, c: -10.5, z: 0.99, want: 2.5101757354622962e+22},
-		{a: -10, b: 900, c: 10.5, z: 0.99, want: 5.5748237303615776e+17},
-		{a: -1, b: 2, c: 1, z: -1, want: 3},
-		{a: 0.5, b: 1 - 270.5, c: 1.5, z: 0.999 * 0.999, want: 0.053963052503373715},
-	}
-	for i, test := range tests {
-		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
-			y := Hypergeo(test.a, test.b, test.c, test.z)
-			if !scalar.EqualWithinRel(y, test.want, 1e-10) {
-				t.Errorf("expected result from Hypergeo(%f, %f, %f, %f): got %f want %f", test.a, test.b, test.c, test.z, y, test.want)
-			}
-		})
 	}
 }

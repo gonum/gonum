90d2326f872921953100e4c5cd484c010fb9f140
shaoyu <awawfumin@gmail.com>
2025-03-02 05:43:38 +0800

    add comments explaining the isCephesBug function

--- mathext/hypergeo.go
+++ mathext/hypergeo.go
@@ -204,6 +204,13 @@ func lpochhammer(x float64, n int) (float64, int) {
 	return y, sign
 }
 
+// isCephesBug checks if the received arguments hit a Cephes bug.
+// If the bug is hit, isCephesBug returns the remedied value.
+//
+// This bug concerns Cephes' evaluation of mathext.Digamma on negative integers,
+// which is mathematically undefined.
+// For reference, scipy also found this workaround necessary in:
+// https://github.com/scipy/scipy/commit/1dfea85b34e8cbd868f9fc43345898084ba73aab
 func isCephesBug(a, b, c, z float64) (float64, bool) {
 	const cephesEPS = 1e-13
 	d := c - a - b
